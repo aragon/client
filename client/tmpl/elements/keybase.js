28931527059b943729a37de13f4632118b9a3057
Jorge Izquierdo <jorge@izqui.me>
2016-12-02 12:50:55 +0100

    Refactor keybase autocomplete

--- client/tmpl/elements/keybase.js
+++ client/tmpl/elements/keybase.js
@@ -1,24 +1,16 @@
 import Keybase from '/client/lib/keybase'
 
 Template.keybase_el.rendered = () => {
-  let tmplInstance = this
-
   this.$('.ui.search').search({
     apiSettings: {
       url: 'https://keybase.io/_/api/1.0/user/autocomplete.json?q={query}',
-      onResponse: (res) => {
-        let results = []
-        for (let completion of res.completions) {
-          let comps = completion.components
-          let user = {
-            username: comps.username.val,
-            name: (comps.full_name || {}).val,
-            pic: completion.thumbnail || 'http://placekitten.com/g/64/64'
-          }
-          results.push(user)
-        }
-        return {results}
-      }
+      onResponse: res => ({
+        results: res.completions.map(c => ({
+          username: c.components.username.val,
+          name: (c.components.full_name || {}).val,
+          pic: c.thumbnail || 'http://placekitten.com/g/64/64'
+        }))
+      })
     },
     fields: {
       title: 'name',
