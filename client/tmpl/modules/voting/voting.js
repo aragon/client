31f6e09772016329771db51d39ca3d19aec37b08
Jorge Izquierdo <jorge@izqui.me>
2016-12-12 12:32:03 +0100

    Selected item

--- client/tmpl/modules/voting/voting.js
+++ client/tmpl/modules/voting/voting.js
@@ -3,16 +3,22 @@ import VotingWatcher from '/client/lib/ethereum/votings'
 const Votings = VotingWatcher.Votings
 
 const tmpl = Template.Module_Voting.extend()
+const selectedVoting = new ReactiveVar()
+
 
 tmpl.routes({
   '/': () => TemplateVar.set('rightSection', 'Module_Voting_Empty'),
-  '/:id': (q) => {
+  '/:id': () => {
     TemplateVar.set('rightSection', 'Module_Voting_Section')
     Template.instance().$('#votingSection').trigger('reload')
+    selectedVoting.set(FlowRouter.current().params.id)
   },
 })
 
-tmpl.onCreated(() => TemplateVar.set('rightSection', 'Module_Voting_Empty'))
+tmpl.onCreated(() => {
+  TemplateVar.set('rightSection', 'Module_Voting_Empty')
+  selectedVoting.set(FlowRouter.current().params.id)
+})
 
 tmpl.onRendered(function () {
   this.$('.button.toggle').state()
@@ -23,11 +29,12 @@ tmpl.events({
   'click #pastVotings': () => (
     TemplateVar.set('pastVotings', !TemplateVar.get('pastVotings'))
   ),
-  'click tbody tr': (e) => (FlowRouter.go(`/voting/${e.currentTarget.dataset.voting}`)),
+  'click #votings tbody tr': (e) => (FlowRouter.go(`/voting/${e.currentTarget.dataset.voting}`)),
 })
 
 tmpl.helpers({
   votings: () => Votings.find({ closingTime: { $gt: new Date() } }),
   pastVotings: () => Votings.find({ closingTime: { $lt: new Date() } }),
   displayOutcome: (outcome) => ((outcome) ? '👍' : '👎'),
+  isSelected: id => id === selectedVoting.get(),
 })
--- client/tmpl/modules/voting/votingSection.js
+++ client/tmpl/modules/voting/votingSection.js
@@ -10,25 +10,49 @@ const Stocks = StockWatcher.Stocks
 const tmpl = Template.Module_Voting_Section.extend([ClosableSection])
 
 const votingVar = new ReactiveVar()
+const updated = new ReactiveVar()
+
 const voteId = () => FlowRouter.current().params.id
 const voting = () => Votings.findOne({ address: voteId() })
+const reload = () => {
+  votingVar.set(voting())
+  updated.set(Math.random())
+}
+
 const canVote = async () => {
   const address = EthAccounts.findOne().address
   const vote = voting().index
   const allVotes = Stocks.find().fetch().map(s => Stock.at(s.address).canVote.call(address, vote))
   const allResults = await Promise.all(allVotes)
   // as long as it can vote in any stock, return true
-  return allResults.reduce((v, acc) => v || acc, false)
+  return allResults.reduce((acc, v) => v || acc, false)
+}
+
+const countVotes = async (optionId) => {
+  const counted = await Company.countVotes.call(voting().index, optionId)
+  const votes = counted[0].toNumber()
+  return { votes, percentaje: Math.round(10000 * (votes / counted[1].toNumber())) / 100 }
+}
+
+const pendingVotes = async (options) => {
+  const allOptions = await Promise.all(options.map((o, i) => Company.countVotes.call(voting().index, i)))
+  const total = allOptions[0][1].toNumber()
+  const allVotes = allOptions.reduce((acc, v) => acc + v[0].toNumber(), 0)
+  const votes = total - allVotes
+  return { votes, percentaje: Math.round(10000 * (votes / total)) / 100 }
 }
 
 tmpl.onCreated(() => {
-  votingVar.set(voting())
+  reload()
 })
 
 tmpl.helpers({
+  updatesHack: () => updated.get(),
   voting: () => votingVar.get(),
-  options: () => voting().options,
+  options: () => votingVar.get().options,
   canVote: ReactivePromise(canVote),
+  countVotes: ReactivePromise(countVotes),
+  pendingVotes: ReactivePromise(pendingVotes),
 })
 
 const castVote = async option => {
@@ -36,10 +60,11 @@ const castVote = async option => {
   const result = await Company.castVote(voting().index, option,
                   { from: EthAccounts.findOne().address, gas: 4800000 })
   console.log(result)
+  reload()
 }
 
 tmpl.events({
   'click .button': (e) => castVote($(e.currentTarget).data('option')),
   'success .dimmer': () => FlowRouter.go('/voting'),
-  'reload #votingSection': () => votingVar.set(voting()),
+  'reload #votingSection': reload,
 })
