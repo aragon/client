f8aa7fbc10edefd961fce15179e2942aea24248b
Jorge Izquierdo <jorge@izqui.me>
2016-12-12 13:11:30 +0100

    App knows about Binary votings only

--- client/helpers.js
+++ client/helpers.js
@@ -5,6 +5,6 @@ helpers.timeRange = (a, b) => moment(a).twix(b).humanizeLength()
 helpers.session = name => Session.get(name)
 helpers.plus = (a, b) => a + b
 helpers.equals = (a, b) => a === b
-helpers.percentFormat = (x) => `${Math.round(10000 * x) / 100}%`
+helpers.percentFormat = (x) => `${Math.round(10000 * (x || 0)) / 100}%`
 
 Object.keys(helpers).forEach(k => Template.registerHelper(k, helpers[k]))
--- client/lib/ethereum/contracts.js
+++ client/lib/ethereum/contracts.js
@@ -1,13 +1,13 @@
 import Company from '/imports/lib/contracts/build/contracts/Company.sol.js'
 import Stock from '/imports/lib/contracts/build/contracts/Stock.sol.js'
-import Voting from '/imports/lib/contracts/build/contracts/Voting.sol.js'
+import BinaryVoting from '/imports/lib/contracts/build/contracts/BinaryVoting.sol.js'
 import IssueStockVoting from '/imports/lib/contracts/build/contracts/IssueStockVoting.sol.js'
 
 import web3 from './web3'
 
 Company.setProvider(web3.currentProvider)
 Stock.setProvider(web3.currentProvider)
-Voting.setProvider(web3.currentProvider)
+BinaryVoting.setProvider(web3.currentProvider)
 IssueStockVoting.setProvider(web3.currentProvider)
 
-export { Company, Stock, Voting, IssueStockVoting }
+export { Company, Stock, BinaryVoting as Voting, IssueStockVoting }
