2efa9be5c675b2f5f4de05a27fc27eee7d2e970b
Luis Iván Cuende <me@luisivan.net>
2016-12-13 17:09:51 -0800

    Entity doesn't require a dimmer

--- client/tmpl/modules/entity/entity.js
+++ client/tmpl/modules/entity/entity.js
@@ -6,14 +6,8 @@ const tmpl = Template.Module_Entity.extend([ClosableSection])
 tmpl.helpers({
   address: () => FlowRouter.current().params.address,
   entity: ReactivePromise(async () => {
-    if (FlowRouter.current()) {
-      const dimmer = this.$('.dimmer')
-      dimmer.trigger('loading')
-      const entity = Identity.get(FlowRouter.current().params.address)
-      if (dimmer) dimmer.trigger('finished')
-      return entity
-    }
-    return null
+    if (FlowRouter.current()) return Identity.get(FlowRouter.current().params.address)
+    return {}
   }),
-  formatFingerprint: (fingerprint) => fingerprint.match(/.{1,4}/g).join(' '),
+  formatFingerprint: (fingerprint) => (fingerprint && fingerprint.match(/.{1,4}/g).join(' ')),
 })
