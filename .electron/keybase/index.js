4cbfd8b882cd6eff3ce9539f08e9b2a9c71918d9
Jorge Izquierdo <jorge@izqui.me>
2016-12-16 14:01:07 +0100

    Save proof file

--- .electron/keybase/index.js
+++ .electron/keybase/index.js
@@ -1,11 +1,25 @@
 const fs = require('fs')
+const path = require('path')
 
-const Keybase = {
-  getUsername: (cb) => {
-    fs.readdir('/keybase/public', (err, dirs) => {
+const proofFilename = 'ethereum_test.json'
+const keybasePublicDirectory = user => path.join('/keybase/public', user || String())
+const proofPath = user => path.join(keybasePublicDirectory(user), proofFilename)
+
+// TODO: Promisify all this?
+class Keybase {
+  static getUsername(cb) {
+    fs.readdir(keybasePublicDirectory(), (err, dirs) => {
       cb(err, dirs[0])
     })
-  },
+  }
+
+  static saveProof(proofPayload, cb) {
+    this.getUsername((err, username) => {
+      const proof = JSON.parse(proofPayload)
+      if (username !== proof.username) { return cb(new Error('username doesnt match')) }
+      return fs.writeFile(proofPath(username), proofPayload, cb)
+    })
+  }
 }
 
 module.exports = Keybase
