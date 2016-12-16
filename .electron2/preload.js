7db4701e4344a69e457eea7da4b577e06a3ba422
Luis Iván Cuende <me@luisivan.net>
2016-12-16 15:54:17 -0800

    Renamed

--- .electron2/preload.js
+++ .electron2/preload.js
@@ -1,14 +1,10 @@
 const { ipcRenderer } = require('electron')
 
 process.once('loaded', () => {
-  console.log('hola')
+  window.ipcRenderer = ipcRenderer
+
   try {
     require('devtron').install()
     window.__devtron = { require, process }
-    console.log('manola')
   } catch (e) {}
-
-  console.log('patata')
-  console.log(ipcRenderer)
-  window.ipcRenderer = ipcRenderer
 })
