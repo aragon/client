ea65134d539990c5ac0edf9bc5e5e79cb9e98aed
Luis Iván Cuende <me@luisivan.net>
2016-12-22 17:25:15 -0800

    Account enhancements

--- client/tmpl/modules/account/account.js
+++ client/tmpl/modules/account/account.js
@@ -1,13 +1,19 @@
 const tmpl = Template.Module_Account.extend()
 
-tmpl.routes({
-  '/': () => TemplateVar.set('rightSection', 'Module_Account_Empty'),
-  '/entity/:address': () => TemplateVar.set('rightSection', 'Module_Entity'),
+tmpl.onRendered(function () {
+  this.autorun(() => {
+    if (TemplateVar.get('viewAllAccounts')) {
+      requestAnimationFrame(() => this.$('.dropdown').dropdown())
+    }
+  })
+  this.$('#logout').popup({ position: 'top center' })
 })
 
-tmpl.onRendered(function () {
-  this.$('.ui.dropdown').dropdown()
-  this.$('#logout').popup({ position: 'top center' })
+tmpl.events({
+  'click #viewAllAccounts': () => {
+    alert('If you change your account, you will have to re-link your identity and you will have to take care of past transactions incurred with your current Ethereum account.\nProceed carefully.')
+    TemplateVar.set('viewAllAccounts', true)
+  },
 })
 
 tmpl.helpers({
