e0d217cede6766a5cb3f6853f9f7fa71c0606325
Luis Iván Cuende <me@luisivan.net>
2017-01-04 17:53:14 +0100

    Shake API

--- client/init.js
+++ client/init.js
@@ -1,6 +1,30 @@
 import Identity from '/client/lib/identity'
-import { Module, Core } from './lib/modules'
-import { BrowserNotifications } from './lib/notifications'
+import { Module, Core } from '/client/lib/modules'
+import { BrowserNotifications } from '/client/lib/notifications'
+
+import Shake from '/client/lib/shake'
+
+Shake.createUser({
+  user: {
+    firstName: 'Shakey',
+    lastName: 'McShakester',
+    dateOfBirth: '1988-07-22',
+    phoneNumber: '15145551234',
+    email: 'shakey@shakepay.co',
+  },
+  address: {
+    address1: '123 Awesome Drive',
+    address2: 'Apt 1223',
+    city: 'Montreal',
+    zipCode: 'H2X2E3',
+    fedDistrict: 'Quebec',
+    country: 'CA',
+  },
+  card: {
+    currency: 'USD',
+    type: 'physical',
+  },
+})
 
 const setIdentity = async () => {
   let current = Identity.current(true)
---
+++ client/lib/shake/index.js
@@ -0,0 +1,36 @@
+// @flow
+
+import type { ShakeUser } from './user'
+
+const authToken = '7396fcac-0ab4-4475-852e-0f1963ee356b'
+
+const shakeUrl = (action): string => `https://backend.qa.shakepay.co/api/${action}?key=${authToken}`
+
+const postJSON = (action, data) => (
+  fetch(shakeUrl(action), {
+    method: 'POST',
+    mode: 'cors',
+    body: JSON.stringify(data),
+    headers: new Headers({
+      'Content-Type': 'application/json',
+    }),
+  })
+)
+
+type ShakeInvoice = {
+  email: string,
+  currency: string,
+  amount: number,
+}
+
+export default class Shake {
+  static async createUser(userData: ShakeUser) {
+    const res = await postJSON('user', userData)
+    console.log(res)
+  }
+
+  static async createInvoice(invoiceData: ShakeInvoice) {
+    const res = await postJSON('invoices', invoiceData)
+    console.log(res)
+  }
+}
