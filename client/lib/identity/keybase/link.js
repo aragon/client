ee65591f535215fb7203b712c1c921794dabbacb
Jorge Izquierdo <jorge@izqui.me>
2017-01-10 19:50:03 +0100

    Keybase proofs

--- client/lib/identity/keybase/link.js
+++ client/lib/identity/keybase/link.js
@@ -2,19 +2,21 @@
 
 import IntertronClient from 'intertron-client'
 
-// import Proofs from './proofs'
+import Proofs from './proofs'
 
 const intertron = new IntertronClient()
 
-const link = async (address: string): Promise<string> => {
+const link = async (address: string, proofFilename: string): Promise<string> => {
+  console.log('linking', address)
+
   const username = await intertron.call('Keybase.getUsername')
   console.log('Your Keybase username is', username, address)
 
-  /* TODO: Make it work
   const proof = await Proofs.createProof(username, address)
   const proofPayload = JSON.stringify(proof)
-  await intertron.call('Keybase.saveProof', proofPayload)
-  console.log('saved proof', proofPayload) */
+  await intertron.call('Keybase.saveProof', proofPayload, proofFilename)
+
+  console.log('saved proof', proofPayload)
 
   return username
 }
