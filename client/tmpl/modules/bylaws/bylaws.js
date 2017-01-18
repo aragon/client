8f3fe3ace95a19893a3297ee4d37fe6b8e1a2656
Luis Iván Cuende <me@luisivan.net>
2017-01-18 12:48:08 +0100

    Bylaws structure

--- client/tmpl/modules/bylaws/bylaws.js
+++ client/tmpl/modules/bylaws/bylaws.js
@@ -5,7 +5,24 @@ import { Template } from 'meteor/templating'
 const tmpl = Template.Module_Bylaws.extend()
 
 tmpl.onRendered(function () {
+  $('.ui.modal').modal({
+    inverted: true,
+    // The callbacks are inverted since the recommended action is to cancel
+    onApprove: () => {
+      console.log('Cancelled')
+      BlazeLayout.reset()
+    },
+    onDeny: () => {
+      console.log('Approved')
+    },
+  })
+
+  setTimeout(showModal.bind(this), 1000)
 })
 
 tmpl.helpers({
 })
+
+const showModal = () => {
+  this.$('.ui.modal').modal('show')
+}
