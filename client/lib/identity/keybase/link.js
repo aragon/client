82c72968bcd32dae0ad2a0a51e033b8d8ab349c7
Jorge Izquierdo <jorge@izqui.me>
2017-01-20 11:55:32 +0100

    Fix keybase linking

--- client/lib/identity/keybase/link.js
+++ client/lib/identity/keybase/link.js
@@ -7,16 +7,12 @@ import Proofs from './proofs'
 const intertron = new IntertronClient()
 
 const link = async (address: string, proofFilename: string): Promise<string> => {
-  console.log('linking', address)
-
   const username = await intertron.call('Keybase.getUsername')
   console.log('Your Keybase username is', username, address)
 
   const proof = await Proofs.createProof(username, address)
   const proofPayload = JSON.stringify(proof)
   await intertron.call('Keybase.saveProof', proofPayload, proofFilename)
-
-  console.log('saved proof', proofPayload)
 
   return username
 }
