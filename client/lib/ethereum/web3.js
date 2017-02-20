bab000f0da7fee9120391260cd0a49f492d8b323
Luis Iván Cuende <me@luisivan.net>
2017-02-20 17:21:51 +0100

    Stupid fix

--- client/lib/ethereum/web3.js
+++ client/lib/ethereum/web3.js
@@ -1,9 +1,8 @@
-window.injectMetaMask = (typeof web3 === 'undefined' && !Meteor.settings.public.noMetamask)
-
 if (Meteor.settings.public.landingNode) {
   window.web3 = new Web3(new Web3.providers.HttpProvider(Meteor.settings.public.landingNode))
 } else {
-  window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+  // window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+  window.injectMetaMask = (typeof web3 === 'undefined' && !Meteor.settings.public.noMetamask)
 }
 
 export default web3
