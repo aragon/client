075f986927cc770e959897da855e07df7b71dd33
Luis Iván Cuende <me@luisivan.net>
2017-02-22 10:41:18 +0100

    Build folder

---
+++ client/lib/build/index.js
@@ -0,0 +1,24 @@
+// @flow
+
+import { Meteor } from 'meteor/meteor'
+
+const deepValue = (obj, path) => {
+  let newObj = {}
+  for (let i = 0, newPath = path.split('.'), len = newPath.length; i < len; i++) {
+    newObj = newObj[newPath[i]]
+  }
+  return newObj
+}
+
+const Build = {
+  Settings: {
+    get: (key: string) => {
+      if (Meteor.settings && Meteor.settings.public) {
+        return deepValue(Meteor.settings.public, key)
+      }
+      return undefined
+    },
+  },
+}
+
+export default Build
