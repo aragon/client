036babff33748cbc011426dc7f576358d74941b9
Luis Iván Cuende <me@luisivan.net>
2017-03-13 13:01:58 +0100

    Sayonara Inbox module

--- client/tmpl/layout/layoutInbox.js
+++ client/tmpl/layout/layoutInbox.js
@@ -6,13 +6,23 @@ import { NotificationsManager } from '/client/lib/notifications'
 
 const Notifications = NotificationsManager.Notifications
 
-const tmpl = Template.Module_Inbox
+const tmpl = Template.Layout_Inbox
 
 const performAction = id =>
     NotificationsManager.performNotificationAction(Notifications.findOne(id))
 
 const clearAll = () =>
     Notifications.update({ handled: false }, { handled: true }, { multi: true })
+
+tmpl.onRendered(() => {
+  $('#inboxButton').popup({
+    inline: true,
+    on: 'click',
+    lastResort: 'bottom center',
+    position: 'bottom center',
+    onShow: () => (window.resizePopups()),
+  })
+})
 
 tmpl.helpers({
   unhandledNotifs: () => Notifications.find({ handled: false }, { sort: { date: -1 } }),
