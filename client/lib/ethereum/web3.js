abcc88a8f34f32b0ac369e2a31b689715f085126
Jorge Izquierdo <jorge@izqui.me>
2017-03-30 10:50:43 +0200

    Alert when no web3 is present and isnt being injected

--- client/lib/ethereum/web3.js
+++ client/lib/ethereum/web3.js
@@ -1,6 +1,15 @@
 // @flow
 import Build from '/client/lib/build'
 
-window.injectMetaMask = (typeof web3 === 'undefined' && !Build.Settings.get('noMetamask'))
+const isDev = location.origin.indexOf('localhost') > -1
+const isClient = location.origin.indexOf('aragon://') > -1
+
+window.injectMetaMask = isClient || isDev
+
+console.log('[Aragon] Injecting metamask for:', location.origin, isClient, isDev)
+
+if (typeof web3 === 'undefined' && !window.injectMetaMask) {
+  alert('Your browser is not web3 compatible, which means Aragon cannot connect to the Ethereum network and therefore cannot run. Visit https://aragon.one to download the client for your OS or use https://metamask.io for Chrome.')
+}
 
 export default window.web3
