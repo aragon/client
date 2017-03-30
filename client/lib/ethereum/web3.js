73a56899cea0a3b3afce64f404f41cabab2bb0c7
Jorge Izquierdo <jorge@izqui.me>
2017-03-30 10:57:46 +0200

    improve no web3 alert logic

--- client/lib/ethereum/web3.js
+++ client/lib/ethereum/web3.js
@@ -4,11 +4,14 @@ import Build from '/client/lib/build'
 const isDev = location.origin.indexOf('localhost') > -1
 const isClient = location.origin.indexOf('aragon://') > -1
 
-window.injectMetaMask = isClient || isDev
+const needsWeb3 = typeof web3 === 'undefined'
+const canInjectMetamask = isClient || isDev
+
+window.injectMetaMask = canInjectMetamask && needsWeb3
 
 console.log('[Aragon] Injecting metamask for:', location.origin, isClient, isDev)
 
-if (typeof web3 === 'undefined' && !window.injectMetaMask) {
+if (!canInjectMetamask && needsWeb3) {
   alert('Your browser is not web3 compatible, which means Aragon cannot connect to the Ethereum network and therefore cannot run. Visit https://aragon.one to download the client for your OS or use https://metamask.io for Chrome.')
 }
 
