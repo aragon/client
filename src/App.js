imhomos/aragon

Permissions: base structure

src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import createHistory from 'history/createHashHistory'
 import { styled, AragonApp } from '@aragon/ui'
-import Home from './components/Home/Home'
 import MenuPanel from './components/MenuPanel/MenuPanel'
+import Home from './components/Home/Home'
+import Permissions from './components/Permissions/Permissions'
 import { apps, notifications, tokens, prices, homeActions } from './demo-state'
 
 class App extends React.Component {
@@ -95,6 +96,7 @@ class App extends React.Component {
                 onAction={this.handleAction}
               />
             )}
+            {app === 'permissions' && <Permissions />}
           </AppScreen>
         </Main>
       </AragonApp>

aragon/client

src/App.js
@@ -4,6 +4,7 @@ import { styled, AragonApp } from '@aragon/ui'
 import AppIFrame from './components/App/AppIFrame'
 import Home from './components/Home/Home'
 import MenuPanel from './components/MenuPanel/MenuPanel'
+import Permissions from './components/Permissions/Permissions'
 import { apps, notifications, tokens, prices, homeActions } from './demo-state'
 
 class App extends React.Component {
@@ -89,6 +90,7 @@ class App extends React.Component {
                 onOpenApp={this.handleOpenApp}
               />
             )}
+            {appId === 'permissions' && <Permissions />}
             <AppIFrame src={this.getAppSrc(appId)} />
           </AppScreen>
         </Main>