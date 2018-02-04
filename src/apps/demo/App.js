87337e621328a8c1b005c9049a22e4e3281b8f64
Pierre Bertet <hello@pierre.world>
2018-02-04 10:55:57 +0100

    Demo app tweaks (#102)

--- src/apps/demo/App.js
+++ src/apps/demo/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
-import { AragonApp, EmptyStateCard, IconSettings } from '@aragon/ui'
+import { AragonApp, EmptyStateCard } from '@aragon/ui'
+
+import icon from './assets/icon-demo.svg'
+
+const Icon = <img src={icon} alt="" />
 
 const App = () => (
   <AragonApp publicUrl="/aragon-ui/">
     <Main>
-      <EmptyStateCard
-        actionText="This is a demo!"
-        icon={IconSettings}
-        title="Demo app"
-      />
+      <EmptyStateCard actionText="Button" icon={() => Icon} title="Demo App" />
     </Main>
   </AragonApp>
 )
