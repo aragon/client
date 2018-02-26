nexus420/arg_p2p

Add Settings app UI

src/apps/index.js
@@ -1,0 +1,1 @@
+export { default as Settings } from './settings/Settings'

src/demo-state.js
@@ -103,3 +103,13 @@ export const homeActions = [
   { id: 'check-finance', label: 'Check Finance', app: 'finance' },
   { id: 'new-payment', label: 'New Payment', app: 'tokens' },
 ]
+
+export const appStates = {
+  settings: {
+    network: 'Kovan',
+    networkName: 'Kovan testnet',
+    organizationAddress: '0x3f5ce5fbfe3e9af3971dd833d26ba9b5c936f0be',
+    currencies: ['USD', 'EUR', 'GBP', 'CAD', 'RMB', 'JPY'],
+    selectedCurrency: 'EUR',
+  },
+}

aragon/client

src/apps/index.js
@@ -1,0 +1,2 @@
+export { default as Permissions } from './Permissions/Permissions'
+export { default as Settings } from './Settings/Settings'

src/demo-state.js
@@ -171,7 +171,14 @@ export const permissions = {
   ],
 }
 
-export const network = 'rinkeby'
+export const settings = {
+  network: 'rinkeby',
+  networkName: 'Rinkeby testnet',
+  organizationAddress: '0x3f5ce5fbfe3e9af3971dd833d26ba9b5c936f0be',
+  currencies: ['USD', 'EUR', 'GBP', 'CAD', 'RMB', 'JPY'],
+  defaultCurrency: 'EUR',
+}
+
 export const actionIntent = {
   description: 'perform a payment to',
   to: '0x3f5ce5fbfe3e9af3971dd833d26ba9b5c936f0be',