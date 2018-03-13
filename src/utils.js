bd864059946832db896796244a6003c7c12e2909
Brett Sun <qisheng.brett.sun@gmail.com>
2018-03-13 14:51:15 +0100

    Add compose util

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
+// Stealing this from recompose / etc for now
+export function compose(...funcs) {
+  if (funcs.length === 0) {
+    return arg => arg
+  }
+
+  if (funcs.length === 1) {
+    return funcs[0]
+  }
+
+  return funcs.reduce((a, b) => (...args) => a(b(...args)))
+}
+
 export function noop() {}
