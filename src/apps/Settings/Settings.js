9a7d770b9ac00f359048aa75210bea70be77cc84
Brett Sun <qisheng.brett.sun@gmail.com>
2018-03-20 18:52:49 +0100

    Settings: add test tokens

--- src/apps/Settings/Settings.js
+++ src/apps/Settings/Settings.js
@@ -18,6 +18,45 @@ import { compose } from '../../utils'
 // const AVAILABLE_CURRENCIES = ['USD', 'EUR', 'GBP', 'CAD', 'RMB', 'JPY']
 const AVAILABLE_CURRENCIES = ['USD'] // Only use USD for now
 
+const TEST_TOKENS = [
+  {
+    address: '0x0d5263b7969144a852d58505602f630f9b20239d',
+    symbol: '',
+  },
+  {
+    address: '0x6142214d83670226872d51e935fb57bec8832a60',
+    symbol: '',
+  },
+  {
+    address: '0x1e1cab55639f67e70973586527ec1dfdaf9bf764',
+    symbol: '',
+  },
+  {
+    address: '0x5e381afb0104d374f1f3ccde5ba7fe8f5b8af0e6',
+    symbol: '',
+  },
+  {
+    address: '0xa53899a7eb70b309f05f8fdb344cdc8c8f272abe',
+    symbol: '',
+  },
+  {
+    address: '0x5b2fdbba47e8ae35b9d6f8e1480703334f48b96c',
+    symbol: '',
+  },
+  {
+    address: '0x51e53b52555a4ab7227423a7761cc8e418b147c8',
+    symbol: '',
+  },
+  {
+    address: '0xc42da14b1c0ae7d4dd3946633f1046c3d46f3101',
+    symbol: '',
+  },
+  {
+    address: '0x4fc6e3b791560f25ed4c1bf5e2db9ab0d0e80747',
+    symbol: '',
+  },
+]
+
 const CACHE_KEY = 'settings'
 
 const Main = styled.div`
@@ -52,6 +91,12 @@ const LinkButton = styled(Button.Anchor).attrs({
 class Settings extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      selectedTestToken: 0,
+      testTokens: TEST_TOKENS,
+    }
+
     this.correctCurrencyIfNecessary(props)
   }
   componentWillReceiveProps(nextProps) {
@@ -71,11 +116,23 @@ class Settings extends React.Component {
     const { cache, currencies } = this.props
     this.propagateSelectedCurrency(cache, currencies[index])
   }
+  handleDepositTestTokens = () => {
+    const { selectedTestToken, testTokens } = this.state
+    const tokenAddress = testTokens[selectedTestToken]
+    // TODO: add some web3 magic to 0x39a4d265db942361d92e2b0039cae73ea72a2ff9 depositor
+  }
+  handleTestTokenChange = index => {
+    this.setState({
+      selectedTestToken: index,
+    })
+  }
   propagateSelectedCurrency(cache, selectedCurrency) {
     cache.update(CACHE_KEY, settings => ({ ...settings, selectedCurrency }))
   }
   render() {
     const { currencies, daoAddr, network, selectedCurrency } = this.props
+    const { selectedTestToken, testTokens } = this.state
+    const testTokenSymbols = testTokens.map(({ symbol }) => symbol)
     return (
       <Main>
         <StyledAppBar title="Your Settings" />
@@ -113,6 +170,21 @@ class Settings extends React.Component {
                   </Field>
                 </Option>
               )}
+            <Option
+              name="Deposit Test Tokens"
+              text="Deposit a random amount of testnet tokens into your DAO to play with."
+            >
+              <Field label="Select token">
+                <DropDown
+                  active={selectedTestToken}
+                  items={testTokenSymbols}
+                  onChange={this.handleTestTokenChange}
+                />
+              </Field>
+              <Button mode="strong" onSubmit={this.handleDepositTestTokens}>
+                Give me some tokens!
+              </Button>
+            </Option>
           </Content>
         </ScrollWrapper>
       </Main>
