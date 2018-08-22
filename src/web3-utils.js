d99a23675908564de725c228614e778b2f86c9ff
juslar <justinlaroche95@gmail.com>
2018-08-22 14:45:35 -0400

    Commented functions in web3-utils

--- src/web3-utils.js
+++ src/web3-utils.js
@@ -5,27 +5,39 @@
  */
 import Web3 from 'web3'
 
-// Check address equality without checksums
+/** Check address equality without checksums
+ * @param {string} first - First address
+ * @param {string} second - Second address
+ */ 
 export function addressesEqual(first, second) {
   first = first && first.toLowerCase()
   second = second && second.toLowerCase()
   return first === second
 }
 
+/**
+ *  Takes a Wei balance and returns it rounded to the number of digits after the decimal point specified (by default 2 digits).
+ * @param {string} balance - Balance in Wei
+ * @param {Number} [digits=2] - Number of digits after the decimal point
+ */
 export function fromWeiRounded(balance, digits = 2) {
   var ethBalance = Web3.utils.fromWei(balance, 'ether')
   var decimalIndex = ethBalance.indexOf('.') + 1
   return ethBalance.substring(0, decimalIndex != -1 ? decimalIndex + digits : ethBalance.length)
 }
 
-// Shorten an Ethereum address. `charsLength` allows to change the number of
-// characters on both sides of the ellipsis.
-//
-// Examples:
-//   shortenAddress('0x19731977931271')    // 0x1973…1271
-//   shortenAddress('0x19731977931271', 2) // 0x19…71
-//   shortenAddress('0x197319')            // 0x197319 (already short enough)
-//
+/** 
+ * Shorten an Ethereum address. `charsLength` allows to change the number of
+ * characters on both sides of the ellipsis.
+ *
+ * Examples:
+ *   shortenAddress('0x19731977931271')    // 0x1973…1271
+ *   shortenAddress('0x19731977931271', 2) // 0x19…71
+ *   shortenAddress('0x197319')            // 0x197319 (already short enough)
+ * 
+ * @param {string} address - The address to shorten
+ * @param {number }[charsLength=4] - The number of characters to change on both sides of the ellipsis
+ */
 export function shortenAddress(address, charsLength = 4) {
   const prefixLength = 2 // "0x"
   if (address.length < charsLength * 2 + prefixLength) {
@@ -38,7 +50,9 @@ export function shortenAddress(address, charsLength = 4) {
   )
 }
 
-// Cache web3 instances used in the app
+/** Cache web3 instances used in the app
+ * @param provider
+ */
 const cache = new WeakMap()
 export function getWeb3(provider) {
   if (cache.has(provider)) {
