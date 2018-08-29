28f63b4816c708d53508ec28a9b08109f123c849
Pierre Bertet <hello@pierre.world>
2018-08-29 23:23:55 +0100

    Add RemoteIcon / RemoteImage (#337)

---
+++ src/components/RemoteImage.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { noop } from '../utils'
+
+// Tries to load an image, while displaying an alternative.
+// Use `children` or `render` to render its children.
+class RemoteImage extends React.Component {
+  static defaultProps = {
+    src: '',
+    render: null, // render is an alias of children and takes priority
+    children: noop,
+  }
+
+  state = { exists: false }
+
+  componentDidMount() {
+    this.startLoading()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.src !== this.props.src) {
+      this.startLoading()
+    }
+  }
+
+  componentWillUnmount() {
+    this.stopLoading()
+  }
+
+  startLoading() {
+    const { src } = this.props
+    this.stopLoading()
+    this.setState(() => {
+      this._img = new Image()
+      this._img.addEventListener('load', this.handleLoad)
+      this._img.src = src
+      return { exists: false }
+    })
+  }
+
+  stopLoading() {
+    if (this._img) {
+      this._img.removeEventListener('load', this.handleLoad)
+      this._img = null
+    }
+  }
+
+  handleLoad = () => {
+    this.stopLoading() // no need to keep the image around
+    this.setState({ exists: true })
+  }
+
+  render() {
+    const { exists } = this.state
+    const { src, render, children } = this.props
+    return (render || children)({ exists })
+  }
+}
+
+export default RemoteImage
