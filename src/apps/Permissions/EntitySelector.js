bd95f9cb5ad32dd9841fdcf993440a0c46a835b9
Pierre Bertet <hello@pierre.world>
2018-09-07 16:52:53 +0200

    Add EntitySelector

---
+++ src/apps/Permissions/EntitySelector.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import styled from 'styled-components'
+import { DropDown, Field, TextInput } from '@aragon/ui'
+import { getAnyAddress, getEmptyAddress } from '../../aragonos-utils'
+import AppInstanceLabel from './AppInstanceLabel'
+
+class EntitySelector extends React.Component {
+  static defaultProps = {
+    apps: [],
+    activeIndex: 0,
+    label: 'Entity',
+    labelCustomAddress: 'Entity address',
+  }
+  state = {
+    customAddress: '',
+  }
+  handleChange = index => {
+    this.setState({ customAddress: '' }, () => {
+      this.props.onChange({
+        index,
+        address: this.getAddress(index),
+      })
+    })
+  }
+  handleCustomAddressChange = event => {
+    this.setState({ customAddress: event.target.value })
+    this.props.onChange({
+      index: this.getItems().length - 1,
+      address: event.target.value,
+    })
+  }
+  getApps() {
+    const { apps } = this.props
+    return apps.filter(app => Boolean(app.name))
+  }
+  getAppsItems() {
+    return this.getApps().map(app => (
+      <AppInstanceLabel app={app} proxyAddress={app.proxyAddress} />
+    ))
+  }
+  getAddress(index) {
+    if (index === 0) {
+      return getEmptyAddress()
+    }
+
+    const items = this.getItems()
+
+    if (index === items.length - 1) {
+      return this.state.customAddress
+    }
+
+    if (index === items.length - 2) {
+      return getAnyAddress()
+    }
+
+    const app = this.getApps()[index - 1]
+    return (app && app.proxyAddress) || getEmptyAddress()
+  }
+  getItems() {
+    return [
+      'Select an entity',
+      ...this.getAppsItems(),
+      'Any account',
+      'Custom address…',
+    ]
+  }
+  render() {
+    const { customAddress } = this.state
+    const { activeIndex, label, labelCustomAddress } = this.props
+    const items = this.getItems()
+    const showAssignAddress = activeIndex === items.length - 1
+    return (
+      <React.Fragment>
+        <Field label={label}>
+          <DropDown
+            items={items}
+            active={activeIndex}
+            onChange={this.handleChange}
+            wide
+          />
+        </Field>
+
+        {showAssignAddress && (
+          <Field label="Assign role to address">
+            <TextInput
+              placeholder="0xcafe…"
+              value={customAddress}
+              onChange={this.handleCustomAddressChange}
+              wide
+            />
+          </Field>
+        )}
+      </React.Fragment>
+    )
+  }
+}
+
+export default EntitySelector
