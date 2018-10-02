1c25798c27b1fe186c9e3dd57f91dfbdeb963aa4
Delfina <delfinapolito@gmail.com>
2018-10-02 14:34:34 +0200

    Not found component

---
+++ src/components/NotFound/EagleAnimation.js
@@ -0,0 +1,141 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+export default class EagleAnimation extends React.Component {
+  componentDidMount() {
+    let Script
+    Script = document.createElement('script')
+    (Script.type = 'text/javascript'), (Script.async = !0)
+    Script.id = 'tweenMaxScript'
+    Script.src =
+      'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/TweenMax.min.js'
+    document.getElementById('tweenMaxScript').appendChild(Script)
+
+    window.addEventListener('mousemove', onMouseMove)
+  }
+
+  render() {
+    return (
+      <SvgWrapper>
+        <div id="tweenMaxScript" />
+        <svg id="svg" viewBox="0 0 300 300">
+          <defs>
+            <linearGradient x1="100%" y1="0%" x2="24%" y2="100%" id="a">
+              <stop stopColor="#00EAE5" offset="0%" />
+              <stop stopColor="#02B5E4" offset="100%" />
+            </linearGradient>
+            <linearGradient x1="100%" y1="0%" x2="-3%" y2="100%" id="b">
+              <stop stopColor="#4DF4F0" offset="0%" />
+              <stop stopColor="#1AA8CD" offset="100%" />
+            </linearGradient>
+          </defs>
+          <g fill="none" fillRule="evenodd">
+            <path
+              d="M0 51v76c0 17 14 30 30 30h22V21H27c-8 0-15-2-20-8L0 1v50z"
+              fill="url(#a)"
+            />
+
+            <path d="M52 96L42 86s1-6 10-6v16z" fill="#F5C268" />
+            <path
+              d="M73 157c17 0 30-13 30-30V1s-2 7-7 12c-4 6-12 8-20 8H52v136"
+              fill="url(#b)"
+            />
+
+            <path d="M52 96l10-10s-1-6-10-6v16z" fill="#D89144" />
+          </g>
+
+          <g id="left-eye">
+            <circle className="eye-outer" cx="30" cy="55" r="20" />
+            <circle className="eye-inner" cx="32" cy="65" r="6" />
+          </g>
+
+          <g id="right-eye">
+            <circle className="eye-outer" cx="76" cy="55" r="20" />
+            <circle className="eye-inner" cx="82" cy="65" r="6" />
+          </g>
+        </svg>
+      </SvgWrapper>
+    )
+  }
+}
+
+const SvgWrapper = styled.div`
+  width: 150px;
+  margin-left: 35px;
+  top: 0;
+  #svg {
+    width: 140px;
+    height: 140px;
+    .eye-outer {
+      fill: #fff;
+    }
+
+    .eye-inner {
+      fill: #000;
+    }
+
+    #container {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+    }
+
+    #box {
+      z-index: 1;
+      width: 630px;
+      height: 400px;
+      background: #ffffff;
+      box-shadow: 0 0 19px 0 rgba(201, 201, 201, 0.61);
+      border-radius: 4px;
+    }
+  }
+`
+
+function onMouseMove(event) {
+  const svg = document.querySelector('#svg')
+
+  const mouse = svg.createSVGPoint()
+  const leftEye = createEye('#left-eye')
+  const rightEye = createEye('#right-eye')
+
+  mouse.x = event.clientX
+  mouse.y = event.clientY
+  const requestId = requestAnimationFrame(function() {
+    const point = mouse.matrixTransform(svg.getScreenCTM().inverse())
+    leftEye.rotateTo(point)
+    rightEye.rotateTo(point)
+    const requestId = null
+  })
+}
+
+function createEye(selector) {
+  const element = document.querySelector(selector)
+
+  TweenLite.set(element, {
+    transformOrigin: 'center',
+  })
+
+  const bbox = element.getBBox()
+  const centerX = bbox.x + bbox.width / 2
+  const centerY = bbox.y + bbox.height / 2
+
+  function rotateTo(point) {
+    const dx = point.x - centerX
+    const dy = point.y - centerY
+
+    const angle = Math.atan2(dy, dx)
+
+    TweenLite.to(element, 0.3, {
+      rotation: angle + '_rad_short',
+    })
+  }
+
+  return {
+    element: element,
+    rotateTo: rotateTo,
+  }
+}
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,152 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import EagleAnimation from './EagleAnimation'
+import { theme, breakpoint, Button } from '@aragon/ui'
+const medium = css => breakpoint('medium', css)
+
+class NotFound extends React.Component {
+  state = { details: false }
+  clickMoreDetails = () => {
+    const details = this.state.details
+    this.setState({ details: !details })
+  }
+  render() {
+    const {
+      title,
+      issue,
+      reload,
+      detailsTitle,
+      detailsContent,
+      children,
+      ...props
+    } = this.props
+    const { details } = this.state
+    return (
+      <div>
+        <EagleAnimation />
+        <Card>
+          <h1>{title}</h1>
+          {children}
+          {(detailsContent || detailsTitle) && (
+            <div>
+              <DetailsButton onClick={this.clickMoreDetails}>
+                {details ? 'Hide details...' : 'More details...'}
+              </DetailsButton>
+              {details && (
+                <DetailsContainer>
+                  {detailsTitle && <h2>{detailsTitle}</h2>}
+                  {detailsContent}
+                </DetailsContainer>
+              )}
+            </div>
+          )}
+          {(issue || reload) && (
+            <ButtonBox>
+              {issue && (
+                <IssueLink>
+                  <Button.Anchor
+                    mode="text"
+                    href="https://github.com/aragon/aragon/issues"
+                    target="_blank"
+                  >
+                    Tell us what went wrong
+                  </Button.Anchor>
+                </IssueLink>
+              )}
+              {reload && (
+                <Button mode="strong" onClick={() => location.reload()}>
+                  Reload
+                </Button>
+              )}
+            </ButtonBox>
+          )}
+        </Card>
+      </div>
+    )
+  }
+}
+
+const Card = styled.div`
+  background: white;
+  -webkit-box-shadow: 0px 0px 19px 0px #c9c9c9;
+  -moz-box-shadow: 0px 0px 19px 0px #c9c9c9;
+  box-shadow: 0px 0px 19px 0px #c9c9c9;
+  padding: 30px;
+  border: solid 1px #c9c9c9;
+  border-radius: 4px;
+  max-width: 550px;
+  color: ${theme.textSecondary};
+  margin: auto 15px;
+  margin-top: -90px;
+  z-index: 2;
+  position: relative;
+  a {
+    cursor: pointer;
+    color: ${theme.accent};
+  }
+  a:hover,
+  a:active {
+    color: ${theme.gradientStartActive};
+  }
+  h1 {
+    color: ${theme.textDimmed};
+    font-size: 20px;
+    margin-bottom: 20px;
+    ${medium('font-size: 30px;')};
+  }
+`
+
+const DetailsButton = styled.p`
+  color: black;
+  text-decoration: underline;
+  margin: 20px 0;
+  cursor: pointer;
+`
+
+const DetailsContainer = styled.div`
+  background: #f6f6f6;
+  padding: 15px;
+  border-radius: 4px;
+  max-height: 200px;
+  overflow-y: scroll;
+  color: ${theme.text};
+  font-size: 14px;
+  line-height: 1.6;
+  h2 {
+    font-weight: bold;
+    font-size: 16px;
+    margin-bottom: 10px;
+  }
+`
+
+const ButtonBox = styled.div`
+  margin: 20px 0 0 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .issue-link {
+    margin-left: -10px;
+    color: inherit;
+    text-decoration: none;
+  }
+`
+
+const IssueLink = styled.div`
+  a {
+    margin-left: -10px;
+    color: inherit;
+    text-decoration: none;
+  }
+`
+
+NotFound.propTypes = {
+  title: PropTypes.string,
+  className: PropTypes.string,
+}
+
+NotFound.defaultProps = {
+  title: 'Not found :(',
+}
+
+export default NotFound
