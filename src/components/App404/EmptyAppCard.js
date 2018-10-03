3964240f0386956bf53f3da8262a54300bbd0f08
Delfina <delfinapolito@gmail.com>
2018-10-03 09:18:27 +0200

    Fix on coding standard

--- src/components/App404/EmptyAppCard.js
+++ src/components/App404/EmptyAppCard.js
@@ -2,18 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 import { EmptyStateCard } from '@aragon/ui'
 import notFoundIcon from './assets/not-found.svg'
-import NotFound from '../NotFound/NotFoundCard'
-
-// <StyledEmptyStateCard
-//   actionText="Go back"
-//   icon={NotFoundIcon}
-//   onActivate={onActivate}
-//   text="Are you trying to access an Aragon app that is not installed?"
-//   title="Error: unknown app."
-// />
 
 const EmptyAppCard = ({ onActivate }) => (
-  <NotFound detailsContent="fdkdh"></NotFound>
+  <StyledEmptyStateCard
+    actionText="Go back"
+    icon={NotFoundIcon}
+    onActivate={onActivate}
+    text="Are you trying to access an Aragon app that is not installed?"
+    title="Error: unknown app."
+  />
 )
 
 const StyledEmptyStateCard = styled(EmptyStateCard)`
