69985a1e4ceabf224b4d22d0a53acacdd2cb4c9c
Gorka <gorka@aquigorka.com>
2018-10-03 14:49:37 +0200

    Modal: add modal component

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Button, Text, breakpoint } from '@aragon/ui'
+
+const WrapModal = styled.div`
+  height: calc(100vh - 2em);
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  background: #fff;
+  padding: 2.5em 2.5em 1.5em 2.5em;
+  margin: 1em;
+  box-shadow: -2px 0px 20px rgba(0, 0, 0, 0.236894);
+  border-radius: 4px;
+
+  /* desktop */
+  ${breakpoint(
+    'medium',
+    `
+    height: unset;
+    min-height: 40vh;
+    max-height: 70vh;
+    max-width: 90%;
+    padding: 3.5em;
+    margin: auto;
+  `
+  )} ${breakpoint(
+    'large',
+    `
+    max-width: 70%;
+  `
+  )};
+`
+
+const Header = styled.header`
+  margin-bottom: 1.5em;
+`
+
+const Main = styled.main`
+  margin-bottom: 30px;
+  word-break: break-word;
+  overflow: auto;
+`
+
+const Footer = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const Modal = ({ title, body, hideText, moreText, onHide, onMore }) => {
+  return (
+    <WrapModal role="alertdialog">
+      <Header>
+        <Text size="xxlarge">{title}</Text>
+      </Header>
+      <Main>{body}</Main>
+      <Footer>
+        <Button mode="text" onClick={onHide}>
+          {hideText}
+        </Button>
+        <Button mode="strong" onClick={onMore}>
+          {moreText}
+        </Button>
+      </Footer>
+    </WrapModal>
+  )
+}
+
+export default Modal
