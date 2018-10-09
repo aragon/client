059c73ee97d322987461ccb01bedeadeaaf04596
Adam Soltys <asoltys@gmail.com>
2018-10-09 10:43:24 -0700

    Allow aragon apps to request camera and microphone access

--- src/components/App/AppIFrame.js
+++ src/components/App/AppIFrame.js
@@ -191,6 +191,7 @@ class AppIFrame extends React.Component {
         {progressBar}
         <StyledIFrame
           name="AppIFrame"
+          allow="camera; microphone"
           frameBorder="0"
           onLoad={this.handleOnLoad}
           innerRef={this.handleIFrameRef}
