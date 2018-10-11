b0567960a4d3ba880241e11555a43257baba6d1e
Pierre Bertet <hello@pierre.world>
2018-10-12 00:49:22 +0100

    Banner: allow the button to be injected as a prop

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,27 +1,35 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Button, Text } from '@aragon/ui'
+import { noop } from '../../utils'
 
-const Banner = ({ text, textColor, buttonText, onClick, color }) => (
-  <WrapBanner color={color}>
-    <Text weight="bold" color={textColor}>
-      {text}
-    </Text>
-    <Button compact onClick={onClick}>
-      <Text weight="bold">{buttonText}</Text>
-    </Button>
-  </WrapBanner>
-)
+class Banner extends React.Component {
+  static defaultProps = {
+    onClick: noop,
+    border: '0',
+  }
+  render() {
+    const { text, textColor, button, buttonText, onClick, color } = this.props
+    return (
+      <WrapBanner color={color}>
+        <Text color={textColor}>{text}</Text>
+        {button || (
+          <Button onClick={onClick} mode="strong" size="mini">
+            {buttonText}
+          </Button>
+        )}
+      </WrapBanner>
+    )
+  }
+}
 
 const WrapBanner = styled.div`
-  position: relative;
-  z-index: 10;
   display: grid;
   grid-template-columns: auto auto;
   grid-gap: 1em;
   align-items: center;
   justify-content: center;
-  padding: 6px 1em;
+  padding: 6px 10px;
   background-color: ${({ color }) => color};
 `
 
