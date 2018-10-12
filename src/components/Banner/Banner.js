cc656cbc8849606253b713877dd9c8e2eeb35c4f
Pierre Bertet <hello@pierre.world>
2018-10-12 12:04:39 +0100

    Banner: remove buttonText / onClick until needed

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,23 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Button, Text } from '@aragon/ui'
+import { Text } from '@aragon/ui'
 import { noop } from '../../utils'
 
 class Banner extends React.Component {
-  static defaultProps = {
-    onClick: noop,
-    border: '0',
-  }
   render() {
-    const { text, textColor, button, buttonText, onClick, color } = this.props
+    const { text, textColor, button, color } = this.props
     return (
       <WrapBanner color={color}>
         <Text color={textColor}>{text}</Text>
-        {button || (
-          <Button onClick={onClick} mode="strong" size="mini">
-            {buttonText}
-          </Button>
-        )}
+        <div>{button}</div>
       </WrapBanner>
     )
   }
