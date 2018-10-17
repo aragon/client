d7bd97c0c8f924de5966ca2c6eceeade287569ed
Pierre Bertet <hello@pierre.world>
2018-10-17 02:38:58 +0100

    Linting fix

--- src/GlobalErrorHandler.js
+++ src/GlobalErrorHandler.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import { SafeLink, Text, Info } from '@aragon/ui'
 import ErrorCard from './components/ErrorCard/ErrorCard'
 
 class GlobalErrorHandler extends React.Component {
