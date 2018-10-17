af61e90138fc29c1d772593c99eebd15b466cc1e
Brett Sun <qisheng.brett.sun@gmail.com>
2018-10-17 20:46:40 +0200

    Purge localstorage if user's last session was not on the current version (#401)
    Rudimentary cache busting based on Aragon Core versions.
    
    Let's assume for now that we only make breaking changes to the cache structure on minor version increments (for our own apps). Ideally this would be done in each app instead.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,21 @@ import { PublicUrl, BaseStyles } from '@aragon/ui'
 import GlobalErrorHandler from './GlobalErrorHandler'
 import App from './App'
 
+const PACKAGE_VERSION = process.env.REACT_APP_PACKAGE_VERSION || ''
+const PACKAGE_VERSION_KEY = 'PACKAGE_VERSION_KEY'
+
+// Purge localstorage cache when upgrading between different minor versions
+const lastAppVersion = window.localStorage.getItem(PACKAGE_VERSION_KEY) || ''
+const [lastMajorVersion, lastMinorVersion] = lastAppVersion.split('.')
+const [currentMajorVersion, currentMinorVersion] = PACKAGE_VERSION.split('.')
+if (
+  lastMajorVersion !== currentMajorVersion ||
+  lastMinorVersion !== currentMinorVersion
+) {
+  window.localStorage.clear()
+  window.localStorage.setItem(PACKAGE_VERSION_KEY, PACKAGE_VERSION)
+}
+
 ReactDOM.render(
   <PublicUrl.Provider url="./aragon-ui/">
     <BaseStyles />
