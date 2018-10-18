91cec406c56ed83beec768fb17b48a0777212611
Brett Sun <qisheng.brett.sun@gmail.com>
2018-10-18 18:38:05 +0200

    Fix JSDoc style and add linting rule

--- src/web3-utils.js
+++ src/web3-utils.js
@@ -8,12 +8,12 @@ import Web3 from 'web3'
 const ANY_ADDRESS = '0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF'
 const EMPTY_ADDRESS = '0x0000000000000000000000000000000000000000'
 
-/** 
+/**
  * Check address equality without checksums
- * @param {string} first - First address
- * @param {string} second - Second address
- * @returns {boolean} - Address equality
- */ 
+ * @param {string} first First address
+ * @param {string} second Second address
+ * @returns {boolean} Address equality
+ */
 export function addressesEqual(first, second) {
   first = first && first.toLowerCase()
   second = second && second.toLowerCase()
@@ -21,24 +21,6 @@ export function addressesEqual(first, second) {
 }
 
 /**
- *  Takes a Wei value and returns it converted to the specified Ether subunit rounded to the specified number of digits after the decimal point.
- * @param {string} wei - Value in Wei
- * @param {number} [digits=2] - Number of digits after the decimal point (by default 2)
- * @param {string} [unit='ether'] - Unit to convert to (by default 'ether')
- * @return {string} - The converted Wei value to the specified Ether subunit and rounded to the specified digits after the decimal point
- */
-export function fromWeiRounded(wei, digits = 2, unit = 'ether') {
-  let unitValue = Web3.utils.fromWei(wei, unit)
-  const decimalIndex = unitValue.indexOf('.')
-  if(decimalIndex != -1) {
-    unitValue = unitValue.substring(0, decimalIndex + digits + 2)
-    const roundedLastDigit = unitValue[unitValue.length-2].concat('.' + unitValue[unitValue.length -1])
-    unitValue = unitValue.substring(0, unitValue.length - 2).concat(Math.round(roundedLastDigit))
-  }
-  return unitValue
-}
-
-/** 
  * Shorten an Ethereum address. `charsLength` allows to change the number of
  * characters on both sides of the ellipsis.
  *
@@ -46,9 +28,9 @@ export function fromWeiRounded(wei, digits = 2, unit = 'ether') {
  *   shortenAddress('0x19731977931271')    // 0x1973…1271
  *   shortenAddress('0x19731977931271', 2) // 0x19…71
  *   shortenAddress('0x197319')            // 0x197319 (already short enough)
- * 
- * @param {string} address - The address to shorten
- * @param {number}[charsLength=4] - The number of characters to change on both sides of the ellipsis
+ *
+ * @param {string} address The address to shorten
+ * @param {number} [charsLength=4] The number of characters to change on both sides of the ellipsis
  * @returns {string} The shortened address
  */
 export function shortenAddress(address, charsLength = 4) {
@@ -66,18 +48,19 @@ export function shortenAddress(address, charsLength = 4) {
   )
 }
 
-/** 
+const web3Cache = new WeakMap()
+
+/**
  * Cache web3 instances used in the app
- * @param provider
- * @returns The web3 instance
+ * @param {Web3.Provider} provider Web3 provider
+ * @returns {Web3} The web3 instance
  */
-const cache = new WeakMap()
 export function getWeb3(provider) {
-  if (cache.has(provider)) {
-    return cache.get(provider)
+  if (web3Cache.has(provider)) {
+    return web3Cache.get(provider)
   }
   const web3 = new Web3(provider)
-  cache.set(provider, web3)
+  web3Cache.set(provider, web3)
   return web3
 }
 
