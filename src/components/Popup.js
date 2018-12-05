0cfcb5b3cf984c7560fe33f66b60d922c79f45b7
Pierre Bertet <hello@pierre.world>
2018-12-05 07:43:49 +0100

    Add a Popup component
    Implements:
    
    - The default popup style (using Card).
    - Focus: move it to the popup when opened, then back to the previously
      focused element (usually the opener) when closed.
    - Closing events: click outside (using the focusout event) and <ESC>.
    - The transition animation.

---
+++ src/components/Popup.js
@@ -0,0 +1,139 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { Transition, animated } from 'react-spring'
+import { Button, Card, theme } from '@aragon/ui'
+import { noop } from '../utils'
+import springs from '../springs'
+
+export const TRANSITION_SCALE_DIFF = 0.04
+export const TRANSITION_SPRING = { ...springs.swift, precision: 0.1 }
+
+class Popup extends React.Component {
+  static propTypes = {
+    onRequestClose: PropTypes.func,
+    visible: PropTypes.bool,
+  }
+  static defaultProps = {
+    onRequestClose: noop,
+    visible: true,
+  }
+
+  state = { previouslyFocusedElement: null }
+
+  _element = React.createRef()
+
+  _closeRequested = false
+
+  componentDidMount() {
+    this.attachEvents()
+
+    this.setState({ previouslyFocusedElement: document.activeElement })
+    this._element.current.focus()
+  }
+
+  componentWillUnmount() {
+    this.removeEvents()
+  }
+
+  attachEvents() {
+    this._element.current.addEventListener('focusout', this.handleFocusout)
+    document.addEventListener('keydown', this.handleKeydown)
+  }
+
+  removeEvents() {
+    this._element.current.removeEventListener('focusout', this.handleFocusout)
+    document.removeEventListener('keydown', this.handleKeydown)
+  }
+
+  close() {
+    if (this._closeRequested) {
+      return
+    }
+
+    const { onRequestClose } = this.props
+    const { previouslyFocusedElement } = this.state
+    if (previouslyFocusedElement) {
+      previouslyFocusedElement.focus()
+    }
+
+    // We can’t reliably know if the element previously focused can actually
+    // receive focus or if it was implicitly focused. That’s why
+    // onRequestClose() is called even if `previouslyFocusedElement.focus()`
+    // should call it already, via `this.handleFocusout`.
+    // The `_closeRequested` flag prevents `onRequestClose` to be called twice.
+    onRequestClose()
+    this._closeRequested = true
+  }
+
+  handleKeydown = event => {
+    if (event.keyCode === 27) {
+      this.close()
+    }
+  }
+
+  handleFocusout = event => {
+    const popupElement = this._element.current
+    const focusedElement = event.relatedTarget
+    if (!popupElement.contains(focusedElement)) {
+      this.close()
+    }
+  }
+
+  render() {
+    const { children } = this.props
+    return (
+      <Main tabIndex="0" innerRef={this._element}>
+        {children}
+      </Main>
+    )
+  }
+}
+
+const Main = styled(Card)`
+  display: flex;
+  width: auto;
+  height: auto;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.03);
+  padding: 10px 0;
+  &:focus {
+    outline: 0;
+  }
+`
+
+const PopupWrapper = styled(animated.div)`
+  position: absolute;
+  z-index: 3;
+  top: 10px;
+  left: 10px;
+`
+
+export default props => (
+  <Transition
+    items={props.visible}
+    from={{ visibleProgress: 0 }}
+    enter={{ visibleProgress: 1 }}
+    leave={{ visibleProgress: 0 }}
+    config={TRANSITION_SPRING}
+    native
+  >
+    {visible =>
+      visible &&
+      (({ visibleProgress }) => (
+        <PopupWrapper
+          style={{
+            opacity: visibleProgress,
+            transform: visibleProgress.interpolate(
+              v =>
+                `scale(
+                  ${1 - TRANSITION_SCALE_DIFF + TRANSITION_SCALE_DIFF * v}
+                )`
+            ),
+          }}
+        >
+          <Popup {...props} />
+        </PopupWrapper>
+      ))
+    }
+  </Transition>
+)
