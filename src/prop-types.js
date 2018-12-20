b968b752ea755ada1239f4a2b36fa3638ec07829
Beer van der Drift <beerdrift@gmail.com>
2018-12-20 22:47:40 +0100

    chore: created ethereum address proptype

--- src/prop-types.js
+++ src/prop-types.js
@@ -1,5 +1,47 @@
 import PropTypes from 'prop-types'
 
+const validatorCreator = nonRequiredFunction => {
+  const validator = nonRequiredFunction
+
+  validator.isRequired = (props, propName, componentName) => {
+    const value = props[propName]
+
+    if (value === null || value === undefined || value === '') {
+      return new Error(
+        `Property ${propName} is required on ${componentName}, but ${value} was given.`
+      )
+    }
+
+    return nonRequiredFunction(props, propName, componentName)
+  }
+
+  return validator
+}
+
+const ethereumAddressValidator = (props, propName, componentName) => {
+  const value = props[propName]
+
+  if (value === null || value === undefined || value === '') {
+    return null
+  }
+
+  if (!/^0x[a-fA-F0-9]{4}$/.test(value)) {
+    const valueType = typeof value
+    let nonAddress = null
+
+    if (valueType !== 'object') {
+      nonAddress = value.toString()
+    }
+
+    return new Error(
+      `Invalid prop ${propName} supplied to ${componentName}. The provided value is not a valid ethereum address.${nonAddress &&
+        ` You provided "${nonAddress}"`}`
+    )
+  }
+}
+
+export const EthereumAddress = validatorCreator(ethereumAddressValidator)
+
 export const FavoriteDaoType = PropTypes.shape({
   name: PropTypes.string,
   address: PropTypes.string,
