ea00496d66bacad81fe75ef4920cd2f3182078f3
rodrigo <rodrigopi1989@gmail.com>
2019-01-10 20:50:10 -0300

    remove makeEtherscanUrl

--- src/components/Etherscan/EtherscanLink.js
+++ src/components/Etherscan/EtherscanLink.js
@@ -1,15 +1,18 @@
 import PropTypes from 'prop-types'
 import { network } from '../../environment'
+import { blockExplorerUrl } from '@aragon/ui'
 
-const { etherscanBaseUrl } = network
+const  networkType  = network.type
 
 // Render props component that injects an appropriate Etherscan url if possible
-const EtherscanLink = ({ address, children }) =>
-  children(
-    typeof etherscanBaseUrl === 'string'
-      ? `${etherscanBaseUrl}/address/${address}`
+const EtherscanLink = ({ address, children }) =>{
+  const etherscanUrl = blockExplorerUrl('address', address, { networkType })
+  return children(
+    typeof etherscanUrl === 'string'
+      ? etherscanUrl
       : null
   )
+}
 EtherscanLink.propTypes = {
   children: PropTypes.func.isRequired,
 }
