34574d4caf5ac65f0ec077f98567afed0fa78b11
rodrigo <rodrigopi1989@gmail.com>
2019-01-15 20:37:09 -0300

    pr fix

--- src/components/Etherscan/EtherscanLink.js
+++ src/components/Etherscan/EtherscanLink.js
@@ -2,11 +2,9 @@ import PropTypes from 'prop-types'
 import { network } from '../../environment'
 import { blockExplorerUrl } from '@aragon/ui'
 
-const networkType = network.type
-
 // Render props component that injects an appropriate Etherscan url if possible
 const EtherscanLink = ({ address, children }) => {
-  const etherscanUrl = blockExplorerUrl('address', address, { networkType })
+  const etherscanUrl = blockExplorerUrl('address', address, { networkType: network.type })
   return children(typeof etherscanUrl === 'string' ? etherscanUrl : null)
 }
 EtherscanLink.propTypes = {
