0176356b9cfccd7dcc85e3e40e8a2d51c3c169b7
rodrigo <rodrigopi1989@gmail.com>
2019-01-15 21:05:14 -0300

    lint fix

--- src/components/Etherscan/EtherscanLink.js
+++ src/components/Etherscan/EtherscanLink.js
@@ -4,7 +4,9 @@ import { blockExplorerUrl } from '@aragon/ui'
 
 // Render props component that injects an appropriate Etherscan url if possible
 const EtherscanLink = ({ address, children }) => {
-  const etherscanUrl = blockExplorerUrl('address', address, { networkType: network.type })
+  const etherscanUrl = blockExplorerUrl('address', address, {
+    networkType: network.type,
+  })
   return children(typeof etherscanUrl === 'string' ? etherscanUrl : null)
 }
 EtherscanLink.propTypes = {
