4a3ae0b509f6a636e2f0a3b9bdd569a91a8993fb
Gorka <gorka@aquigorka.com>
2019-01-22 13:31:41 +0100

    EmptyBlock: add responsive behaviour

--- src/apps/Permissions/EmptyBlock.js
+++ src/apps/Permissions/EmptyBlock.js
@@ -1,12 +1,21 @@
 import styled from 'styled-components'
-import { Card } from '@aragon/ui'
+import { Card, breakpoint } from '@aragon/ui'
 
 const EmptyBlock = styled(Card)`
+  margin: 30px;
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 100%;
+  width: auto;
   height: 180px;
+
+  ${breakpoint(
+    'medium',
+    `
+      margin: 0;
+      width: 100%;
+    `
+  )}
 `
 
 export default EmptyBlock
