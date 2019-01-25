974a8429720f84d71146e476558f98520ca360cc
Rodrigo Perez <rodrigopi1989@gmail.com>
2019-01-25 10:58:10 -0300

    fix: Use blockExplorerUrl() instead of makeEtherscanBaseUrl() (#560)

--- src/utils.js
+++ src/utils.js
@@ -20,18 +20,6 @@ export function appIconUrl(app) {
     : null
 }
 
-export function makeEtherscanBaseUrl(network) {
-  // Don't make etherscan urls if the network isn't one that etherscan supports
-  if (
-    network === 'main' ||
-    network === 'kovan' ||
-    network === 'rinkeby' ||
-    network === 'ropsten'
-  ) {
-    return `https://${network === 'main' ? '' : `${network}.`}etherscan.io`
-  }
-}
-
 export function noop() {}
 
 export function removeStartingSlash(str) {
