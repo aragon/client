494c8e6d6e28dc241f5cc328e9cf9f28bf3b4916
Gorka <gorka@aquigorka.com>
2019-02-20 09:09:35 +0100

    AppLayout: refactor api

--- src/components/AppLayout/AppLayout.js
+++ src/components/AppLayout/AppLayout.js
@@ -1,47 +1,110 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { AppBar } from '@aragon/ui'
+import { AppBar, AppView, Button, ButtonIcon, Viewport, font } from '@aragon/ui'
+import MenuButton from '../MenuPanel/MenuButton'
 
-class AppLayout extends React.Component {
-  static defaultProps = {
-    title: '',
-  }
-  render() {
-    const { title, endContent, children, padding, maxWidth } = this.props
-    return (
-      <Main>
-        <StyledAppBar title={title} endContent={endContent} />
-        <ScrollWrapper>
-          <Content maxWidth={maxWidth} padding={padding}>
-            {children}
-          </Content>
-        </ScrollWrapper>
-      </Main>
-    )
-  }
+const AppLayout = ({
+  children,
+  title,
+  afterTitle,
+  smallViewPadding,
+  largeViewPadding,
+  onMenuOpen,
+  mainButton,
+}) => {
+  return (
+    <Viewport>
+      {({ below }) => (
+        <AppView
+          padding={below('medium') ? smallViewPadding : largeViewPadding}
+          appBar={
+            <AppBar>
+              <AppBarContainer
+                style={{ padding: below('medium') ? '0' : '0 30px' }}
+              >
+                <Title>
+                  {below('medium') && (
+                    <MenuButton onClick={onMenuOpen} label="Menu" />
+                  )}
+                  <TitleLabel>{title}</TitleLabel>
+                  {afterTitle}
+                </Title>
+                {mainButton &&
+                  (mainButton.button ||
+                    (below('medium') ? (
+                      <ButtonIcon
+                        onClick={mainButton.onClick}
+                        title={mainButton.label}
+                        css={`
+                          width: auto;
+                          height: 100%;
+                          padding: 0 20px 0 10px;
+                          margin-left: 8px;
+                        `}
+                      >
+                        {mainButton.icon}
+                      </ButtonIcon>
+                    ) : (
+                      <Button mode="strong" onClick={mainButton.onClick}>
+                        {mainButton.label}
+                      </Button>
+                    )))}
+              </AppBarContainer>
+            </AppBar>
+          }
+        >
+          {children}
+        </AppView>
+      )}
+    </Viewport>
+  )
 }
 
-const Main = styled.div`
+AppLayout.defaultProps = {
+  smallViewPadding: 20,
+  largeViewPadding: 30,
+}
+
+AppLayout.propTypes = {
+  afterTitle: PropTypes.node,
+  children: PropTypes.node,
+  largeViewPadding: PropTypes.number,
+  mainButton: PropTypes.shape({
+    button: PropTypes.node,
+    icon: PropTypes.node,
+    label: PropTypes.node,
+    onClick: PropTypes.func,
+  }),
+  onMenuOpen: PropTypes.func.isRequired,
+  smallViewPadding: PropTypes.number,
+  title: PropTypes.node.isRequired,
+}
+
+const Title = styled.h1`
   display: flex;
+  flex: 1 1 auto;
+  width: 0;
+  align-items: center;
   height: 100%;
-  flex-direction: column;
-  align-items: stretch;
-  justify-content: stretch;
 `
 
-const StyledAppBar = styled(AppBar)`
-  flex-shrink: 0;
+const TitleLabel = styled.span`
+  flex: 0 1 auto;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  margin-right: 10px;
+  ${font({ size: 'xxlarge' })};
 `
 
-const ScrollWrapper = styled.div`
-  height: 100%;
-  overflow: auto;
-`
-
-const Content = styled.div`
+const AppBarContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  min-height: 100%;
+  width: 100%;
+  height: 100%;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: nowrap;
 `
 
 export default AppLayout
