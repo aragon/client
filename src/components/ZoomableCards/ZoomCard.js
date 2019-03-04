24997a516f49395185515df10c58209d14d5c08f
Pierre Bertet <hello@pierre.world>
2019-03-04 23:10:39 +0100

    ZoomCard: fix the cards width

--- src/components/ZoomableCards/ZoomCard.js
+++ src/components/ZoomableCards/ZoomCard.js
@@ -27,6 +27,7 @@ class ZoomCard extends React.PureComponent {
         css={`
           display: flex;
           flex-direction: column;
+          width: 100%;
           height: 100%;
           padding: 20px 30px;
           background: #ffffff;
