174313018c68a0fb02ab8dcce2d9104fb96a7681
Pierre Bertet <hello@pierre.world>
2019-04-05 18:13:53 +0200

    Use ButtonBase

--- src/components/MenuPanel/OrganizationSwitcher/ItemButton.js
+++ src/components/MenuPanel/OrganizationSwitcher/ItemButton.js
@@ -1,36 +1,15 @@
 import React from 'react'
-import { theme } from '@aragon/ui'
-import FocusVisible from '../../FocusVisible'
+import { ButtonBase } from '@aragon/ui'
 
 const ItemButton = props => (
-  <FocusVisible>
-    {({ focusVisible, onFocus }) => (
-      <button
-        onFocus={onFocus}
-        css={`
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-          height: 44px;
-          padding: 0;
-          white-space: nowrap;
-          cursor: pointer;
-          border: 0;
-          background: transparent;
-          &:active {
-            background: rgba(220, 234, 239, 0.3);
-          }
-          &:focus {
-            outline: ${focusVisible ? `2px solid ${theme.accent}` : '0'};
-          }
-          &::-moz-focus-inner {
-            border: 0;
-          }
-        `}
-        {...props}
-      />
-    )}
-  </FocusVisible>
+  <ButtonBase
+    css={`
+      display: flex;
+      align-items: center;
+      height: 44px;
+    `}
+    {...props}
+  />
 )
 
 export default ItemButton
