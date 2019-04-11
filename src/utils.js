4e8e2f4881082aeb1081f3a6845cde8266b523be
Brett Sun <qisheng.brett.sun@gmail.com>
2019-04-11 16:22:11 +0200

    feat: load app icons from new icon schema

--- src/utils.js
+++ src/utils.js
@@ -15,9 +15,12 @@ export function compose(...funcs) {
 
 // Get the icon URL of an app
 export function appIconUrl(app) {
-  return app && app.baseUrl
-    ? resolvePathname('images/icon.svg', app.baseUrl)
-    : null
+  if (app && app.baseUrl && Array.isArray(app.icons)) {
+    const iconSize =
+      app.icons.find(({ sizes }) => sizes === '22x22') || app.icons[0]
+    return resolvePathname(removeStartingSlash(iconSize.src), app.baseUrl)
+  }
+  return null
 }
 
 export function isElectron() {
