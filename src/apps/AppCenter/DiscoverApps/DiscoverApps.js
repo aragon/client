2c84c1aa09554743f051eb153eda17ed29013451
Brett Sun <qisheng.brett.sun@gmail.com>
2019-04-14 22:01:06 +0200

    Discover Apps: sort by readiness, update some details, and add pando

--- src/apps/AppCenter/DiscoverApps/DiscoverApps.js
+++ src/apps/AppCenter/DiscoverApps/DiscoverApps.js
@@ -24,55 +24,77 @@ const statuses = {
   ready: colors.Green['Spring Green'],
 }
 
-const appsInDevelopment = shuffleArray([
-  {
-    icon: agentIcon,
-    name: 'Agent',
-    status: 'experimental',
-    description: `Hold assets and perform actions from DAOs`,
-    link: 'https://github.com/aragon/aragon-apps/tree/master/apps/agent',
-  },
-  {
-    icon: surveyIcon,
-    name: 'Survey',
-    status: 'alpha',
-    description: `Create signaling votes.`,
-    link: 'https://github.com/aragon/aragon-apps/tree/master/apps/survey',
-  },
-  {
-    icon: null,
-    name: 'That Planning Suite',
-    status: 'alpha',
-    description: `Suite for open and fluid organizations.
-                  Bounties, range voting, and more.`,
-    link: 'https://github.com/spacedecentral/planning-suite',
-  },
-  {
-    icon: payrollIcon,
-    name: 'Payroll',
-    status: 'experimental',
-    description: `Pay and get paid, by the block.
-                  Supports tokens and price feeds.`,
-    link:
-      'https://github.com/aragon/aragon-apps/tree/master/future-apps/payroll',
-  },
-  {
-    icon: espressoIcon,
-    name: 'Espresso',
-    status: 'pre-alpha',
-    description: `Collaborative data vault.
-                  Encrypt and share data with people in your organization.`,
-    link: 'https://github.com/espresso-org',
-  },
-  {
-    icon: null,
-    name: 'Liquid democracy',
-    status: 'pre-alpha',
-    description: `Delegate your voting power to others,
-                  and vote on important matters.`,
-    link: 'https://github.com/aragonlabs/liquid-democracy',
-  },
-])
+const appsInDevelopment = [
+  // Ready
+  ...shuffleArray([
+    {
+      icon: surveyIcon,
+      name: 'Survey',
+      status: 'ready',
+      description: `Create multi-option signaling votes.`,
+      link: 'https://github.com/aragon/aragon-apps/tree/master/apps/survey',
+    },
+  ]),
+
+  // Experimental
+  ...shuffleArray([
+    {
+      icon: agentIcon,
+      name: 'Agent',
+      status: 'experimental',
+      description: `Hold assets and perform actions from DAOs`,
+      link: 'https://github.com/aragon/aragon-apps/tree/master/apps/agent',
+    },
+    {
+      icon: payrollIcon,
+      name: 'Payroll',
+      status: 'experimental',
+      description: `Pay and get paid, by the block.
+                    Supports tokens and price feeds.`,
+      link:
+        'https://github.com/aragon/aragon-apps/tree/master/future-apps/payroll',
+    },
+    {
+      icon: null,
+      name: 'That Planning Suite',
+      status: 'experimental',
+      description: `Suite for open and fluid organizations.
+                    Bounties, range voting, and more.`,
+      link: 'https://github.com/AutarkLabs/planning-suite',
+    },
+    {
+      icon: null,
+      name: 'Pando',
+      status: 'experimental',
+      description:
+        'Distributed git remote protocol based on IPFS, ethereum and aragonOS',
+      link: 'https://github.com/pandonetwork/pando',
+    },
+  ]),
+
+  // Alpha
+  ...shuffleArray([]),
+
+  // Pre-alpha
+  ...shuffleArray([
+    {
+      icon: espressoIcon,
+      name: 'Espresso',
+      status: 'pre-alpha',
+      description: `Collaborative data vault.
+                    Encrypt and share data with people in your organization.`,
+      link: 'https://github.com/espresso-org',
+    },
+    {
+      icon: null,
+      name: 'Liquid democracy',
+      status: 'pre-alpha',
+      description: `Delegate your voting power to others,
+                    and vote on important matters.`,
+      link: 'https://github.com/aragonlabs/liquid-democracy',
+    },
+  ]),
+]
 
 const DiscoverApps = React.memo(() => (
   <div>
