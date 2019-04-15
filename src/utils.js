c6728994d82eabd344bc411b6712fd8bf8c4e8f9
Pierre Bertet <hello@pierre.world>
2019-04-15 13:44:04 +0200

    appIconUrl(): select the right icon based on provided size

--- src/utils.js
+++ src/utils.js
@@ -13,14 +13,46 @@ export function compose(...funcs) {
   return funcs.reduce((a, b) => (...args) => a(b(...args)))
 }
 
-// Get the icon URL of an app
-export function appIconUrl(app) {
-  if (app && app.baseUrl && Array.isArray(app.icons)) {
-    const iconSize =
-      app.icons.find(({ sizes }) => sizes === '22x22') || app.icons[0]
-    return resolvePathname(removeStartingSlash(iconSize.src), app.baseUrl)
+// Get the best icon for the given size.
+// Set size to -1 to get the largest one, or to 0 to get the smallest one.
+function getAppIconBySize(icons, size = -1) {
+  // Collect the sizes and sort them
+  const sizes = icons
+    .map((icon, i) => {
+      const width = parseInt(icon.sizes.split('x')[1], 10)
+      return [i, isNaN(width) ? -1 : width]
+    })
+    .filter(size => size[1] !== -1)
+    .sort((a, b) => a[1] - b[1])
+
+  // No valid size found
+  if (sizes.length === 0) {
+    return null
   }
-  return null
+
+  // No rendering size provided: return the largest icon.
+  if (size === -1) {
+    return icons[sizes[sizes.length - 1][0]]
+  }
+
+  // Find the first icon that is equal or larger than the provided size.
+  const greaterOrEqualSize = sizes.find(iconSize => iconSize[1] >= size)
+
+  // Return the found icon, or the largest one otherwise.
+  return icons[greaterOrEqualSize ? greaterOrEqualSize[0] : icons.length - 1]
+}
+
+// Get the icon URL of an app.
+// `size` is the size at which the icon will be rendered.
+export function appIconUrl(app, size = -1) {
+  if (!app || !app.baseUrl || !Array.isArray(app.icons)) {
+    return null
+  }
+
+  const icon = getAppIconBySize(app.icons, size)
+  return icon
+    ? resolvePathname(removeStartingSlash(icon.src), app.baseUrl)
+    : null
 }
 
 export function isElectron() {
