8827132b6c8fb60de49f12d16cec3aa7a2a14a0d
Brett Sun <qisheng.brett.sun@gmail.com>
2019-04-17 16:11:33 +0200

    chore: remove unused app center files

--- src/known-apps/index.js
+++
@@ -1,8 +0,0 @@
-import finance from './apps/finance'
-import voting from './apps/voting'
-import tokenManager from './apps/token-manager'
-import survey from './apps/survey'
-
-export const KnownApps = new Map([finance, voting, tokenManager, survey])
-
-export const getKnownApp = appName => KnownApps.get(appName) || null
