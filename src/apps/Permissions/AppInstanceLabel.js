dab05ff478f183a764be73a81bcdf6340d7ef9e0
Gorka <gorka@aquigorka.com>
2019-04-25 12:45:25 +0200

    AppInstanceLabel: add react memo to avoid re-renders

--- src/apps/Permissions/AppInstanceLabel.js
+++ src/apps/Permissions/AppInstanceLabel.js
@@ -6,31 +6,33 @@ import { AppType, EthereumAddressType } from '../../prop-types'
 import { shortenAddress } from '../../web3-utils'
 import AppIcon from '../../components/AppIcon/AppIcon'
 
-const AppInstanceLabel = ({ app, proxyAddress, showIcon = true }) => {
-  const { above } = useViewport()
+const AppInstanceLabel = React.memo(
+  ({ app, proxyAddress, showIcon = true }) => {
+    const { above } = useViewport()
 
-  return (
-    <Main>
-      {above('medium') && showIcon && (
-        <div
-          css={`
-            display: flex;
-            align-items: center;
-            height: 0;
-            margin-right: 10px;
-            margin-top: -1px;
-          `}
-        >
-          <AppIcon app={app} />
-        </div>
-      )}
-      <AppName>{app ? app.name : 'Unknown'}</AppName>
-      <StyledBadge title={proxyAddress}>
-        {(app && app.identifier) || shortenAddress(proxyAddress)}
-      </StyledBadge>
-    </Main>
-  )
-}
+    return (
+      <Main>
+        {above('medium') && showIcon && (
+          <div
+            css={`
+              display: flex;
+              align-items: center;
+              height: 0;
+              margin-right: 10px;
+              margin-top: -1px;
+            `}
+          >
+            <AppIcon app={app} />
+          </div>
+        )}
+        <AppName>{app ? app.name : 'Unknown'}</AppName>
+        <StyledBadge title={proxyAddress}>
+          {(app && app.identifier) || shortenAddress(proxyAddress)}
+        </StyledBadge>
+      </Main>
+    )
+  }
+)
 
 AppInstanceLabel.propTypes = {
   app: AppType.isRequired,
