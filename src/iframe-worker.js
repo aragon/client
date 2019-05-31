8643ba5f8c1d7b06461a4819f4027dd79b0c1f18
Gorka <gorka@aquigorka.com>
2019-05-31 11:21:34 +0200

    Iframe worker: add clean up for created url

--- src/iframe-worker.js
+++ src/iframe-worker.js
@@ -34,6 +34,8 @@ class IframeWorker extends EventTarget {
           worker.addEventListener('error', error => window.parent.postMessage({ from: '${name}', error }, '*'), false)
           worker.addEventListener('message', event => window.parent.postMessage({ from: '${name}', msg: event.data }, '*'), false)
           window.addEventListener('message', ({ data }) => worker.postMessage(data))
+          // Clean up the url we created to spawn the worker
+-         URL.revokeObjectURL(workerUrl)
         }
         init()
       </script>
