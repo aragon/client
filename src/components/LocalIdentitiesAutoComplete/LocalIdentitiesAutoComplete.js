6ccc2c350c1e5eda611d24d35142161bec4c9b6b
Gorka <gorka@aquigorka.com>
2019-06-03 10:01:53 +0200

    LocalIdentitiesAutoComplete: fix lint issues

--- src/components/LocalIdentitiesAutoComplete/LocalIdentitiesAutoComplete.js
+++ src/components/LocalIdentitiesAutoComplete/LocalIdentitiesAutoComplete.js
@@ -3,112 +3,86 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { EthIdenticon, IdentityBadge, theme } from '@aragon/ui'
 import AutoCompleteSelected from '../AutoComplete/AutoCompleteSelected'
+import { AragonType } from '../../prop-types'
 
 const withKey = item => ({ key: item.address, ...item })
 const sortAlphAsc = (a, b) => a.name.localeCompare(b.name)
 
-const LocalIdentitiesAutoComplete = React.memo(
-  React.forwardRef(function LocalIdentitiesAutoComplete(
-    { onChange, wide, value, placeholder, required, wrapper },
-    ref
-  ) {
-    const [items, setItems] = useState([])
-    const [selected, setSelected] = useState(null)
-    const [searchTerm, setSearchTerm] = useState('')
+function LocalIdentitiesAutoComplete(
+  { onChange, wide, value, placeholder, required, wrapper },
+  ref
+) {
+  const [items, setItems] = useState([])
+  const [selected, setSelected] = useState(null)
+  const [searchTerm, setSearchTerm] = useState('')
 
-    const handleSearch = useCallback(
-      async term => {
-        if (term.length < 3) {
-          setItems([])
-          return
-        }
-        //const items = await wrapper.searchIdentities(term).toPromise()
-        const items = await wrapper.searchIdentities(term)
-        setItems(items.map(withKey).sort(sortAlphAsc))
-      },
-      [wrapper]
-    )
-    const handleChange = useCallback(
-      value => {
+  const handleSearch = useCallback(
+    async term => {
+      if (term.length < 3) {
+        setItems([])
+        return
+      }
+      const items = await wrapper.searchIdentities(term)
+      setItems(items.map(withKey).sort(sortAlphAsc))
+    },
+    [wrapper]
+  )
+  const handleChange = useCallback(
+    value => {
+      setSearchTerm(value)
+      handleSearch(value)
+      onChange(value)
+    },
+    [onChange, handleSearch]
+  )
+  const handleSelect = useCallback(
+    selected => {
+      const { name, address } = selected
+      setSearchTerm(name)
+      handleSearch(name)
+      setSelected(selected)
+      onChange(address)
+    },
+    [onChange, handleSearch]
+  )
+  const handleSelectedClick = () => {
+    setSelected(null)
+    onChange(selected.name)
+  }
+
+  useEffect(() => {
+    const effect = async () => {
+      // reset
+      if (value === '') {
+        setSelected(null)
         setSearchTerm(value)
         handleSearch(value)
-        onChange(value)
-      },
-      [onChange]
-    )
-    const handleSelect = useCallback(
-      selected => {
-        const { name, address } = selected
-        setSearchTerm(name)
-        handleSearch(name)
-        setSelected(selected)
-        onChange(address)
-      },
-      [onChange]
-    )
-    const handleSelectedClick = () => {
-      setSelected(null)
-      onChange(selected.name)
-    }
-    const renderItem = useCallback(({ address, name }, searchTerm) => {
-      if (searchTerm.indexOf('0x') === 0) {
-        return (
-          <Option>
-            <IdentityBadge compact badgeOnly entity={address} />
-            <Name>{name}</Name>
-          </Option>
-        )
+        return
       }
-      return (
-        <Option>
-          <Name>{name}</Name>
-          <IdentityBadge compact badgeOnly entity={address} />
-        </Option>
-      )
-    })
-    const renderSelected = useCallback(({ address, name }) => {
-      return (
-        <Option selected>
-          <EthIdenticon address={address} scale={0.6} radius={2} />
-          <Name>{name}</Name>
-        </Option>
-      )
-    })
-
-    useEffect(() => {
-      const effect = async () => {
-        // reset
-        if (value === '') {
-          setSelected(null)
-          setSearchTerm(value)
-          handleSearch(value)
-          return
-        }
-        // value coming from up the tree not from typing
-        if (searchTerm === '') {
-          //const exists = await wrapper.searchIdentities(value).toPromise()
-          const exists = await wrapper.searchIdentities(value)
-          if (exists && exists.length === 1) {
-            const item = exists[0]
-            if (
-              item.name.toLowerCase() === value.toLowerCase() ||
-              item.address.toLowerCase() === value.toLowerCase()
-            ) {
-              setSelected(item)
-              setSearchTerm(item.name)
-              handleSearch(item.name)
-              return
-            }
+      // value coming from up the tree not from typing
+      if (searchTerm === '') {
+        const exists = await wrapper.searchIdentities(value)
+        if (exists && exists.length === 1) {
+          const item = exists[0]
+          if (
+            item.name.toLowerCase() === value.toLowerCase() ||
+            item.address.toLowerCase() === value.toLowerCase()
+          ) {
+            setSelected(item)
+            setSearchTerm(item.name)
+            handleSearch(item.name)
+            return
           }
-          setSearchTerm(value)
         }
+        setSearchTerm(value)
       }
-      effect()
-    }, [selected, value, wrapper])
+    }
+    effect()
+  }, [selected, value, wrapper, handleSearch, searchTerm])
 
-    return (
-      <AutoCompleteSelected
-        itemButtonStyles={`
+  return (
+    <AutoCompleteSelected
+      itemButtonStyles={`
           border-left: 3px solid transparent;
           cursor: pointer;
           border-radius: 0;
@@ -120,17 +94,17 @@ const LocalIdentitiesAutoComplete = React.memo(
             border-left: 3px solid ${theme.accent}
           }
         `}
-        items={items}
-        onChange={handleChange}
-        onSelect={handleSelect}
-        onSelectedClick={handleSelectedClick}
-        placeholder={placeholder}
-        ref={ref}
-        renderItem={renderItem}
-        renderSelected={renderSelected}
-        required={required}
-        selected={selected}
-        selectedButtonStyles={`
+      items={items}
+      onChange={handleChange}
+      onSelect={handleSelect}
+      onSelectedClick={handleSelectedClick}
+      placeholder={placeholder}
+      ref={ref}
+      renderItem={Item}
+      renderSelected={Selected}
+      required={required}
+      selected={selected}
+      selectedButtonStyles={`
           &:hover,
           &:focus {
             outline: none;
@@ -138,18 +112,55 @@ const LocalIdentitiesAutoComplete = React.memo(
             border-radius: 3px;
           }
         `}
-        value={searchTerm}
-        wide={wide}
-      />
-    )
-  })
-)
+      value={searchTerm}
+      wide={wide}
+    />
+  )
+}
 
 LocalIdentitiesAutoComplete.propTypes = {
   onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
   required: PropTypes.bool,
   value: PropTypes.string,
   wide: PropTypes.bool,
+  wrapper: AragonType.isRequired,
+}
+
+const Item = ({ address, name }, searchTerm) => {
+  if (searchTerm.indexOf('0x') === 0) {
+    return (
+      <Option>
+        <IdentityBadge compact badgeOnly entity={address} />
+        <Name>{name}</Name>
+      </Option>
+    )
+  }
+  return (
+    <Option>
+      <Name>{name}</Name>
+      <IdentityBadge compact badgeOnly entity={address} />
+    </Option>
+  )
+}
+
+Item.propTypes = {
+  address: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+}
+
+const Selected = ({ address, name }) => {
+  return (
+    <Option selected>
+      <EthIdenticon address={address} scale={0.6} radius={2} />
+      <Name>{name}</Name>
+    </Option>
+  )
+}
+
+Selected.propTypes = {
+  address: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
 }
 
 const Option = styled.div`
@@ -169,4 +180,4 @@ const Name = styled.div`
   color: #000;
 `
 
-export default LocalIdentitiesAutoComplete
+export default React.memo(React.forwardRef(LocalIdentitiesAutoComplete))
