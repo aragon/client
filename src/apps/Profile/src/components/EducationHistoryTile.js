cb02b851e37b0f039c39537d4e17256028cf7488
Chad Ostrowski <hi@chadoh.com>
2019-06-04 15:10:20 -0400

    Profile: right-align education edit buttons
    Also, use an actual button element, which improves accessibility.

--- src/apps/Profile/src/components/EducationHistoryTile.js
+++ src/apps/Profile/src/components/EducationHistoryTile.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-import { Text, theme } from '@aragon/ui'
+import { Button as AragonButton, Text, theme } from '@aragon/ui'
 import { IconPencil, IconTrash } from '../assets'
 import { displayStartEndDates } from '../utils'
 
@@ -23,39 +23,39 @@ const EducationHistoryTile = ({
       <Dates>{displayStartEndDates(educationHistoryData)}</Dates>
     </div>
     <Icons>
-      <IconPencil
-        width="16px"
-        color={theme.accent}
-        onClick={() => openModal()}
-      />
-      <IconTrash
-        width="16px"
-        color={theme.accent}
-        onClick={() => removeItem()}
-      />
+      <Button onClick={() => openModal()}>
+        <IconPencil width="16px" height="16px" color={theme.accent} />
+      </Button>
+      <Button onClick={() => removeItem()}>
+        <IconTrash width="16px" height="16px" color={theme.accent} />
+      </Button>
     </Icons>
   </SingleEducationItem>
 )
 const SingleEducationItem = styled.div`
-  display: flex;
-  > :not(:last-child) {
-    margin-bottom: 3px;
-  }
+  position: relative;
 `
 const Icons = styled.div`
-  display: inline-flex;
-  width: auto;
+  display: flex;
   flex-direction: column;
+  position: absolute;
+  top: -4px;
+  right: -4px;
   visibility: hidden;
-  > * {
-    margin: 0 0 8px 8px;
-    cursor: pointer;
-  }
   ${SingleEducationItem}:hover & {
     visibility: visible;
   }
 `
-
+const Button = styled(AragonButton).attrs({ mode: 'text' })`
+  background: rgba(255, 255, 255, 0.9);
+  box-sizing: content-box;
+  height: 16px;
+  overflow: hidden;
+  padding: 4px;
+  &:not(:last-child) {
+    margin-bottom: 4px;
+  }
+`
 const Dates = styled(Text.Block).attrs({ size: 'small' })`
   color: ${theme.textTertiary};
   margin-top: 2px;
