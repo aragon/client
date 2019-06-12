49ca5b95368a42c27a2e87f04a91d1fa5a1098a1
Gorka <gorka@aquigorka.com>
2019-06-12 15:23:29 +0200

    useArrowKeysFocus: update var names

--- src/hooks.js
+++ src/hooks.js
@@ -10,6 +10,9 @@ import keycodes from './keycodes'
 import { log, removeStartingSlash } from './utils'
 import { atou } from './string-utils'
 
+const KEYCODE_UP = 38
+const KEYCODE_DOWN = 40
+
 // Update `now` at a given interval.
 export function useNow(updateEvery = 1000) {
   const [now, setNow] = useState(new Date())
@@ -243,13 +246,14 @@ export function useOnBlur(cb, ref = useRef()) {
 /* eslint-disable react-hooks/rules-of-hooks */
 export function useArrowKeysFocus(query, containerRef = useRef()) {
   /* eslint-enable react-hooks/rules-of-hooks */
-  const [index, setIndex] = useState(-1)
+  const [highlightedIndex, setHighlightedIndex] = useState(-1)
 
-  const reset = () => setIndex(-1)
+  const reset = () => setHighlightedIndex(-1)
   const cycleFocus = useCallback(
-    change => {
+    (e, change) => {
+      e.preventDefault()
       const elements = document.querySelectorAll(query)
-      let next = index + change
+      let next = highlightedIndex + change
       if (next > elements.length - 1) {
         next = 0
       }
@@ -259,30 +263,34 @@ export function useArrowKeysFocus(query, containerRef = useRef()) {
       if (!elements[next]) {
         next = -1
       }
-      setIndex(next)
+      setHighlightedIndex(next)
     },
-    [index, query]
+    [highlightedIndex, query]
   )
   const handleKeyDown = useCallback(
-    ({ keyCode }) =>
-      keyCode === 38 ? cycleFocus(-1) : keyCode === 40 ? cycleFocus(1) : null,
+    e => {
+      const { keyCode } = e
+      if (keyCode === KEYCODE_UP || keyCode === KEYCODE_DOWN) {
+        cycleFocus(e, keyCode === KEYCODE_UP ? -1 : 1)
+      }
+    },
     [cycleFocus]
   )
 
   const { handleBlur: handleContainerBlur } = useOnBlur(reset, containerRef)
   useEffect(() => {
-    if (index === -1) {
+    if (highlightedIndex === -1) {
       return
     }
     const elements = document.querySelectorAll(query)
-    if (!elements[index]) {
+    if (!elements[highlightedIndex]) {
       return
     }
-    elements[index].focus()
-  }, [index, query])
+    elements[highlightedIndex].focus()
+  }, [highlightedIndex, query])
   useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown)
-    return () => window.removeEventListener('keydown', handleKeyDown)
+    document.addEventListener('keydown', handleKeyDown)
+    return () => document.removeEventListener('keydown', handleKeyDown)
   }, [handleKeyDown])
 
   return { containerRef, handleContainerBlur }
