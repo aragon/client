bc0d6a2f087f89d8cbdcd58fe88ce8f724da6e62
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-06-13 10:24:11 -0400

    fix linter errors

--- src/apps/index.js
+++ src/apps/index.js
@@ -1,4 +1,3 @@
 export { default as Permissions } from './Permissions/Permissions'
 export { default as Settings } from './Settings/Settings'
 export { default as AppCenter } from './AppCenter/AppCenter'
-export { default as Profile } from './Profile/src/App'
