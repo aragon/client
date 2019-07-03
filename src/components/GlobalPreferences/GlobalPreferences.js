2e34345dd75b41a4580bed930686a3661ab73199
Gorka <gorka@aquigorka.com>
2019-07-03 14:53:33 +0200

    GlobalPreferences: add close button and boilperplate to open

--- src/components/GlobalPreferences/GlobalPreferences.js
+++ src/components/GlobalPreferences/GlobalPreferences.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Bar, Layout, Header, TabBar } from '@aragon/ui'
+import React, { useState, useEffect } from 'react'
+import { Bar, ButtonIcon, Layout, Header, TabBar } from '@aragon/ui'
 import Network from './Network/Network'
 import Notifications from './Notifications'
 import CustomLabels from './CustomLabels/CustomLabels'
@@ -14,32 +14,79 @@ const SECTIONS = new Map([
 const PATHS = Array.from(SECTIONS.keys())
 const VALUES = Array.from(SECTIONS.values())
 
-// path = PATHS[0]
-function GlobalPreferences({ dao, wrapper, path = 'help-and-feedback' }) {
-  const [currentSection, setCurrentSection] = useState(
-    SECTIONS.has(path) ? PATHS.findIndex(item => item === path) : 0
-  )
-  const handleChange = index => {
-    setCurrentSection(index)
+function GlobalPreferences({ dao, wrapper, onClose, opened }) {
+  const { setCurrentSection, currentSection } = useGlobalPreferences()
+
+  if (!opened) {
+    return null
   }
 
   return (
-    <Layout>
-      <Header primary="Global preferences" />
-      <Bar>
-        <TabBar
-          items={VALUES}
-          onChange={handleChange}
-          selected={currentSection}
-        />
-      </Bar>
-      <main>
-        {currentSection === 0 && <CustomLabels dao={dao} wrapper={wrapper} />}
-        {currentSection === 1 && <Network wrapper={wrapper} />}
-        {currentSection === 2 && <Notifications />}
-        {currentSection === 3 && <HelpAndFeedback />}
-      </main>
-    </Layout>
+    <div
+      css={`
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: 11;
+        background: white;
+      `}
+    >
+      <View
+        dao={dao}
+        wrapper={wrapper}
+        onClose={onClose}
+        currentSection={currentSection}
+        setCurrentSection={setCurrentSection}
+      />
+    </div>
+  )
+}
+
+// path = PATHS[0]
+function useGlobalPreferences() {
+  const [currentSection, setCurrentSection] = useState(0)
+
+  // effect for location url?
+  //useEffect(() => {
+    //setCurrentSection(
+      //SECTIONS.has(path) ? PATHS.findIndex(item => item === path) : 0
+    //)
+  //}, [path])
+
+  return { setCurrentSection, currentSection }
+}
+
+function View({ dao, wrapper, onClose, currentSection, setCurrentSection }) {
+  return (
+    <React.Fragment>
+      <Close onClick={onClose} />
+      <Layout>
+        <Header primary="Global preferences" />
+        <Bar>
+          <TabBar
+            items={VALUES}
+            onChange={setCurrentSection}
+            selected={currentSection}
+          />
+        </Bar>
+        <main>
+          {currentSection === 0 && <CustomLabels dao={dao} wrapper={wrapper} />}
+          {currentSection === 1 && <Network wrapper={wrapper} />}
+          {currentSection === 2 && <Notifications />}
+          {currentSection === 3 && <HelpAndFeedback />}
+        </main>
+      </Layout>
+    </React.Fragment>
+  )
+}
+
+function Close({ onClick }) {
+  return (
+    <div css="text-align: right;">
+      <ButtonIcon onClick={onClick}>x</ButtonIcon>
+    </div>
   )
 }
 
