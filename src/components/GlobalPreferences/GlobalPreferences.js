c3a53a1c058e47704fb9deaef25549b8fb50576e
Gorka <gorka@aquigorka.com>
2019-07-03 12:47:47 +0200

    GlobalPreferences: update layout and remove hooks

--- src/components/GlobalPreferences/GlobalPreferences.js
+++ src/components/GlobalPreferences/GlobalPreferences.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react'
-import { TabBar } from '@aragon/ui'
+import { Bar, Layout, Header, TabBar } from '@aragon/ui'
 import Network from './Network/Network'
-import useNetwork from './Network/useNetwork'
 import Notifications from './Notifications'
-import CustomLabels from './CustomLabels'
-import HelpAndFeedback from './HelpAndFeedback'
+import CustomLabels from './CustomLabels/CustomLabels'
+import HelpAndFeedback from './HelpAndFeedback/HelpAndFeedback'
 
 const SECTIONS = new Map([
   ['custom-labels', 'Custom Labels'],
@@ -16,7 +15,7 @@ const PATHS = Array.from(SECTIONS.keys())
 const VALUES = Array.from(SECTIONS.values())
 
 // path = PATHS[0]
-function GlobalPreferences({ wrapper, path = 'custom-labels' }) {
+function GlobalPreferences({ dao, wrapper, path = 'help-and-feedback' }) {
   const [currentSection, setCurrentSection] = useState(
     SECTIONS.has(path) ? PATHS.findIndex(item => item === path) : 0
   )
@@ -24,43 +23,23 @@ function GlobalPreferences({ wrapper, path = 'custom-labels' }) {
     setCurrentSection(index)
   }
 
-  const {
-    ethNode,
-    ipfsGateway,
-    handleNetworkChange,
-    handleClearCache,
-    networkError,
-    setIpfsGateway,
-    setEthNode,
-    network,
-  } = useNetwork(wrapper)
-
   return (
-    <section>
-      <h2>Global preferences</h2>
-      <TabBar
-        items={VALUES}
-        onChange={handleChange}
-        selected={currentSection}
-      />
+    <Layout>
+      <Header primary="Global preferences" />
+      <Bar>
+        <TabBar
+          items={VALUES}
+          onChange={handleChange}
+          selected={currentSection}
+        />
+      </Bar>
       <main>
-        {currentSection === 0 && <CustomLabels />}
-        {currentSection === 1 && (
-          <Network
-            ethNode={ethNode}
-            ipfsGateway={ipfsGateway}
-            onSave={handleNetworkChange}
-            onClear={handleClearCache}
-            error={networkError}
-            onChangeEthNode={setEthNode}
-            onChangeIpfsGateway={setIpfsGateway}
-            network={network}
-          />
-        )}
+        {currentSection === 0 && <CustomLabels dao={dao} wrapper={wrapper} />}
+        {currentSection === 1 && <Network wrapper={wrapper} />}
         {currentSection === 2 && <Notifications />}
         {currentSection === 3 && <HelpAndFeedback />}
       </main>
-    </section>
+    </Layout>
   )
 }
 
