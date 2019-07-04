c6a362b7e4772c9388afc3ac5e493749cbcb77dd
Gorka <gorka@aquigorka.com>
2019-07-04 11:14:02 +0200

    GlobalSettingsButton: use icons and update styles

--- src/components/OrgView/GlobalSettingsButton.js
+++ src/components/OrgView/GlobalSettingsButton.js
@@ -8,11 +8,10 @@ import {
   IconSettings,
   Popover,
 } from '@aragon/ui'
-
-const networkIcon = <IconSettings />
-const customLabelsIcon = <IconSettings />
-const notificationIcon = <IconSettings />
-const helpFeedbackIcon = <IconSettings />
+import IconNetwork from './IconNetwork'
+import IconCustomLabels from './IconCustomLabels'
+import IconNotifications from './IconNotifications'
+import IconHelpAndFeedback from './IconHelpAndFeedback'
 
 function GlobalSettingsButton({ onOpen }) {
   const [opened, setOpened] = useState(false)
@@ -32,7 +31,14 @@ function GlobalSettingsButton({ onOpen }) {
   return (
     <React.Fragment>
       <div ref={containerRef}>
-        <ButtonIcon onClick={handleToggle} css="height:100%;">
+        <ButtonIcon
+          onClick={handleToggle}
+          css={`
+            height: 100%;
+            width: 40px;
+          `}
+          label="Global preferences"
+        >
           <IconSettings />
         </ButtonIcon>
       </div>
@@ -50,32 +56,42 @@ function GlobalSettingsButton({ onOpen }) {
             list-style: none;
           `}
         >
-          <ListItem>Global preferences</ListItem>
+          <ListItem
+            css={`
+              height: 32px;
+              color: #637381;
+              text-transform: uppercase;
+              font-size: 12px;
+              padding-left: ${2 * GU}px;
+            `}
+          >
+            Global preferences
+          </ListItem>
           <ListItem>
             <Item
               onClick={handleItemClick('custom-labels')}
-              icon={customLabelsIcon}
+              icon={<IconCustomLabels />}
               label="Custom labels"
             />
           </ListItem>
           <ListItem>
             <Item
               onClick={handleItemClick('network')}
-              icon={networkIcon}
+              icon={<IconNetwork />}
               label="Network"
             />
           </ListItem>
           <ListItem>
             <Item
               onClick={handleItemClick('notifications')}
-              icon={notificationIcon}
+              icon={<IconNotifications />}
               label="Notifications"
             />
           </ListItem>
           <ListItem>
             <Item
               onClick={handleItemClick('help-and-feedback')}
-              icon={helpFeedbackIcon}
+              icon={<IconHelpAndFeedback />}
               label="Help & Feedback"
             />
           </ListItem>
@@ -87,17 +103,20 @@ function GlobalSettingsButton({ onOpen }) {
 
 function Item({ icon, label, onClick }) {
   return (
-    <ButtonBase
+    <ButtonIcon
       css={`
         width: 100%;
+        height: 100%;
         display: flex;
         align-items: flex-end;
+        padding: ${2 * GU}px;
+        justify-content: left;
       `}
       onClick={onClick}
     >
       {icon}
       <span css={icon && `margin-left: ${1 * GU}px;`}>{label}</span>
-    </ButtonBase>
+    </ButtonIcon>
   )
 }
 
@@ -105,7 +124,6 @@ const ListItem = styled.li`
   display: flex;
   align-items: center;
   border-bottom: 1px solid #dfe3e8;
-  padding: ${2 * GU}px;
   height: 56px;
 `
 
