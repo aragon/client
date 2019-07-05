44980abccc97182d5eb2a9a774a4ae120367f01f
Gorka <gorka@aquigorka.com>
2019-07-05 13:14:12 +0200

    Network: refactor & cleanup

--- src/components/GlobalPreferences/Network/Network.js
+++ src/components/GlobalPreferences/Network/Network.js
@@ -5,12 +5,7 @@ import { InvalidNetworkType, InvalidURI, NoConnection } from '../../../errors'
 import { sanitizeNetworkType } from '../../../network-config'
 import { defaultEthNode, ipfsDefaultConf, network } from '../../../environment'
 import { checkValidEthNode } from '../../../web3-utils'
-import {
-  getSelectedCurrency,
-  setDefaultEthNode,
-  setIpfsGateway,
-  setSelectedCurrency,
-} from '../../../local-settings'
+import { setDefaultEthNode, setIpfsGateway } from '../../../local-settings'
 
 function Network({ wrapper }) {
   const {
@@ -19,22 +14,79 @@ function Network({ wrapper }) {
     handleNetworkChange,
     handleClearCache,
     networkError,
-    setIpfsGateway,
-    setEthNode,
+    handleEthNodeChange,
+    handleIpfsGatewayChange,
     network,
   } = useNetwork(wrapper)
 
   return (
-    <View
-      ethNode={ethNode}
-      ipfsGateway={ipfsGateway}
-      onSave={handleNetworkChange}
-      onClear={handleClearCache}
-      error={networkError}
-      onChangeEthNode={setEthNode}
-      onChangeIpfsGateway={setIpfsGateway}
-      network={network}
-    />
+    <React.Fragment>
+      <Box heading={'Node settings'}>
+        <Label>
+          Ethereum node{' '}
+          <TextInput
+            value={ethNode}
+            wide
+            onChange={handleEthNodeChange}
+            css={networkError && `border-color: ${theme.negative};`}
+          />
+          {networkError && (
+            <Text color={theme.negative} size="xsmall">
+              {(() => {
+                if (networkError instanceof InvalidNetworkType) {
+                  return `Node must be connected to ${sanitizeNetworkType(
+                    network.type
+                  )}`
+                }
+                if (networkError instanceof InvalidURI) {
+                  return 'Must provide WebSocket endpoint to node'
+                }
+                if (networkError instanceof NoConnection) {
+                  return 'Could not connect to node'
+                }
+                return 'URI does not seem to be a ETH node'
+              })()}
+            </Text>
+          )}
+        </Label>
+        <Label>
+          IPFS Gateway{' '}
+          <TextInput
+            value={ipfsGateway}
+            wide
+            onChange={handleIpfsGatewayChange}
+          />
+        </Label>
+        <Button mode="strong" onClick={handleNetworkChange}>
+          Save changes
+        </Button>
+      </Box>
+      <Box heading={'Troubleshooting'}>
+        <div
+          css={`
+            margin-bottom: ${2 * GU}px;
+          `}
+        >
+          <Text>
+            Press this button to refresh the cache of the application in your
+            browser.
+          </Text>
+        </div>
+        <Button
+          css={`
+            margin-bottom: ${2 * GU}px;
+          `}
+          onClick={handleClearCache}
+        >
+          Clear application cache
+        </Button>
+        <Info>
+          This will only delete the data stored in youur browser to make the app
+          load faster. No data related to the organization itself will be
+          altered.
+        </Info>
+      </Box>
+    </React.Fragment>
   )
 }
 
@@ -69,87 +121,11 @@ const useNetwork = wrapper => {
     handleNetworkChange,
     handleClearCache,
     networkError,
-    setEthNode: ({ currentTarget: { value } }) => setEthNodeValue(value),
-    setIpfsGateway: ({ currentTarget: { value } }) =>
+    handleEthNodeChange: ({ currentTarget: { value } }) =>
+      setEthNodeValue(value),
+    handleIpfsGatewayChange: ({ currentTarget: { value } }) =>
       setIpfsGatewayValue(value),
   }
-}
-
-function View({
-  error,
-  ethNode,
-  ipfsGateway,
-  network,
-  onChangeEthNode,
-  onChangeIpfsGateway,
-  onClear,
-  onSave,
-}) {
-  return (
-    <React.Fragment>
-      <Box heading={'Node settings'}>
-        <Label>
-          Ethereum node{' '}
-          <TextInput
-            value={ethNode}
-            wide
-            onChange={onChangeEthNode}
-            css={error && `border-color: ${theme.negative};`}
-          />
-          {error && (
-            <Text color={theme.negative} size="xsmall">
-              {(() => {
-                if (error instanceof InvalidNetworkType) {
-                  return `Node must be connected to ${sanitizeNetworkType(
-                    network.type
-                  )}`
-                }
-                if (error instanceof InvalidURI) {
-                  return 'Must provide WebSocket endpoint to node'
-                }
-                if (error instanceof NoConnection) {
-                  return 'Could not connect to node'
-                }
-                return 'URI does not seem to be a ETH node'
-              })()}
-            </Text>
-          )}
-        </Label>
-        <Label>
-          IPFS Gateway{' '}
-          <TextInput value={ipfsGateway} wide onChange={onChangeIpfsGateway} />
-        </Label>
-        <Button mode="strong" onClick={onSave}>
-          Save changes
-        </Button>
-      </Box>
-      <Box heading={'Troubleshooting'}>
-        <div
-          css={`
-            margin-bottom: ${2 * GU}px;
-          `}
-        >
-          <Text>
-            Press this button to refresh the cache of the application in your
-            browser.
-          </Text>
-        </div>
-        <Button
-          css={`
-            margin-bottom: ${2 * GU}px;
-          `}
-          onClick={onClear}
-        >
-          Clear application cache
-        </Button>
-        <Info>
-          This will only delete the data stored in youur browser to make the app
-          load faster. No data related to the organization itself will be
-          altered.
-        </Info>
-      </Box>
-    </React.Fragment>
-  )
 }
 
 const Label = styled.label`
