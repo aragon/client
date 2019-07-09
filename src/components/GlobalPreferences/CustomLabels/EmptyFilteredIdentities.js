d6f89f1455fc82526453ecc8b93d3d809a79e5c6
Gorka <gorka@aquigorka.com>
2019-07-09 11:58:36 +0200

    EmptyFilteredIdentities: add component

---
+++ src/components/GlobalPreferences/CustomLabels/EmptyFilteredIdentities.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Button, GU, theme } from '@aragon/ui'
+import noResultsSvg from './no-results.svg'
+
+function EmptyFilteredIdentities({ onClear }) {
+  return (
+    <div
+      css={`
+        margin-top: ${4 * GU}px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      `}
+    >
+      <img
+        css={`
+          margin: ${4 * GU}px 0;
+        `}
+        src={noResultsSvg}
+        alt="No results"
+      />
+      <h3
+        css={`
+          font-size: 28px;
+          color: ${theme.content};
+        `}
+      >
+        No results found.
+      </h3>
+      <div
+        css={`
+          margin-bottom: ${4 * GU}px;
+          color: ${theme.surfaceContentSecondary};
+        `}
+      >
+        We can’t find any item matching your search query.{' '}
+        <Button
+          css={`
+            color: ${theme.link};
+            border: none;
+            background: none;
+            border-radius: 0;
+            box-shadow: none;
+            padding: 0;
+            min-width: unset;
+          `}
+          onClick={onClear}
+        >
+          Clear search
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default EmptyFilteredIdentities
