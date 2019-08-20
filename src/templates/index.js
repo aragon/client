b52596e7630b406d8eae957555f378d638d684f8
Pierre Bertet <hello@pierre.world>
2019-08-20 02:08:20 +0200

    Add templates data

---
+++ src/templates/index.js
@@ -0,0 +1,58 @@
+import startupHeader from './assets/header-startup.svg'
+import reputationHeader from './assets/header-reputation.svg'
+import membershipHeader from './assets/header-membership.svg'
+import fundraisingHeader from './assets/header-fundraising.svg'
+import multisignHeader from './assets/header-multisign.svg'
+
+export default [
+  {
+    id: 'startup',
+    name: 'Startup',
+    header: startupHeader,
+    description: `
+      Use fully transferrable tokens to represent ownership stake in your
+      organization. Decisions are made based on stake-weighted voting.
+    `,
+    detailsUrl: 'http://example.com/',
+  },
+  {
+    id: 'reputation',
+    name: 'Reputation',
+    header: reputationHeader,
+    description: `
+      Use non-transferrable tokens to represent reputation. Decisions are made
+      using reputation-weighted voting.
+    `,
+    detailsUrl: 'http://example.com/',
+  },
+  {
+    id: 'membership',
+    name: 'Membership',
+    header: membershipHeader,
+    description: `
+      Use a non-transferrable token to represent membership. Decisions can be
+      made based on one-member-one-vote governance.
+    `,
+    detailsUrl: 'http://example.com/',
+  },
+  {
+    id: 'fundraising',
+    name: 'Fundraising',
+    header: fundraisingHeader,
+    description: `
+      Initialize a transparent and accountable crowdfunding campaign for your
+      project or community organization.
+    `,
+    detailsUrl: 'http://example.com/',
+  },
+  {
+    id: 'multisign',
+    name: 'Multisign',
+    header: multisignHeader,
+    description: `
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. 
+    `,
+    detailsUrl: 'http://example.com/',
+  },
+]
