a52cb5ee807e3e5cebfb9e14b5465e4d81ba5cd3
Gorka <gorka@aquigorka.com>
2019-08-21 16:00:07 +0200

    InstalledApps: update to new styles layout

--- src/apps/AppCenter/InstalledApps/InstalledApps.js
+++ src/apps/AppCenter/InstalledApps/InstalledApps.js
@@ -1,67 +1,71 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import PropTypes from 'prop-types'
-import { Viewport } from '@aragon/ui'
+import { useTheme, useLayout, Card, CardLayout, GU } from '@aragon/ui'
 import { RepoType } from '../../../prop-types'
-import { ZoomableCards } from '../../../components/ZoomableCards'
-
-import AppsGrid from './AppsGrid'
 import AppContent from './AppContent'
 import AppCardContent from './AppCardContent'
 import RepoVersions from './RepoVersions'
 
-class InstalledApps extends React.Component {
-  static propTypes = {
-    repos: PropTypes.arrayOf(RepoType).isRequired,
-    openedRepoId: PropTypes.string,
-    onOpenApp: PropTypes.func.isRequired,
-    onRequestUpgrade: PropTypes.func.isRequired,
-  }
-  static defaultProps = {
-    openedRepoId: null,
-  }
-  getOpenedRepoByAppId(appId) {
-    return this.props.repos.find(repo => repo.appId === appId)
-  }
-  render() {
-    const { repos, openedRepoId, onOpenApp, onRequestUpgrade } = this.props
+const InstalledApps = React.memo(function InstalledApps({
+  repos,
+  openedRepoId,
+  onOpenApp,
+  onRequestUpgrade,
+  onCloseRepo,
+}) {
+  const theme = useTheme()
+  const { layoutName } = useLayout()
+  const compactMode = layoutName === 'small'
+  const rowHeight = compactMode ? null : 294
+
+  if (openedRepoId) {
+    const openedRepo = repos.find(repo => repo.appId === openedRepoId)
     return (
-      <Viewport>
-        {({ above, below }) => (
-          <ZoomableCards
-            currentId={openedRepoId}
-            renderCards={({ card }) => (
-              <AppsGrid>
-                {repos.map(repo =>
-                  card(
-                    repo.appId,
-                    <AppCardContent repo={repo} onOpen={onOpenApp} />
-                  )
-                )}
-              </AppsGrid>
-            )}
-            renderOpenedCard={({ currentId }) => {
-              const openedRepo = this.getOpenedRepoByAppId(currentId)
-              return (
-                <AppContent
-                  repo={openedRepo}
-                  repoVersions={
-                    below('large') && <RepoVersions repo={openedRepo} />
-                  }
-                  onRequestUpgrade={onRequestUpgrade}
-                />
-              )
-            }}
-            renderOpenedAside={
-              above('large') &&
-              (({ currentId }) => (
-                <RepoVersions repo={this.getOpenedRepoByAppId(currentId)} />
-              ))
-            }
-          />
-        )}
-      </Viewport>
+      <AppContent
+        repo={openedRepo}
+        repoVersions={<RepoVersions repo={openedRepo} />}
+        onRequestUpgrade={onRequestUpgrade}
+        onClose={onCloseRepo}
+      />
     )
   }
+
+  return (
+    <CardLayout columnWidthMin={30 * GU} rowHeight={rowHeight}>
+      {repos.map(repo => (
+        <AppCard repo={repo} onOpenApp={onOpenApp} key={repo.appId} />
+      ))}
+    </CardLayout>
+  )
+})
+
+InstalledApps.propTypes = {
+  repos: PropTypes.arrayOf(RepoType).isRequired,
+  openedRepoId: PropTypes.string,
+  onOpenApp: PropTypes.func.isRequired,
+  onRequestUpgrade: PropTypes.func.isRequired,
+}
+
+InstalledApps.defaultProps = {
+  openedRepoId: null,
+}
+
+const AppCard = React.memo(function AppCard({ repo, onOpenApp }) {
+  const { repoName, appId } = repo
+  const handleOpenApp = useCallback(() => {
+    onOpenApp(repoName)
+  }, [onOpenApp, repoName])
+
+  return (
+    <Card onClick={handleOpenApp}>
+      <AppCardContent repo={repo} />
+    </Card>
+  )
+})
+
+AppCard.propTypes = {
+  repo: RepoType.isRequired,
+  onOpenApp: PropTypes.func.isRequired,
 }
 
 export default InstalledApps
