d74b79de6837687313895d7363aa7de13fe9f0c6
Brett Sun <qisheng.brett.sun@gmail.com>
2019-08-22 00:15:16 +0200

    Organization: add component name

--- src/apps/Organization/Organization.js
+++ src/apps/Organization/Organization.js
@@ -23,222 +23,215 @@ import airdrop, { testTokensEnabled } from '../../testnet/airdrop'
 import { toChecksumAddress } from '../../web3-utils'
 import useAppWidth from '../useAppWidth'
 
-const Organization = React.memo(
-  ({
-    account,
-    apps,
-    appsLoading,
-    daoAddress,
-    onOpenApp,
-    walletNetwork,
-    walletWeb3,
-    walletProviderId,
-  }) => {
-    const theme = useTheme()
-    const { layoutName } = useLayout()
+const Organization = React.memo(function Organization({
+  account,
+  apps,
+  appsLoading,
+  daoAddress,
+  onOpenApp,
+  walletNetwork,
+  walletWeb3,
+  walletProviderId,
+}) {
+  const theme = useTheme()
+  const { layoutName } = useLayout()
 
-    const handleDepositTestTokens = useCallback(() => {
-      const finance = apps.find(app => app.appId === appIds.Finance)
-      if (finance && finance.proxyAddress) {
-        airdrop(walletWeb3, finance.proxyAddress, account)
-      }
-    }, [account, apps, walletWeb3])
-    const handleOpenAgentApp = useCallback(() => {
-      const agent = apps.find(app => app.appId === appIds.Agent)
-      if (agent && agent.proxyAddress) {
-        onOpenApp(agent.proxyAddress)
-      }
-    }, [apps, onOpenApp])
-    const handleOpenFinanceApp = useCallback(() => {
-      const finance = apps.find(app => app.appId === appIds.Finance)
-      if (finance && finance.proxyAddress) {
-        onOpenApp(finance.proxyAddress)
-      }
-    }, [apps, onOpenApp])
+  const handleDepositTestTokens = useCallback(() => {
+    const finance = apps.find(app => app.appId === appIds.Finance)
+    if (finance && finance.proxyAddress) {
+      airdrop(walletWeb3, finance.proxyAddress, account)
+    }
+  }, [account, apps, walletWeb3])
+  const handleOpenAgentApp = useCallback(() => {
+    const agent = apps.find(app => app.appId === appIds.Agent)
+    if (agent && agent.proxyAddress) {
+      onOpenApp(agent.proxyAddress)
+    }
+  }, [apps, onOpenApp])
+  const handleOpenFinanceApp = useCallback(() => {
+    const finance = apps.find(app => app.appId === appIds.Finance)
+    if (finance && finance.proxyAddress) {
+      onOpenApp(finance.proxyAddress)
+    }
+  }, [apps, onOpenApp])
 
-    const apmApps = apps.filter(app => !app.isAragonOsInternalApp)
-    const hasAgentApp = apps.some(app => app.appId === appIds.Agent)
-    const hasFinanceApp = apps.some(app => app.appId === appIds.Finance)
-    const checksummedDaoAddr =
-      daoAddress.address && toChecksumAddress(daoAddress.address)
-    const enableTransactions = !!account && walletNetwork === network.type
-    const shortAddresses = layoutName !== 'large'
+  const apmApps = apps.filter(app => !app.isAragonOsInternalApp)
+  const hasAgentApp = apps.some(app => app.appId === appIds.Agent)
+  const hasFinanceApp = apps.some(app => app.appId === appIds.Finance)
+  const checksummedDaoAddr =
+    daoAddress.address && toChecksumAddress(daoAddress.address)
+  const enableTransactions = !!account && walletNetwork === network.type
+  const shortAddresses = layoutName !== 'large'
 
-    const depositFundsHelpText = appsLoading ? (
-      ''
-    ) : hasFinanceApp || hasAgentApp ? (
-      <span>
-        If you'd like to deposit funds into this organization, you can do so
-        from{' '}
-        {hasFinanceApp ? (
-          <OpenAppButton onClick={handleOpenFinanceApp}>Finance</OpenAppButton>
-        ) : (
-          <OpenAppButton onClick={handleOpenAgentApp}>Agent</OpenAppButton>
-        )}
-        .
-      </span>
-    ) : (
-      `This organization does not have a Finance or Agent app installed and may
+  const depositFundsHelpText = appsLoading ? (
+    ''
+  ) : hasFinanceApp || hasAgentApp ? (
+    <span>
+      If you'd like to deposit funds into this organization, you can do so from{' '}
+      {hasFinanceApp ? (
+        <OpenAppButton onClick={handleOpenFinanceApp}>Finance</OpenAppButton>
+      ) : (
+        <OpenAppButton onClick={handleOpenAgentApp}>Agent</OpenAppButton>
+      )}
+      .
+    </span>
+  ) : (
+    `This organization does not have a Finance or Agent app installed and may
        not be able to receive funds. Please check with the organization's
        administrators if any other installed apps are able to receive funds.`
-    )
+  )
 
-    return (
-      <React.Fragment>
-        <Header primary="Organization Settings" />
-        <Section heading="Organization address">
+  return (
+    <React.Fragment>
+      <Header primary="Organization Settings" />
+      <Section heading="Organization address">
+        <p
+          css={`
+            ${textStyle('body2')}
+          `}
+        >
+          {checksummedDaoAddr
+            ? `This organization is deployed on the Ethereum ${network.name}.`
+            : 'Resolving DAO address…'}
+        </p>
+        {checksummedDaoAddr && (
+          <React.Fragment>
+            <div
+              css={`
+                margin-top: ${2 * GU}px;
+                margin-bottom: ${3 * GU}px;
+              `}
+            >
+              <LocalIdentityBadge
+                entity={checksummedDaoAddr}
+                shorten={shortAddresses}
+              />
+            </div>
+            <Info>
+              <strong css="font-weight: 800">
+                Do not send ETH or ERC20 tokens to this address.
+              </strong>{' '}
+              {depositFundsHelpText}
+            </Info>
+          </React.Fragment>
+        )}
+      </Section>
+      {hasFinanceApp && testTokensEnabled(network.type) && (
+        <Section heading="Request test tokens">
           <p
             css={`
+              margin-bottom: ${2 * GU}px;
               ${textStyle('body2')}
             `}
           >
-            {checksummedDaoAddr
-              ? `This organization is deployed on the Ethereum ${network.name}.`
-              : 'Resolving DAO address…'}
+            Deposit some tokens into your organization for testing purposes.
           </p>
-          {checksummedDaoAddr && (
-            <React.Fragment>
-              <div
+          <Button
+            label="Request test tokens"
+            icon={<IconCoin />}
+            display="all"
+            onClick={handleDepositTestTokens}
+            disabled={!enableTransactions}
+            css={`
+              margin-bottom: ${2 * GU}px;
+            `}
+          />
+          {enableTransactions ? (
+            <Info>
+              <p>
+                Requesting tokens will assign random{' '}
+                <strong css="font-weight: 800">test tokens</strong> to your
+                organization. These tokens are named after existing projects,
+                but keep in mind{' '}
+                <strong css="font-weight: 800">they are not real tokens</strong>
+                .
+              </p>
+              <p
                 css={`
-                  margin-top: ${2 * GU}px;
+                  margin-top: ${1 * GU}px;
+                `}
+              >
+                You can view the received tokens in{' '}
+                <OpenAppButton onClick={handleOpenFinanceApp}>
+                  Finance
+                </OpenAppButton>
+                .
+              </p>
+            </Info>
+          ) : (
+            <Info mode="warning">
+              {walletNetwork !== network.type
+                ? `Please select the ${sanitizeNetworkType(
+                    network.type
+                  )} network in your Ethereum provider.`
+                : `Please unlock your account in ${getProviderString(
+                    'your Ethereum provider',
+                    walletProviderId
+                  )}.`}
+            </Info>
+          )}
+        </Section>
+      )}
+      {appsLoading ? (
+        <Section heading="Installed Aragon apps">
+          <div
+            css={`
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              height: 180px;
+              ${textStyle('body2')}
+            `}
+          >
+            Loading apps…
+          </div>
+        </Section>
+      ) : (
+        <Section heading="Installed Aragon apps">
+          <ul
+            css={`
+              list-style: none;
+              display: grid;
+              grid-template-columns: minmax(50%, 1fr) minmax(50%, 1fr);
+              grid-column-gap: ${2 * GU}px;
+              margin-bottom: -${3 * GU}px;
+            `}
+          >
+            {apmApps.map(({ appId, description, name, proxyAddress, tags }) => (
+              <li
+                key={proxyAddress}
+                css={`
                   margin-bottom: ${3 * GU}px;
                 `}
               >
-                <LocalIdentityBadge
-                  entity={checksummedDaoAddr}
-                  shorten={shortAddresses}
-                />
-              </div>
-              <Info>
-                <strong css="font-weight: 800">
-                  Do not send ETH or ERC20 tokens to this address.
-                </strong>{' '}
-                {depositFundsHelpText}
-              </Info>
-            </React.Fragment>
-          )}
-        </Section>
-        {hasFinanceApp && testTokensEnabled(network.type) && (
-          <Section heading="Request test tokens">
-            <p
-              css={`
-                margin-bottom: ${2 * GU}px;
-                ${textStyle('body2')}
-              `}
-            >
-              Deposit some tokens into your organization for testing purposes.
-            </p>
-            <Button
-              label="Request test tokens"
-              icon={<IconCoin />}
-              display="all"
-              onClick={handleDepositTestTokens}
-              disabled={!enableTransactions}
-              css={`
-                margin-bottom: ${2 * GU}px;
-              `}
-            />
-            {enableTransactions ? (
-              <Info>
-                <p>
-                  Requesting tokens will assign random{' '}
-                  <strong css="font-weight: 800">test tokens</strong> to your
-                  organization. These tokens are named after existing projects,
-                  but keep in mind{' '}
-                  <strong css="font-weight: 800">
-                    they are not real tokens
-                  </strong>
-                  .
-                </p>
-                <p
+                <label
+                  css={`
+                    color: ${theme.surfaceContentSecondary};
+                    ${unselectable()};
+                    ${textStyle('label2')};
+                  `}
+                >
+                  {name}
+                  {tags.length > 0 ? ` (${tags.join(', ')})` : ''}
+                </label>
+                <div
                   css={`
                     margin-top: ${1 * GU}px;
                   `}
                 >
-                  You can view the received tokens in{' '}
-                  <OpenAppButton onClick={handleOpenFinanceApp}>
-                    Finance
-                  </OpenAppButton>
-                  .
-                </p>
-              </Info>
-            ) : (
-              <Info mode="warning">
-                {walletNetwork !== network.type
-                  ? `Please select the ${sanitizeNetworkType(
-                      network.type
-                    )} network in your Ethereum provider.`
-                  : `Please unlock your account in ${getProviderString(
-                      'your Ethereum provider',
-                      walletProviderId
-                    )}.`}
-              </Info>
-            )}
-          </Section>
-        )}
-        {appsLoading ? (
-          <Section heading="Installed Aragon apps">
-            <div
-              css={`
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                height: 180px;
-                ${textStyle('body2')}
-              `}
-            >
-              Loading apps…
-            </div>
-          </Section>
-        ) : (
-          <Section heading="Installed Aragon apps">
-            <ul
-              css={`
-                list-style: none;
-                display: grid;
-                grid-template-columns: minmax(50%, 1fr) minmax(50%, 1fr);
-                grid-column-gap: ${2 * GU}px;
-                margin-bottom: -${3 * GU}px;
-              `}
-            >
-              {apmApps.map(
-                ({ appId, description, name, proxyAddress, tags }) => (
-                  <li
-                    key={proxyAddress}
-                    css={`
-                      margin-bottom: ${3 * GU}px;
-                    `}
-                  >
-                    <label
-                      css={`
-                        color: ${theme.surfaceContentSecondary};
-                        ${unselectable()};
-                        ${textStyle('label2')};
-                      `}
-                    >
-                      {name}
-                      {tags.length > 0 ? ` (${tags.join(', ')})` : ''}
-                    </label>
-                    <div
-                      css={`
-                        margin-top: ${1 * GU}px;
-                      `}
-                    >
-                      <LocalIdentityBadge
-                        entity={proxyAddress}
-                        shorten={shortAddresses}
-                      />
-                    </div>
-                  </li>
-                )
-              )}
-            </ul>
-          </Section>
-        )}
-      </React.Fragment>
-    )
-  }
-)
+                  <LocalIdentityBadge
+                    entity={proxyAddress}
+                    shorten={shortAddresses}
+                  />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </Section>
+      )}
+    </React.Fragment>
+  )
+})
 
 Organization.propTypes = {
   account: EthereumAddressType,
