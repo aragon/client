47863266e43b891dd9d59970e356622b1f7ab1cb
Gorka <gorka@aquigorka.com>
2019-08-22 11:05:14 +0200

    RepoVersions: remove header

--- src/apps/AppCenter/InstalledApps/RepoVersions.js
+++ src/apps/AppCenter/InstalledApps/RepoVersions.js
@@ -2,96 +2,82 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { Trail, animated } from 'react-spring'
-import { Info, Tag, GU, theme, springs } from '@aragon/ui'
+import { Info, Tag, GU, useTheme, springs } from '@aragon/ui'
 import { format } from 'date-fns'
 import { TextLabel } from '../../../components/TextStyles'
 import { RepoType } from '../../../prop-types'
 
-const RepoVersions = ({ animate, repo: { currentVersion, versions } }) => (
-  <div
-    css={`
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-    `}
-  >
-    <table
-      css={`
-        border-collapse: collapse;
-        width: 100%;
-      `}
-    >
-      <thead>
-        <tr
-          css={`
-            th {
-              border-bottom: 1px solid ${theme.contentBorder};
-            }
-          `}
-        >
-          <Td as="th">
-            <TextLabel>App version</TextLabel>
-          </Td>
-          <Td as="th">
-            <TextLabel>Release date</TextLabel>
-          </Td>
-        </tr>
-      </thead>
-      <tbody>
-        <Trail
-          items={versions}
-          keys={v => v.version}
-          from={{ progress: 0 }}
-          to={{ progress: 1 }}
-          config={springs.smooth}
-          delay={150}
-          immediate={!animate}
-          native
-        >
-          {/* eslint-disable react/prop-types */
-          ({ timestamp, version }) => ({ progress }) => (
-            <BodyTr
-              key={name}
-              style={{
-                opacity: progress,
-                transform: progress.interpolate(
-                  v => `translate3d(${10 * (1 - v)}%, 0, 0)`
-                ),
-              }}
-            >
-              <Td>
-                {version}{' '}
-                {version === currentVersion.version && (
-                  <Tag
-                    css={`
-                      margin-left: ${1 * GU}px;
-                    `}
-                  >
-                    current
-                  </Tag>
-                )}
-              </Td>
-              <Td>{timestamp ? format(timestamp, 'dd/MM/yy') : ''}</Td>
-            </BodyTr>
-          )
-          /* eslint-enable react/prop-types */
-          }
-        </Trail>
-      </tbody>
-    </table>
-
+const RepoVersions = ({ animate, repo: { currentVersion, versions } }) => {
+  return (
     <div
       css={`
-        margin-top: ${2 * GU}px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
       `}
     >
-      <Info.Action>
-        Minor and patch upgrades are front-end only, and are performed
-        automatically.
-      </Info.Action>
+      <table
+        css={`
+          border-collapse: collapse;
+          width: 100%;
+        `}
+      >
+        <tbody>
+          <Trail
+            items={versions}
+            keys={v => v.version}
+            from={{ progress: 0 }}
+            to={{ progress: 1 }}
+            config={springs.smooth}
+            delay={150}
+            immediate={!animate}
+            native
+          >
+            {/* eslint-disable react/prop-types */
+            ({ timestamp, version }) => ({ progress }) => (
+              <BodyTr
+                key={name}
+                style={{
+                  opacity: progress,
+                  transform: progress.interpolate(
+                    v => `translate3d(${10 * (1 - v)}%, 0, 0)`
+                  ),
+                }}
+              >
+                <Td>
+                  {version}{' '}
+                  {version === currentVersion.version && (
+                    <Tag
+                      css={`
+                        margin-left: ${1 * GU}px;
+                      `}
+                    >
+                      current
+                    </Tag>
+                  )}
+                </Td>
+                <Td>{timestamp ? format(timestamp, 'dd/MM/yy') : ''}</Td>
+              </BodyTr>
+            )
+            /* eslint-enable react/prop-types */
+            }
+          </Trail>
+        </tbody>
+      </table>
+
+      <div
+        css={`
+          margin-top: ${2 * GU}px;
+        `}
+      >
+        <Info.Action>
+          Minor and patch upgrades are front-end only, and are performed
+          automatically.
+        </Info.Action>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 RepoVersions.propTypes = {
   repo: RepoType.isRequired,
