dcc548abbc17fa89d1ec96258b7c3bbd66e0cdf8
Gorka <gorka@aquigorka.com>
2019-08-22 16:54:12 +0200

    DiscoverApps: add smaller card view for smaller screens

--- src/apps/AppCenter/DiscoverApps/DiscoverApps.js
+++ src/apps/AppCenter/DiscoverApps/DiscoverApps.js
@@ -21,12 +21,14 @@ const DiscoverApps = React.memo(() => {
   const theme = useTheme()
   const { layoutName } = useLayout()
   const compactMode = layoutName === 'small'
-  const rowHeight = compactMode ? null : 294
+  const rowHeight = compactMode ? 168 : 294
+  const columnWidthMin = compactMode ? 328 : 30 * GU
 
   return (
     <React.Fragment>
       <Info
         css={`
+          margin: 0 ${compactMode ? 2 * GU : 0}px;
           margin-bottom: ${2 * GU}px;
         `}
       >
@@ -44,24 +46,27 @@ const DiscoverApps = React.memo(() => {
         </Link>
         You can also preview some of the apps being developed.
       </Info>
-      <CardLayout columnWidthMin={30 * GU} rowHeight={rowHeight}>
+      <CardLayout columnWidthMin={columnWidthMin} rowHeight={rowHeight}>
         {appsInDevelopment.map((app, i) => (
-          <Main key={i} columnWidthMin={30 * GU} rowHeight={rowHeight}>
+          <Main key={i} compactMode={compactMode}>
             {app.link && (
               <ExternalButton
+                compactMode={compactMode}
                 href={app.link}
                 target="_blank"
                 icon={<IconExternal size="tiny" />}
               />
             )}
-            <Icon>
+            <Icon compactMode={compactMode}>
               <AppIcon size={9 * GU} src={app.icon} />
             </Icon>
-            <Name>{app.name}</Name>
-            <TagWrapper>
+            <Name compactMode={compactMode}>{app.name}</Name>
+            <TagWrapper compactMode={compactMode}>
               <Tag mode="indicator">{app.status}</Tag>
             </TagWrapper>
-            <Description theme={theme}>{app.description}</Description>
+            <Description theme={theme} compactMode={compactMode}>
+              {app.description}
+            </Description>
           </Main>
         ))}
       </CardLayout>
@@ -73,48 +78,107 @@ const Main = styled(Card)`
   ${unselectable};
   position: relative;
   overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-top: ${5 * GU}px;
+  height: 100%;
+  width: 100%;
+
+  ${({ compactMode }) =>
+    compactMode
+      ? `
+          display: grid;
+          grid-template-columns: auto 1fr;
+          grid-template-rows: auto 1fr 1fr;
+          grid-template-areas:
+            "external tag"
+            "icon title"
+            "icon description";
+          padding: ${1.5 * GU}px ${1.5 * GU}px ${4 * GU}px ${3 * GU}px;
+        `
+      : `
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          padding-top: ${5 * GU}px;
+        `}
 `
 
 const Icon = styled.div`
   height: ${9 * GU}px;
-  margin-bottom: ${2 * GU}px;
-  img {
-    display: block;
-  }
+  width: ${9 * GU}px;
+  ${({ compactMode }) =>
+    compactMode
+      ? `
+        grid-area: icon;
+        margin-right: ${1.5 * GU}px;
+      `
+      : `
+        margin-bottom: ${2 * GU}px;
+      `}
 `
 
 const Name = styled.p`
   display: flex;
   width: 100%;
-  justify-content: center;
-  margin-bottom: ${1 * GU}px;
   ${textStyle('title4')}
+  ${({ compactMode }) =>
+    compactMode
+      ? `
+        grid-area: title;
+        align-self: flex-end;
+      `
+      : `
+        justify-content: center;
+        margin-bottom: ${1 * GU}px;
+      `}
 `
 
 const TagWrapper = styled.div`
-  max-width: 100%;
-  padding: 0 ${2.5 * GU}px;
-  margin-bottom: ${1 * GU}px;
+  ${({ compactMode }) =>
+    compactMode
+      ? `
+        grid-area: tag;
+        text-align: right;
+      `
+      : `
+        max-width: 100%;
+        padding: 0 ${2.5 * GU}px;
+        margin-bottom: ${1 * GU}px;
+      `}
 `
 
 const Description = styled.p`
   color: ${({ theme }) => theme.contentSecondary};
-  padding: 0 1rem;
-  text-align: center;
   ${textStyle('body2')};
-  flex: 1;
+  ${({ compactMode }) =>
+    compactMode
+      ? `
+        grid-area: description;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 2;
+        overflow: hidden;
+        height: fit-content;
+      `
+      : `
+        flex: 1;
+        text-align: center;
+        padding: 0 1rem;
+      `}
 `
 
 const ExternalButton = styled(Button)`
-  position: absolute;
-  top: ${2 * GU}px;
-  right: ${2 * GU}px;
   width: ${3.5 * GU}px;
   height: ${3.5 * GU}px;
+
+  ${({ compactMode }) =>
+    compactMode
+      ? `
+          grid-area: external;
+        `
+      : `
+          position: absolute;
+          top: ${2 * GU}px;
+          right: ${2 * GU}px;
+        `}
 `
 
 export default DiscoverApps
