d1555e3f9f3b673d9cc3aea795e3d489674f7a98
Gorka <gorka@aquigorka.com>
2019-08-22 16:54:42 +0200

    AppCardContent: update view for compact mode

--- src/apps/AppCenter/InstalledApps/AppCardContent.js
+++ src/apps/AppCenter/InstalledApps/AppCardContent.js
@@ -3,7 +3,7 @@ import { GU, Tag, unselectable, useTheme, textStyle } from '@aragon/ui'
 import AppIcon from '../../../components/AppIcon/AppIcon'
 import { RepoType } from '../../../prop-types'
 
-const AppCardContent = ({ repo }) => {
+const AppCardContent = ({ repo, compactMode }) => {
   const theme = useTheme()
   const { name, baseUrl, currentVersion, latestVersion } = repo
   const { description, icons } = latestVersion.content
@@ -15,21 +15,40 @@ const AppCardContent = ({ repo }) => {
         ${unselectable};
         position: relative;
         overflow: hidden;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        padding-top: ${5 * GU}px;
         height: 100%;
         width: 100%;
+
+        ${compactMode
+          ? `
+              display: grid;
+              grid-template-columns: auto 1fr;
+              grid-template-rows: auto 1fr 1fr;
+              grid-template-areas:
+                "tag tag"
+                "icon title"
+                "icon description";
+              padding: ${1.5 * GU}px ${1.5 * GU}px ${4 * GU}px ${3 * GU}px;
+            `
+          : `
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              padding-top: ${5 * GU}px;
+            `}
       `}
     >
       <div
         css={`
           height: ${9 * GU}px;
-          margin-bottom: ${2 * GU}px;
-          img {
-            display: block;
-          }
+          width: ${9 * GU}px;
+          ${compactMode
+            ? `
+                grid-area: icon;
+                margin-right: ${1.5 * GU}px;
+              `
+            : `
+                margin-bottom: ${2 * GU}px;
+              `}
         `}
       >
         <AppIcon app={{ baseUrl, icons }} size={9 * GU} />
@@ -38,18 +57,32 @@ const AppCardContent = ({ repo }) => {
         css={`
           display: flex;
           width: 100%;
-          justify-content: center;
-          margin-bottom: ${1 * GU}px;
           ${textStyle('title4')}
+          ${compactMode
+            ? `
+                grid-area: title;
+                align-self: flex-end;
+              `
+            : `
+                justify-content: center;
+                margin-bottom: ${1 * GU}px;
+              `}
         `}
       >
         {name}
       </p>
       <div
         css={`
-          max-width: 100%;
-          padding: 0 ${2.5 * GU}px;
-          margin-bottom: ${1 * GU}px;
+          ${compactMode
+            ? `
+                grid-area: tag;
+                text-align: right;
+              `
+            : `
+                max-width: 100%;
+                padding: 0 ${2.5 * GU}px;
+                margin-bottom: ${1 * GU}px;
+              `}
         `}
       >
         <Tag mode={canUpgrade ? 'new' : 'indicator'}>
@@ -59,10 +92,21 @@ const AppCardContent = ({ repo }) => {
       <p
         css={`
           color: ${theme.contentSecondary};
-          padding: 0 1rem;
-          text-align: center;
           ${textStyle('body2')};
-          flex: 1;
+          ${compactMode
+            ? `
+                grid-area: description;
+                display: -webkit-box;
+                -webkit-box-orient: vertical;
+                -webkit-line-clamp: 2;
+                overflow: hidden;
+                height: fit-content;
+              `
+            : `
+                flex: 1;
+                text-align: center;
+                padding: 0 1rem;
+              `}
         `}
       >
         {description}
