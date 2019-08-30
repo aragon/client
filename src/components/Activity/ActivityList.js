efaefe5d4bf3753946a0c05ed7cf3653a94cf263
Brett Sun <qisheng.brett.sun@gmail.com>
2019-08-30 20:05:59 +0200

    Activity: add empty state

--- src/components/Activity/ActivityList.js
+++ src/components/Activity/ActivityList.js
@@ -9,8 +9,10 @@ import {
   useTheme,
   useViewport,
 } from '@aragon/ui'
+import activityNoResults from '../../assets/activity-no-results.png'
 import { ActivityContext } from '../../contexts/ActivityContext'
 import { AppType } from '../../prop-types'
+import { ACTIVITY_STATUS_PENDING } from '../../symbols'
 import { addressesEqual } from '../../web3-utils'
 import ActivityItem from './ActivityItem'
 
@@ -37,6 +39,11 @@ function ActivityList({ apps }) {
     [activities, apps]
   )
 
+  const canClear = useMemo(
+    () =>
+      activityItems.some(({ status }) => status !== ACTIVITY_STATUS_PENDING),
+    [activityItems]
+  )
   const maxHeight = Math.max(
     MIN_HEIGHT_CLAMP,
     Math.min(
@@ -51,76 +58,99 @@ function ActivityList({ apps }) {
         width: ${42 * GU}px;
       `}
     >
-      {activityItems.length > 0 && (
-        <React.Fragment>
-          <div
-            css={`
-              display: flex;
-              align-items: center;
-              justify-content: space-between;
-              height: ${HEADER_HEIGHT}px;
-              padding: 0 ${2 * GU}px;
-              border-bottom: 1px solid ${theme.border};
-            `}
+      <div
+        css={`
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          height: ${HEADER_HEIGHT}px;
+          padding: 0 ${2 * GU}px;
+          border-bottom: 1px solid ${theme.border};
+        `}
+      >
+        <label
+          css={`
+            ${textStyle('label2')}
+            color: ${theme.surfaceContentSecondary};
+          `}
+        >
+          Activity
+        </label>
+        <ButtonText
+          onClick={clearActivities}
+          css={`
+            padding: 0;
+            ${textStyle('label2')}
+          `}
+        >
+          Clear all
+        </ButtonText>
+      </div>
+      {activityItems.length > 0 ? (
+        <div
+          css={`
+            max-height: ${maxHeight}px;
+            overflow-x: hidden;
+            overflow-y: scroll;
+          `}
+        >
+          <Transition
+            native
+            items={activityItems}
+            keys={activity => activity.transactionHash}
+            trail={50}
+            enter={{
+              opacity: 1,
+              transform: 'translate3d(0px, 0, 0)',
+            }}
+            leave={{
+              opacity: 0,
+              transform: 'translate3d(20px, 0, 0)',
+            }}
+            config={springs.smooth}
           >
-            <label
-              css={`
-                ${textStyle('label2')}
-                color: ${theme.surfaceContentSecondary};
-              `}
-            >
-              Activity
-            </label>
-            <ButtonText
-              onClick={clearActivities}
-              css={`
-                padding: 0;
-                ${textStyle('label2')}
-              `}
-            >
-              Clear all
-            </ButtonText>
-          </div>
-          <div
-            css={`
-              max-height: ${maxHeight}px;
-              overflow-x: hidden;
-              overflow-y: scroll;
-            `}
-          >
-            <Transition
-              native
-              items={activityItems}
-              keys={activity => activity.transactionHash}
-              trail={50}
-              enter={{
-                opacity: 1,
-                transform: 'translate3d(0px, 0, 0)',
-              }}
-              leave={{
-                opacity: 0,
-                transform: 'translate3d(20px, 0, 0)',
-              }}
-              config={springs.smooth}
-            >
-              {activity => transitionStyles => (
-                <div
-                  css={`
-                    & + & {
-                      border-top: 1px solid ${theme.border};
-                    }
-                  `}
+            {activity => transitionStyles => (
+              <div
+                css={`
+                  & + & {
+                    border-top: 1px solid ${theme.border};
+                  }
+                `}
+              >
+                <animated.div
+                  style={{ ...transitionStyles, overflow: 'hidden' }}
                 >
-                  <animated.div
-                    style={{ ...transitionStyles, overflow: 'hidden' }}
-                  >
-                    <ActivityItem activity={activity} />
-                  </animated.div>
-                </div>
-              )}
-            </Transition>
-          </div>
-        </React.Fragment>
+                  <ActivityItem activity={activity} />
+                </animated.div>
+              </div>
+            )}
+          </Transition>
+        </div>
+      ) : (
+        <div
+          css={`
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: ${28.5 * GU}px;
+          `}
+        >
+          <img
+            src={activityNoResults}
+            alt="No results"
+            height="125px"
+            width="125px"
+          />
+          <span
+            css={`
+              margin-top: ${2 * GU}px;
+              ${textStyle('body2')}
+            `}
+          >
+            No activity yet!
+          </span>
+        </div>
       )}
     </div>
   )
