689f354163d9a3223ce094a5045452218a0a6816
Pierre Bertet <hello@pierre.world>
2019-09-03 05:28:57 +0200

    Add templates base

---
+++ src/templates/membership.js
@@ -0,0 +1,23 @@
+import membershipHeader from './assets/header-membership.svg'
+import membershipIcon from './assets/icon-membership.svg'
+
+export default {
+  id: 'membership',
+  name: 'Membership',
+  header: membershipHeader,
+  icon: membershipIcon,
+  description: `
+      Use a non-transferrable token to represent membership. Decisions can be
+      made based on one-member-one-vote governance.
+    `,
+  longdesc: `
+      The different type of business goals mentioned above can help you
+      determine which priorities you want to go after. You can use them as a
+      reference for adjustments and future decisions, and they can aid in
+      creating the right strategies for accomplishing your company goals and
+      objectives.
+    `,
+  caseStudyUrl: 'https://aragon.org/case-study/startup',
+  sourceCodeUrl: 'aragon/aragon/dao-kit-startup',
+  registry: 'aragonpm.eth',
+}
---
+++ src/templates/reputation.js
@@ -0,0 +1,23 @@
+import reputationHeader from './assets/header-reputation.svg'
+import reputationIcon from './assets/icon-reputation.svg'
+
+export default {
+  id: 'reputation',
+  name: 'Reputation',
+  header: reputationHeader,
+  icon: reputationIcon,
+  description: `
+      Use non-transferrable tokens to represent reputation. Decisions are made
+      using reputation-weighted voting.
+    `,
+  longdesc: `
+      The different type of business goals mentioned above can help you
+      determine which priorities you want to go after. You can use them as a
+      reference for adjustments and future decisions, and they can aid in
+      creating the right strategies for accomplishing your company goals and
+      objectives.
+    `,
+  caseStudyUrl: 'https://aragon.org/case-study/startup',
+  sourceCodeUrl: 'aragon/aragon/dao-kit-startup',
+  registry: 'aragonpm.eth',
+}
