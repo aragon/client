e6a6463d248d2e44207ccdebf668cddeef0b5528
Gorka <gorka@aquigorka.com>
2019-09-04 15:02:10 +0200

    DAONotFoundError: update styles and container component

--- src/components/Error/DAONotFoundError.js
+++ src/components/Error/DAONotFoundError.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useTheme, textStyle, ButtonText, Card, GU, Info } from '@aragon/ui'
+import {
+  useTheme,
+  textStyle,
+  ButtonText,
+  EmptyStateCard,
+  GU,
+  Info,
+} from '@aragon/ui'
 import { network } from '../../environment'
 import { isAddress } from '../../web3-utils'
 import notFoundImage from '../../assets/dao-not-found.png'
@@ -8,64 +15,72 @@ import notFoundImage from '../../assets/dao-not-found.png'
 function DAONotFoundError({ dao }) {
   const theme = useTheme()
   return (
-    <Card
+    <EmptyStateCard
       css={`
         display: grid;
         grid-template-rows: 1fr auto auto auto;
         grid-template-columns: 1fr;
-        text-align: center;
         padding: ${2 * GU}px;
         width: 579px;
         height: auto;
         box-shadow: 0px 2px 4px rgba(180, 188, 202, 0.5);
       `}
-    >
-      <img
-        src={notFoundImage}
-        alt="DAO not found"
-        css={`
-          width: 147px;
-          height: 144px;
-          margin: auto;
-          margin-bottom: ${1.5 * GU}px;
-        `}
-      />
-      <h1
-        css={`
-          color: ${theme.feedbackSurfaceContent};
-          ${textStyle('title2')};
-          margin-bottom: ${1.5 * GU}px;
-        `}
-      >
-        Organization not found
-      </h1>
-      <div
-        css={`
-          color: ${theme.feedbackSurfaceContentSecondary};
-          ${textStyle('body2')};
-          margin: auto;
-          margin-bottom: ${6 * GU}px;
-          max-width: 417px;
-        `}
-      >
-        It looks like there's no organization associated with{' '}
-        {isAddress(dao) ? (
-          <span css="font-weight: bold;">“{dao}”</span>
-        ) : (
-          <React.Fragment>
-            the <span css="font-weight: bold;">“{dao}”</span> ENS domain
-          </React.Fragment>
-        )}
-        on the Ethereum {network.name}.
-      </div>
-      <Info>
-        If you arrived here through a link, please double check that you were
-        given the correct link. Alternatively, you may
-        <ButtonText onClick={() => window.location.reload(true)}>
-          create a new organization
-        </ButtonText>
-      </Info>
-    </Card>
+      illustration={
+        <img
+          src={notFoundImage}
+          alt="DAO not found"
+          css={`
+            width: 147px;
+            height: 144px;
+            margin: ${5 * GU}px auto ${1.5 * GU}px;
+          `}
+        />
+      }
+      text={
+        <React.Fragment>
+          <h1
+            css={`
+              color: ${theme.feedbackSurfaceContent};
+              ${textStyle('title2')};
+              margin-bottom: ${1.5 * GU}px;
+            `}
+          >
+            Organization not found
+          </h1>
+          <div
+            css={`
+              color: ${theme.feedbackSurfaceContentSecondary};
+              ${textStyle('body2')};
+              margin: auto;
+              margin-bottom: ${6 * GU}px;
+              max-width: 417px;
+            `}
+          >
+            It looks like there's no organization associated with{' '}
+            {isAddress(dao) ? (
+              <span css="font-weight: bold;">“{dao}”</span>
+            ) : (
+              <React.Fragment>
+                the <span css="font-weight: bold;">“{dao}”</span> ENS domain
+              </React.Fragment>
+            )}
+            on the Ethereum {network.name}.
+          </div>
+        </React.Fragment>
+      }
+      action={
+        <Info css="text-align: left;">
+          If you arrived here through a link, please double check that you were
+          given the correct link. Alternatively, you may
+          <ButtonText
+            onClick={() => window.location.reload(true)}
+            css="margin-left: -5px;"
+          >
+            create a new organization.
+          </ButtonText>
+        </Info>
+      }
+    />
   )
 }
 
