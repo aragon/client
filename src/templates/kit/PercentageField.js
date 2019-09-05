ff87578daf12c81666a815857dadfe96d49cab48
Pierre Bertet <hello@pierre.world>
2019-09-05 03:39:07 +0200

    Template kit: adapt PercentageField to small screens + other tweaks

--- src/templates/kit/PercentageField.js
+++ src/templates/kit/PercentageField.js
@@ -1,7 +1,9 @@
 import React, { useCallback } from 'react'
-import { Field, GU, Slider, TextInput } from '@aragon/ui'
+import { Field, GU, Slider, TextInput, textStyle, useTheme } from '@aragon/ui'
 
 function PercentageField({ label = 'Percentage', value, onChange }) {
+  const theme = useTheme()
+
   const handleSliderChange = useCallback(
     v => {
       onChange(Math.round(v * 100))
@@ -19,33 +21,60 @@ function PercentageField({ label = 'Percentage', value, onChange }) {
   )
   return (
     <Field label={label}>
-      <div
-        css={`
-          display: flex;
-          flex-direction: row;
-        `}
-      >
-        <Slider
-          value={value / 100}
-          onUpdate={handleSliderChange}
+      {({ id }) => (
+        <div
           css={`
-            flex-grow: 1;
-            padding-left: 0;
-            padding-right: 0;
-            margin-right: ${3 * GU}px;
+            display: flex;
+            flex-direction: row;
           `}
-        />
-        <TextInput
-          value={`${value}%`}
-          onChange={handleInputChange}
-          css={`
-            flex-grow: 0;
-            flex-shrink: 0;
-            width: ${12 * GU}px;
-            text-align: center;
-          `}
-        />
-      </div>
+        >
+          <Slider
+            value={value / 100}
+            onUpdate={handleSliderChange}
+            css={`
+              flex-grow: 1;
+              padding-left: 0;
+              padding-right: 0;
+              margin-right: ${3 * GU}px;
+              min-width: ${20 * GU}px;
+            `}
+          />
+          <div
+            css={`
+              position: relative;
+              flex-grow: 0;
+              flex-shrink: 0;
+              width: ${8 * GU}px;
+            `}
+          >
+            <TextInput
+              id={id}
+              value={value}
+              onChange={handleInputChange}
+              wide
+              css={`
+                padding-right: ${3.5 * GU}px;
+                text-align: right;
+              `}
+            />
+            <span
+              css={`
+                position: absolute;
+                top: 0;
+                right: ${1.5 * GU}px;
+                bottom: 0;
+                display: flex;
+                align-items: center;
+                ${textStyle('body3')};
+                color: ${theme.surfaceContent};
+                pointer-events: none;
+              `}
+            >
+              %
+            </span>
+          </div>
+        </div>
+      )}
     </Field>
   )
 }
