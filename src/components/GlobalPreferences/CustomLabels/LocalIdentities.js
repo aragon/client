626d156e4fa6135eb144849b5f50143d67ccf1e2
Pierre Bertet <hello@pierre.world>
2019-09-08 19:28:29 +0200

    Custom labels: layout fix (#971)

--- src/components/GlobalPreferences/CustomLabels/LocalIdentities.js
+++ src/components/GlobalPreferences/CustomLabels/LocalIdentities.js
@@ -409,7 +409,7 @@ const List = styled.ul`
   padding: 0;
   list-style: none;
   overflow: hidden;
-  width: calc(100% + ${5 * GU}px);
+  width: calc(100% + ${6 * GU}px);
   position: relative;
   left: -${3 * GU}px;
   background: ${({ surface }) => surface};
