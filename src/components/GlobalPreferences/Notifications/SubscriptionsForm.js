297520e4dab75876113ff28eaa442a21e523f91e
Luis Iván Cuende <me@luisivan.net>
2019-09-09 20:56:00 +0200

    Add main button style to 'Subscribe' action (#992)

--- src/components/GlobalPreferences/Notifications/SubscriptionsForm.js
+++ src/components/GlobalPreferences/Notifications/SubscriptionsForm.js
@@ -260,6 +260,7 @@ export default function SubscriptionsForm({
         `}
       >
         <Button
+          mode="strong"
           wide={layoutName === 'small'}
           disabled={isSubscribeDisabled}
           onClick={handleSubscribe}
