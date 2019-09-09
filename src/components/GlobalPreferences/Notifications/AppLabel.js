b8f4e114fa8abf853ac9d1875f4d6a680ffb9828
Gorka <gorka@aquigorka.com>
2019-09-09 18:57:54 +0100

    AppLabel: refactor to use AppBadge

--- src/components/GlobalPreferences/Notifications/AppLabel.js
+++ src/components/GlobalPreferences/Notifications/AppLabel.js
@@ -1,50 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
-import { GU, RADIUS, Tag } from '@aragon/ui'
+import { AppBadge, GU, RADIUS, Tag } from '@aragon/ui'
 import AppIcon from '../../AppIcon/AppIcon'
 import { useLocalIdentity } from '../../../hooks'
 import { AppType } from '../../../prop-types'
 
-const AppLabel = React.memo(function AppLabel({ apps, app }) {
-  const { name: appName, proxyAddress, contractAddress, identifier } = app
+const AppLabel = React.memo(function AppLabel({ apps, app, noIdentifier }) {
+  const {
+    name: appName,
+    proxyAddress,
+    contractAddress,
+    identifier,
+    icons: [{ src: iconSrc }],
+  } = app
   const [label, setLabel] = useState(appName)
   const { name } = useLocalIdentity(proxyAddress)
   const onlyOneInstance =
     apps.filter(a => a.contractAddress === contractAddress).length === 1
 
   useEffect(() => {
-    const getLabel = async () => {
-      // in order of priority
-      // custom label
-      // if more than one instance app name + app identifier
-      // app name
-      if (name) {
-        setLabel(name)
-        return
-      }
-
-      if (!onlyOneInstance && identifier) {
-        setLabel(
-          <span>
-            {appName}{' '}
-            <Tag
-              mode="identifier"
-              css={`
-                margin-left: ${1 * GU}px;
-              `}
-            >
-              {identifier}
-            </Tag>
-          </span>
-        )
-        return
-      }
-
-      setLabel(appName)
-    }
-
-    getLabel()
-  }, [appName, identifier, onlyOneInstance, apps, name])
+    setLabel(name || appName)
+  }, [appName, name])
 
   return (
     <div
@@ -53,15 +29,22 @@ const AppLabel = React.memo(function AppLabel({ apps, app }) {
         align-items: center;
       `}
     >
-      <AppIcon
-        app={app}
-        radius={RADIUS}
-        css={`
-          width: ${3 * GU}px;
-          margin-right: ${1 * GU}px;
-        `}
+      <AppBadge
+        appAddress={app.contractAddress}
+        label={label}
+        compact={true}
+        iconSrc={iconSrc}
       />
-      {label}
+      {!onlyOneInstance && !noIdentifier && !name && (
+        <Tag
+          mode="identifier"
+          css={`
+            margin-left: ${1 * GU}px;
+          `}
+        >
+          {identifier}
+        </Tag>
+      )}
     </div>
   )
 })
