250512f4bdbe7ba7a57a20e467d127bc56d54b09
Gorka <gorka@aquigorka.com>
2019-09-09 19:40:26 +0100

    Add accompanying local label popover action and title components

--- src/components/LocalLabelAppBadge/LocalLabelAppBadge.js
+++ src/components/LocalLabelAppBadge/LocalLabelAppBadge.js
@@ -1,14 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useCallback, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { AppBadge, GU, Tag } from '@aragon/ui'
 import { useLocalIdentity } from '../../hooks'
 import { AppType } from '../../prop-types'
+import LocalLabelPopoverTitle from './LocalLabelPopoverTitle'
+import LocalLabelPopoverActionLabel from './LocalLabelPopoverActionLabel'
+import { LocalIdentityModalContext } from '../LocalIdentityModal/LocalIdentityModalManager'
+import {
+  IdentityContext,
+  identityEventTypes,
+} from '../IdentityManager/IdentityManager'
 
 const LocalLabelAppBadge = React.memo(function LocalLabelAppBadge({
   apps,
   app,
   noIdentifier,
-  editLabel,
+  badgeOnly,
   ...props
 }) {
   const {
@@ -19,13 +26,26 @@ const LocalLabelAppBadge = React.memo(function LocalLabelAppBadge({
     icons: [{ src: iconSrc }],
   } = app
   const [label, setLabel] = useState(appName)
-  const { name } = useLocalIdentity(proxyAddress)
+  const { name, handleResolve } = useLocalIdentity(proxyAddress)
+  const { showLocalIdentityModal } = useContext(LocalIdentityModalContext)
+  const { identityEvents$ } = useContext(IdentityContext)
   const onlyOneInstance =
     apps.filter(a => a.contractAddress === contractAddress).length === 1
 
   useEffect(() => {
     setLabel(name || appName)
   }, [appName, name])
+
+  const handleClick = useCallback(() => {
+    showLocalIdentityModal(proxyAddress)
+      .then(handleResolve)
+      .then(() =>
+        identityEvents$.next({ type: identityEventTypes.MODIFY, address })
+      )
+      .catch(e => {
+        /* user cancelled modify intent */
+      })
+  }, [proxyAddress, identityEvents$, handleResolve, showLocalIdentityModal])
 
   return (
     <div
@@ -39,6 +59,13 @@ const LocalLabelAppBadge = React.memo(function LocalLabelAppBadge({
         label={label}
         iconSrc={iconSrc}
         {...props}
+        popoverAction={{
+          label: <LocalLabelPopoverActionLabel hasLabel={Boolean(label)} />,
+          onClick: handleClick,
+        }}
+        popoverTitle={
+          label ? <LocalLabelPopoverTitle label={label} /> : undefined
+        }
       />
       {!onlyOneInstance && !noIdentifier && !name && (
         <Tag
