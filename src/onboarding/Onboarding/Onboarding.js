3164a37ed5986b668ec016291a484c5150b8761a
Brett Sun <qisheng.brett.sun@gmail.com>
2019-10-10 15:05:53 +0200

    Onboarding: fix ENS resolution on determining whether template is available or not

--- src/onboarding/Onboarding/Onboarding.js
+++ src/onboarding/Onboarding/Onboarding.js
@@ -129,19 +129,21 @@ function Onboarding({ status, selectorNetworks, walletWeb3, web3 }) {
     if (status === 'create' && !templatesResolved) {
       Promise.all(
         embeddedTemplates.map(async template => {
+          let repoAddress
           try {
-            const repoAddress = await resolveEnsDomain(template.id)
-            return {
-              repoAddress,
-              status: TEMPLATE_AVAILABLE,
-              ...template,
-            }
-          } catch (_) {
-            return {
-              status: TEMPLATE_UNAVAILABLE,
-              ...template,
-            }
-          }
+            repoAddress = await resolveEnsDomain(template.id)
+          } catch (_) {}
+
+          return repoAddress
+            ? {
+                repoAddress,
+                status: TEMPLATE_AVAILABLE,
+                ...template,
+              }
+            : {
+                status: TEMPLATE_UNAVAILABLE,
+                ...template,
+              }
         })
       )
         .then(templatesWithRepoAddress => {
