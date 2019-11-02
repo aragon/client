a22a1b584fa21bf7d21c53518af391fb49abefc6
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2019-11-01 22:34:18 -0300

    remove import

--- src/templates/dandelion/config/index.js
+++ src/templates/dandelion/config/index.js
@@ -1,3 +1,2 @@
 export { default as VotingScreen } from './screens/VotingScreen'
-export { default as DelayScreen } from './screens/DelayScreen'
 export { default as LockScreen } from './screens/LockScreen'
