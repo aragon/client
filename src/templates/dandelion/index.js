a6f5b158e4799da37065b153f38b0f4ae205f33b
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2019-11-01 22:54:16 -0300

    Set dandelion as new

--- src/templates/dandelion/index.js
+++ src/templates/dandelion/index.js
@@ -24,6 +24,7 @@ function completeDomain(domain) {
 export default {
   id: 'dandelion-org-template-staging.open.aragonpm.eth',
   name: 'Dandelion',
+  new: true,
   header,
   icon,
   description: `
