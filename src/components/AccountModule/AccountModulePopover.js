03c17db266b2892deea1e0f7271b96eaaeb2e79a
Pierre Bertet <hello@pierre.world>
2020-01-30 16:37:24 +0000

    ESLint fix

--- src/components/AccountModule/AccountModulePopover.js
+++ src/components/AccountModule/AccountModulePopover.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { GU, Popover, springs, textStyle, useTheme } from '@aragon/ui'
 import { Spring, animated } from 'react-spring'
