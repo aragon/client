b02e5e1880bbac40dfef9076fa11325f1caad4be
Pierre Bertet <hello@pierre.world>
2020-02-14 21:45:34 +0000

    Coding style

--- src/components/AccountModule/useWalletSyncState.js
+++ src/components/AccountModule/useWalletSyncState.js
@@ -19,41 +19,44 @@ export function useWalletSyncState(
 ) {
   const { balance } = useWallet()
   const minimumTransactionBalance = new BN(0.005)
-  let syncInfo = { header: '', info: '', message: '' }
 
   if (!clientOnline || !clientListening) {
-    syncInfo = {
+    return {
       header: '',
       info: '',
       message: STATUS_CLIENT_CONNECTION_DROPPED,
     }
-  } else if (!walletListening) {
-    syncInfo = {
+  }
+
+  if (!walletListening) {
+    return {
       header: '',
       info: '',
       message: STATUS_WALLET_CONNECTION_DROPPED,
     }
-  } else if (clientSyncDelay >= 30 && walletSyncDelay >= 30) {
-    syncInfo = {
+  }
+
+  if (clientSyncDelay >= 30 && walletSyncDelay >= 30) {
+    return {
       header: 'Last known state: ',
       info: `${clientSyncDelay} min behind`,
       message: STATUS_MAJOR_NETWORK_SLOWDOWN,
     }
-  } else if (clientSyncDelay >= 3 || walletSyncDelay >= 3) {
-    syncInfo = {
+  }
+
+  if (clientSyncDelay >= 3 || walletSyncDelay >= 3) {
+    return {
       header: 'Out of sync: ',
       info: `${clientSyncDelay} min behind`,
       message: STATUS_NETWORK_SYNC_ISSUES,
     }
-  } else {
-    syncInfo = {
-      header: 'Synced: ',
-      info: `current block ${currentBlock}`,
-      message: balance.lt(minimumTransactionBalance)
-        ? STATUS_TOO_LITTLE_ETH
-        : STATUS_CONNECTION_OK,
-    }
   }
 
-  return syncInfo
+  return {
+    header: 'Synced: ',
+    info: `current block ${currentBlock}`,
+    message: balance.lt(minimumTransactionBalance)
+      ? STATUS_TOO_LITTLE_ETH
+      : STATUS_CONNECTION_OK,
+  }
 }
