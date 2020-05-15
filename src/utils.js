1dcfe45164d743e057fcd41707cde9ad18621c9d
Brett Sun <qisheng.brett.sun@gmail.com>
2020-05-15 14:55:11 +0200

    Utils: update email regex

--- src/utils.js
+++ src/utils.js
@@ -147,8 +147,7 @@ export function cssgu(strings, ...substitutions) {
 // Email validation regex from https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 export function validateEmail(email) {
   // eslint-disable-next-line no-useless-escape
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  return re.test(String(email).toLowerCase())
+  return /^.+\@.+\..+$/.test(email)
 }
 
 export const iOS =
