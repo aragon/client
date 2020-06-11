5f51ce134a087beba32785df8a083ea25b9eebbe
andy-hook <hello@andy-hook.co.uk>
2020-06-11 09:05:28 +0100

    Add disputable apps list

--- src/apps/Agreement/Agreement.js
+++ src/apps/Agreement/Agreement.js
@@ -1,5 +1,15 @@
-import React, { useState } from 'react'
-import { Box, Button, Header, Split, IconEdit, useLayout } from '@aragon/ui'
+import React, { useMemo, useState } from 'react'
+import {
+  Box,
+  Button,
+  Header,
+  Split,
+  IconEdit,
+  useLayout,
+  IconTrash,
+} from '@aragon/ui'
+import DisputableApps from './DisputableApps/DisputableApps'
+import DisputableAppsEmpty from './DisputableApps/DisputableAppsEmpty'
 import AgreementDetails from './AgreementDetails'
 import AgreementHeader from './AgreementHeader'
 import { STATUS_PENDING, STATUS_ACTIVE } from './agreement-statuses'
@@ -7,6 +17,51 @@ import { STATUS_PENDING, STATUS_ACTIVE } from './agreement-statuses'
 const Agreement = React.memo(function Agreement() {
   const { layoutName } = useLayout()
   const [agreementStatus, setAgreementStatus] = useState(STATUS_PENDING)
+
+  // TODO: Replace with real data
+  const mockAppItem = useMemo(() => {
+    return {
+      entryActions: [
+        [
+          () => {
+            console.log('Update disputable app')
+          },
+          IconEdit,
+          'Update',
+        ],
+        [
+          () => {
+            console.log('Remove disputable app')
+          },
+          IconTrash,
+          'Remove',
+        ],
+      ],
+      allowedActions: ['Action one', 'Action two', 'Action three'],
+      actionCollateral: {
+        amount: 100,
+        symbol: 'ANT',
+        address: '0x960b236A07cf122663c4303350609A66A7B288C0',
+      },
+      challengeCollateral: {
+        amount: 100,
+        symbol: 'ANT',
+        address: '0x960b236A07cf122663c4303350609A66A7B288C0',
+      },
+      signerEligibility: {
+        amount: 5,
+        symbol: 'ANT',
+        address: '0x960b236A07cf122663c4303350609A66A7B288C0',
+      },
+      challengeEligibility: 'Open to everyone',
+      challengePeriod: 48,
+      settlementPeriod: 24,
+    }
+  }, [])
+
+  const mockAppItems = useMemo(() => [mockAppItem, mockAppItem, mockAppItem], [
+    mockAppItem,
+  ])
 
   return (
     <React.Fragment>
@@ -51,8 +106,11 @@ const Agreement = React.memo(function Agreement() {
                 ContractHash="0x281c36aee917b24d8e5f59481f6639d81e4cf7125b09fb93a2b43c31ef3fc115"
               />
             </Box>
-            <Box>Disputable apps</Box>
-
+            {mockAppItems.length > 0 ? (
+              <DisputableApps items={mockAppItems} />
+            ) : (
+              <DisputableAppsEmpty />
+            )}
             <Box>Agreement doc</Box>
           </React.Fragment>
         }
