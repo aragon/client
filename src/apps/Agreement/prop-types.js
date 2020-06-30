ceacfd6739712564e5ff52f71fd4ed363076e004
Andy Hook <hello@andy-hook.co.uk>
2020-06-30 09:28:34 +0100

    prop-types code style

--- src/apps/Agreement/prop-types.js
+++ src/apps/Agreement/prop-types.js
@@ -1,25 +1,26 @@
 import PropTypes from 'prop-types'
 
 const AppItemType = PropTypes.shape({
-  entryActions: PropTypes.arrayOf(PropTypes.array),
-  allowedActions: PropTypes.arrayOf(PropTypes.string).isRequired,
   actionCollateral: PropTypes.shape({
+    address: PropTypes.string,
     amount: PropTypes.number,
     symbol: PropTypes.string,
-    address: PropTypes.string,
   }),
+  allowedActions: PropTypes.arrayOf(PropTypes.string).isRequired,
   challengeCollateral: PropTypes.shape({
+    address: PropTypes.string,
     amount: PropTypes.number,
     symbol: PropTypes.string,
-    address: PropTypes.string,
-  }),
-  signerEligibility: PropTypes.shape({
-    amount: PropTypes.number,
-    symbol: PropTypes.string,
-    address: PropTypes.string,
   }),
   challengeEligibility: PropTypes.string.isRequired,
   challengePeriod: PropTypes.number.isRequired,
+  entryActions: PropTypes.arrayOf(PropTypes.array),
+  signerEligibility: PropTypes.shape({
+    address: PropTypes.string,
+    amount: PropTypes.number,
+    symbol: PropTypes.string,
+  }),
+
   settlementPeriod: PropTypes.number.isRequired,
 })
 
