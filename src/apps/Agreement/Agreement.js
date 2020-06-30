41ebf9d14f10d6e57cd8f2c7fb901c3eb4f810e5
Andy Hook <hello@andy-hook.co.uk>
2020-06-30 11:20:08 +0100

    Agreement: 2 – Add disputable apps list (#1446)
    * Agreement: 3 – Add static versions, doc, config checklist (#1447)

--- src/apps/Agreement/Agreement.js
+++ src/apps/Agreement/Agreement.js
@@ -1,14 +1,103 @@
-import React, { useState } from 'react'
-import { Box, Button, Header, Split, IconEdit, useLayout } from '@aragon/ui'
+import React, { useCallback, useMemo, useState } from 'react'
+import {
+  Box,
+  Button,
+  Header,
+  IconEdit,
+  IconTrash,
+  Split,
+  useLayout,
+} from '@aragon/ui'
+import { STATUS_ACTIVE, STATUS_PENDING } from './agreement-statuses'
 import AgreementDetails from './AgreementDetails'
+import AgreementDocument from './AgreementDocument'
 import AgreementHeader from './AgreementHeader'
-import { STATUS_PENDING, STATUS_ACTIVE } from './agreement-statuses'
+import ConfigurationChecklist from './ConfigurationChecklist'
+import DisputableApps from './DisputableApps/DisputableApps'
+import DisputableAppsEmpty from './DisputableApps/DisputableAppsEmpty'
+import VersionHistory from './VersionHistory'
+import VotePending from './VotePending'
 
 const Agreement = React.memo(function Agreement() {
+  const [checklistCompleted, setChecklistCompleted] = useState(false)
   const [agreementStatus, setAgreementStatus] = useState(STATUS_PENDING)
   const { layoutName } = useLayout()
 
   const compactMode = layoutName === 'small'
+
+  const handleChecklistClose = useCallback(() => {
+    setChecklistCompleted(true)
+  }, [])
+
+  // TODO: Replace with real data
+  const mockEndDate = useMemo(() => {
+    const NOW = Date.now()
+    const DAY = 1000 * 60 * 60 * 24
+
+    return new Date(NOW + 5 * DAY)
+  }, [])
+
+  // TODO: Replace with real data
+  const mockChecklistItems = useMemo(
+    () => [
+      ['Create Agreement', true],
+      ['Set permissions', true],
+      ['Set actions requirements', true],
+      ['Share with members', false],
+    ],
+    []
+  )
+
+  // TODO: Replace with real data
+  const mockHistoryItems = useMemo(
+    () => ['2020/05/22', '2020/05/21', '2020/05/20'],
+    []
+  )
+
+  // TODO: Replace with real data
+  const mockAppItem = useMemo(() => {
+    return {
+      entryActions: [
+        [
+          () => {
+            console.log('Update disputable app')
+          },
+          <IconEdit />,
+          'Update',
+        ],
+        [
+          () => {
+            console.log('Remove disputable app')
+          },
+          <IconTrash />,
+          'Remove',
+        ],
+      ],
+      allowedActions: ['Action one', 'Action two', 'Action three'],
+      actionCollateral: {
+        amount: 100,
+        symbol: 'ANT',
+        address: '0x960b236A07cf122663c4303350609A66A7B288C0',
+      },
+      challengeCollateral: {
+        amount: 100,
+        symbol: 'ANT',
+        address: '0x960b236A07cf122663c4303350609A66A7B288C0',
+      },
+      signerEligibility: {
+        amount: 5,
+        symbol: 'ANT',
+        address: '0x960b236A07cf122663c4303350609A66A7B288C0',
+      },
+      challengeEligibility: 'Open to everyone',
+      challengePeriod: 48,
+      settlementPeriod: 24,
+    }
+  }, [])
+
+  const mockAppItems = useMemo(() => [mockAppItem, mockAppItem, mockAppItem], [
+    mockAppItem,
+  ])
 
   return (
     <React.Fragment>
@@ -53,16 +142,31 @@ const Agreement = React.memo(function Agreement() {
                 contractAddress="0x7c708ac7db979fa06705f8880f29f82cfc406993"
               />
             </Box>
-            <Box>Disputable apps</Box>
-
-            <Box>Agreement doc</Box>
+            {mockAppItems.length > 0 ? (
+              <DisputableApps items={mockAppItems} />
+            ) : (
+              <DisputableAppsEmpty />
+            )}
+            <AgreementDocument title="DAO Agreement" />
           </React.Fragment>
         }
         secondary={
           <React.Fragment>
-            <Box>Checklist</Box>
+            {!checklistCompleted && (
+              <ConfigurationChecklist
+                items={mockChecklistItems}
+                onClose={handleChecklistClose}
+              />
+            )}
 
-            <Box>Version history</Box>
+            <Box heading="Version history" padding={0}>
+              {agreementStatus === STATUS_PENDING && (
+                <VotePending endDate={mockEndDate} />
+              )}
+              {agreementStatus === STATUS_ACTIVE && (
+                <VersionHistory items={mockHistoryItems} />
+              )}
+            </Box>
           </React.Fragment>
         }
       />
---
+++ src/apps/Agreement/ConfigurationChecklist.js
@@ -0,0 +1,207 @@
+import React, { useMemo } from 'react'
+import PropTypes from 'prop-types'
+import { Transition, animated } from 'react-spring'
+import {
+  Box,
+  Button,
+  ProgressBar,
+  springs,
+  textStyle,
+  useLayout,
+  useTheme,
+  GU,
+} from '@aragon/ui'
+import checklistCompleteGraphic from './assets/checklist-complete.png'
+
+function countProgress(items) {
+  return items.filter(([_, checked]) => checked).length
+}
+
+const AnimatedDiv = animated.div
+
+function ConfigurationChecklist({ items, onClose }) {
+  const theme = useTheme()
+  const progress = useMemo(() => countProgress(items), [items])
+
+  const barProgress = (1 / items.length) * progress
+  const checklistComplete = items.length === progress
+
+  return (
+    <Box heading="Configuration checklist" padding={0}>
+      <Transition
+        native
+        items={checklistComplete}
+        from={{
+          opacity: 0,
+          transform: 'scale(0.7)',
+        }}
+        enter={{
+          opacity: 1,
+          transform: 'scale(1)',
+        }}
+        config={springs.smooth}
+      >
+        {show =>
+          show &&
+          (props => (
+            <BoxSection>
+              <AnimatedDiv
+                style={props}
+                css={`
+                  display: flex;
+                  flex-direction: column;
+                  align-items: center;
+                `}
+              >
+                <h2
+                  css={`
+                    text-align: center;
+                    margin-bottom: ${2 * GU}px;
+                  `}
+                >
+                  All done! Congratulations!
+                </h2>
+                <img
+                  src={checklistCompleteGraphic}
+                  css={`
+                    height: 130px;
+                  `}
+                />
+              </AnimatedDiv>
+            </BoxSection>
+          ))
+        }
+      </Transition>
+      {!checklistComplete &&
+        items.map(([label, checked]) => (
+          <BoxSection key={label}>
+            <ChecklistItem label={label} checked={checked} />
+          </BoxSection>
+        ))}
+
+      <BoxSection>
+        <div
+          css={`
+            padding-bottom: ${1 * GU}px;
+          `}
+        >
+          <label
+            css={`
+              display: flex;
+              justify-content: space-between;
+              color: ${theme.surfaceContentSecondary};
+              margin-bottom: ${1 * GU}px;
+
+              ${textStyle('label2')}
+            `}
+          >
+            <span>Progress</span>
+            <span>
+              {progress}/{items.length}
+            </span>
+          </label>
+          <ProgressBar value={barProgress} />
+          {checklistComplete && (
+            <Button
+              label="Close"
+              wide
+              onClick={onClose}
+              css={`
+                margin-top: ${2 * GU}px;
+              `}
+            />
+          )}
+        </div>
+      </BoxSection>
+    </Box>
+  )
+}
+
+ConfigurationChecklist.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.array),
+  onClose: PropTypes.func,
+}
+
+/* eslint-disable react/prop-types */
+
+// Checklist items use a custom style and not a standard checkbox because they are non-interactive and thus should not:
+// - Look like other elements that are known to be interactive
+// - Have or inherit hover, active or focus states
+function ChecklistItem({ label, checked }) {
+  const theme = useTheme()
+
+  return (
+    <label
+      css={`
+        display: flex;
+        align-items: center;
+      `}
+    >
+      <div
+        css={`
+          flex-shrink: 0;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          width: ${3 * GU}px;
+          height: ${3 * GU}px;
+          border-radius: 100%;
+          margin-right: ${1.5 * GU}px;
+          background-color: ${theme.infoSurface};
+        `}
+      >
+        {checked && <Check color={theme.surfaceContent} />}
+      </div>
+      <span
+        css={`
+          color: ${checked
+            ? theme.surfaceContent
+            : theme.surfaceContentSecondary};
+        `}
+      >
+        {label}
+      </span>
+    </label>
+  )
+}
+
+const Check = ({ color }) => (
+  <svg width="12" height="8" viewBox="0 0 12 8">
+    <path
+      d={`
+        M11.059 1.393
+        L4.335  7.395
+        L0.944  3.260
+        L2.104  2.309
+        L4.503  5.234
+        L10.060 0.274
+        L11.059 1.393
+        Z
+      `}
+      fill={color}
+    />
+  </svg>
+)
+
+function BoxSection({ children, ...props }) {
+  const { layoutName } = useLayout()
+  const theme = useTheme()
+
+  return (
+    <div
+      css={`
+        padding: ${2 * GU}px ${layoutName === 'small' ? 2 * GU : 3 * GU}px;
+
+        & + & {
+          border-top: 1px solid ${theme.border};
+        }
+      `}
+      {...props}
+    >
+      {children}
+    </div>
+  )
+}
+/* eslint-enable react/prop-types */
+
+export default ConfigurationChecklist
