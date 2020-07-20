cc308026d4b4a30f2937733227513807005425aa
Andy Hook <hello@andy-hook.co.uk>
2020-07-20 20:07:07 +0100

    Minor change on propTypes

--- src/components/SigningModal/prop-types.js
+++ src/components/SigningModal/prop-types.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 
-export const ModalProps = PropTypes.shape({
+export const modalProps = PropTypes.shape({
   prevScreen: PropTypes.func,
   nextScreen: PropTypes.func,
   closeModal: PropTypes.func,
