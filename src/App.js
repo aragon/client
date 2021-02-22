4bfb1b388c4dbff7c7affb0990ef61b7f714d7b7
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-02-22 19:53:47 +0100

    widget through app

--- src/App.js
+++ src/App.js
@@ -85,6 +85,22 @@ class App extends React.Component {
     pollConnectivity([web3Providers.default], connected => {
       this.setState({ connected })
     })
+    var $zoho = $zoho || {}
+    $zoho.salesiq = $zoho.salesiq || {
+      widgetcode:
+        '8368321708326b3f205f6985224f65416311aafa35bf8cb06e7d9f50921fc950',
+      values: {},
+      ready: function() {},
+    }
+    var d = document
+    s = d.createElement('script')
+    s.type = 'text/javascript'
+    s.id = 'zsiqscript'
+    s.defer = true
+    s.src = 'https://salesiq.zoho.eu/widget'
+    t = d.getElementsByTagName('script')[0]
+    t.parentNode.insertBefore(s, t)
+    d.write("<div id='zsiqwidget'></div>")
   }
 
   componentDidUpdate(prevProps, prevState) {
