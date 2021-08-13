74866df55bbf712efb8b57e09048d5a5d37bbb35
yuetloo <contact@yuetloo.com>
2021-08-13 17:23:33 -0300

    add address field and refactoring

--- src/apps/GovernMigration/GovernMigration.js
+++ src/apps/GovernMigration/GovernMigration.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useCallback, useRef, useMemo, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import {
   Box,
@@ -10,18 +10,23 @@ import {
   unselectable,
   useLayout,
   useTheme,
-  TextInput,
   Button,
 } from '@aragon/ui'
 import LocalIdentityBadge from '../../components/IdentityBadge/LocalIdentityBadge'
-import appIds from '../../known-app-ids'
 import { AppType, DaoAddressType } from '../../prop-types'
-import { getEmptyAddress, toChecksumAddress } from '../../web3-utils'
+import {
+  getEmptyAddress,
+  toChecksumAddress,
+  isAddress,
+  isEmptyAddress,
+} from '../../web3-utils'
 import styled from 'styled-components'
 import { NETWORK_TYPE } from '../../NetworkType'
 import { useWallet } from '../../wallet'
+import AddressField from '../../components/AddressField/AddressField'
+import { InvalidAddress, RequiredField } from '../../errors'
 
-const GOVERN_REWARD_URL = 'https://governreward.aragon.org'
+const GOVERN_REWARD_URL = 'https://upgrade.aragon.org/governreward'
 const MIGRATE_REWARD_URL =
   'https://help.aragon.org/article/99-aragon-govern-migration-reward-program'
 
@@ -31,197 +36,193 @@ function getCreateOneUrl(networkType) {
   }.aragon.org/#/create-dao`
 }
 
+function validateAddress(address) {
+  if (!address) {
+    return new RequiredField().message
+  } else if (isEmptyAddress(address)) {
+    return new InvalidAddress().message
+  } else if (!isAddress(address)) {
+    return new InvalidAddress().message
+  } else {
+    return null
+  }
+}
+
 const GovernMigration = React.memo(function GovernMigration({
-  apps,
   appsLoading,
+  apps,
   daoAddress,
 }) {
   const theme = useTheme()
   const { layoutName } = useLayout()
   const { networkType } = useWallet()
 
-  const apmApps = apps.filter(app => !app.isAragonOsInternalApp)
-  const hasAgentApp = apps.some(app => app.appId === appIds.Agent)
-  const hasFinanceApp = apps.some(app => app.appId === appIds.Finance)
-  const checksummedDaoAddr =
-    daoAddress.address && toChecksumAddress(daoAddress.address)
+  const [governAddress, setGovernAddress] = useState('')
+  const [addressError, setAddressError] = useState(null)
+  const addressRef = useRef(null)
+
   const shortAddresses = layoutName !== 'large'
 
-  const organizationText = checksummedDaoAddr ? (
-    <span>Your DAO addresses</span>
-  ) : (
-    'Resolving organization address…'
-  )
+  const checksummedDaoAddr =
+    daoAddress.address && toChecksumAddress(daoAddress.address)
 
-  const depositFundsHelpText = appsLoading ? (
-    ''
-  ) : hasFinanceApp || hasAgentApp ? (
-    <span>If you’d like to deposit funds into this organization</span>
-  ) : (
-    `This organization does not have a Finance or Agent app installed and may
-     not be able to receive funds. Please check with the organization’s
-     administrators if any other installed apps are able to receive funds.`
-  )
+  const apmApps = useMemo(() => {
+    const org = checksummedDaoAddr
+      ? [
+          {
+            name: 'organization',
+            proxyAddress: checksummedDaoAddr,
+            tags: [],
+          },
+        ]
+      : []
+    return org.concat(apps.filter(app => !app.isAragonOsInternalApp))
+  }, [apps, checksummedDaoAddr])
+
+  const handleAddressChange = useCallback(e => {
+    const address = e.target.value
+    setGovernAddress(address)
+    setAddressError(validateAddress(address))
+  }, [])
+
+  const handleMigration = useCallback(() => {
+    const error = validateAddress(governAddress)
+    setAddressError(error)
+    if (!error) {
+      alert('migration')
+    }
+  }, [governAddress])
+
+  // focus address field on mount
+  useEffect(() => {
+    if (addressRef.current) {
+      addressRef.current.focus()
+    }
+  }, [])
 
   return (
     <React.Fragment>
       <Header.Title>Aragon Govern Reward Program</Header.Title>
-      <div
-        css={`
-          ${textStyle('body2')};
-          margin-bottom: ${3 * GU}px;
-        `}
-      >
+      <SubTitle>
         Migrate your DAO to an Aragon Govern DAO and receive options that are
         convertible to ANT.{' '}
         <StyledLink href={MIGRATE_REWARD_URL}>Learn more</StyledLink>.
-      </div>
-      <Box heading="Generate Migration">
-        <p
-          css={`
-            ${textStyle('body2')}
-          `}
-        >
-          {organizationText}
-        </p>
-        {checksummedDaoAddr && (
-          <React.Fragment>
-            <div
-              css={`
-                margin-top: ${2 * GU}px;
-                margin-bottom: ${3 * GU}px;
-              `}
-            >
-              <LocalIdentityBadge
-                entity={checksummedDaoAddr}
-                shorten={shortAddresses}
-              />
-            </div>
-            <Info>
-              <strong css="font-weight: 800">
-                Do not send ETH or ERC20 tokens to this address.
-              </strong>{' '}
-              {depositFundsHelpText}
+      </SubTitle>
+
+      <React.Fragment>
+        <Box heading="Generate migration">
+          <Label theme={theme}>Your DAO addresses</Label>
+          <List>
+            {appsLoading ? (
+              <div>Loading apps…</div>
+            ) : (
+              apmApps.map(({ name, proxyAddress, tags }) => (
+                <li key={proxyAddress}>
+                  <ListItemLabel theme={theme}>
+                    {name}
+                    {tags.length > 0 ? ` (${tags.join(', ')})` : ''}
+                  </ListItemLabel>
+                  <IdentityBadgeContainter>
+                    <LocalIdentityBadge
+                      entity={proxyAddress}
+                      shorten={shortAddresses}
+                    />
+                  </IdentityBadgeContainter>
+                </li>
+              ))
+            )}
+          </List>
+          <div>
+            <AddressField
+              wide
+              label="Your new Aragon Govern DAO"
+              placeholder={getEmptyAddress()}
+              ref={addressRef}
+              onChange={handleAddressChange}
+              value={governAddress}
+              error={addressError}
+            />
+            <Info mode="warning">
+              Make sure your <b>Aragon Govern Executor</b> address is correct,
+              otherwise you might send all your funds to an invalid address and
+              lose access to them
             </Info>
-          </React.Fragment>
-        )}
-      </Box>
-      {appsLoading ? (
-        <Box heading="Installed Aragon apps">
-          <div
-            css={`
-              display: flex;
-              align-items: center;
-              justify-content: center;
-              height: ${22 * GU}px;
-              ${textStyle('body2')}
-            `}
-          >
-            Loading apps…
+            <StyledButton mode="strong" onClick={handleMigration}>
+              Generate Migration
+            </StyledButton>
+            {networkType && (
+              <Label theme={theme}>
+                Don't have a Aragon Govern DAO?{' '}
+                <StyledLink href={getCreateOneUrl(networkType)}>
+                  Create one
+                </StyledLink>
+                .
+              </Label>
+            )}
+            <Info>
+              By executing this you will be creating a proposal that will be
+              voted in your DAO. Once it is approved and executed,{' '}
+              <b>all your DAO funds</b> will be transfered to the new DAO Govern
+              Executor address, and the new DAO will be entitled to receive the
+              KPI options. The options will be sent to the same Aragon Govern
+              Executor address within 10 days from the migration. You can
+              consult the options amount in{' '}
+              <StyledLink href={GOVERN_REWARD_URL}>
+                governreward.aragon.org
+              </StyledLink>
+            </Info>
           </div>
         </Box>
-      ) : (
-        <React.Fragment>
-          <Box heading="Installed Aragon apps">
-            <ul
-              css={`
-                list-style: none;
-                display: grid;
-                grid-template-columns: minmax(50%, 1fr) minmax(50%, 1fr);
-                grid-column-gap: ${2 * GU}px;
-                margin-bottom: -${3 * GU}px;
-              `}
-            >
-              {apmApps.map(
-                ({ appId, description, name, proxyAddress, tags }) => (
-                  <li
-                    key={proxyAddress}
-                    css={`
-                      margin-bottom: ${3 * GU}px;
-                    `}
-                  >
-                    <label
-                      css={`
-                        color: ${theme.surfaceContentSecondary};
-                        ${unselectable()};
-                        ${textStyle('label2')};
-                      `}
-                    >
-                      {name}
-                      {tags.length > 0 ? ` (${tags.join(', ')})` : ''}
-                    </label>
-                    <div
-                      css={`
-                        margin-top: ${1 * GU}px;
-                      `}
-                    >
-                      <LocalIdentityBadge
-                        entity={proxyAddress}
-                        shorten={shortAddresses}
-                      />
-                    </div>
-                  </li>
-                )
-              )}
-            </ul>
-          </Box>
-        </React.Fragment>
-      )}
-      <div
-        css={`
-          margin-top: ${GU * 3}px;
-          display: grid;
-          row-gap: ${GU * 2}px;
-        `}
-      >
-        <Label>Your new Aragon Govern DAO</Label>
-        <TextInput wide placeholder={getEmptyAddress()} />
-        <Info mode="warning">
-          Make sure your <b>Aragon Govern Executor</b> address is correct,
-          otherwise you might send all your funds to an invalid address and lose
-          access to them
-        </Info>
-        <Button mode="strong">Generate Migration</Button>
-        {networkType && (
-          <Label>
-            Don't have a Aragon Govern DAO?{' '}
-            <StyledLink href={getCreateOneUrl(networkType)}>
-              Create one
-            </StyledLink>
-            .
-          </Label>
-        )}
-        <Info>
-          By executing this you will be creating a proposal that will be voted
-          in your DAO. Once it is approved and executed,{' '}
-          <b>all your DAO funds</b> will be transfered to the new DAO Govern
-          Executor address, and the new DAO will be entitled to receive the KPI
-          options. The options will be sent to the same Aragon Govern Executor
-          address within 10 days from the migration. You can consult the options
-          amount in{' '}
-          <StyledLink href={GOVERN_REWARD_URL}>
-            governreward.aragon.org
-          </StyledLink>
-        </Info>
-      </div>
+      </React.Fragment>
     </React.Fragment>
   )
 })
+
+const Label = styled.label`
+  color: ${props => props.theme.content};
+  display: block;
+  margin-bottom: ${2 * GU}px;
+  ${textStyle('body2')}
+`
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`
+
+const SubTitle = styled.div`
+  ${textStyle('body2')};
+  margin-bottom: ${3 * GU}px;
+`
+
+const List = styled.ul`
+  list-style: none;
+  display: grid;
+  grid-template-columns: minmax(50%, 1fr) minmax(50%, 1fr);
+  grid-column-gap: ${2 * GU}px;
+  margin-bottom: ${2 * GU}px;
+  & > li {
+    margin-bottom: ${3 * GU}px;
+  }
+`
+
+const StyledButton = styled(Button)`
+  margin: ${3 * GU}px 0;
+`
+
+const ListItemLabel = styled.label`
+  color: ${props => props.theme.surfaceContentSecondary};
+  ${unselectable()};
+  ${textStyle('label2')};
+`
+
+const IdentityBadgeContainter = styled.div`
+  margin-top: ${1 * GU}px;
+`
 
 GovernMigration.propTypes = {
   apps: PropTypes.arrayOf(AppType).isRequired,
   appsLoading: PropTypes.bool.isRequired,
   daoAddress: DaoAddressType.isRequired,
 }
-
-const Label = styled.label`
-  color: ${({ theme }) => theme.content};
-  display: block;
-  margin-bottom: ${2 * GU}px;
-  ${textStyle('body2')}
-`
-const StyledLink = styled(Link)`
-  text-decoration: none;
-`
 
 export default GovernMigration
