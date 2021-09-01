6a8cf31392ca228659232ed49ab0d70cd85e9a20
yuetloo <contact@yuetloo.com>
2021-08-31 21:23:58 -0400

    cleanup code

--- src/utils.js
+++ src/utils.js
@@ -24,7 +24,6 @@ export function pollEvery(fn, delay) {
     poll(request, onResult)
     return () => {
       stop = true
-      console.log('............>clear timer')
       clearTimeout(timer)
     }
   }
