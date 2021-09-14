0cf6408382d8519a5c75b03e2f6ef4492e13ed54
zain <vphu@clarity.my>
2021-09-15 05:39:04 +0800

    update: fix issue with getLogs query return more than 1024 blocks

--- scripts/config/local.js
+++ scripts/config/local.js
@@ -1,4 +1,4 @@
 module.exports = {
   DEFAULT_LOCAL_ENS_ADDRESS: '0x3c682c41409e18af890545ab17ab0dc8a90777f1',
-  DEFAULT_LOCAL_IPFS_GATEWAY: 'https://ipfs.io/ipfs',
+  DEFAULT_LOCAL_IPFS_GATEWAY: 'https://ipfs.infura.io/ipfs/',
 }
