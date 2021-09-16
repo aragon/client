102b96b1bb6b458525d81496d249f7700a6f4080
zain <vphu@clarity.my>
2021-09-17 02:26:31 +0800

    update: changed the ens address to latest version

--- scripts/config/local.js
+++ scripts/config/local.js
@@ -1,4 +1,4 @@
 module.exports = {
-  DEFAULT_LOCAL_ENS_ADDRESS: '0x3c682c41409e18af890545ab17ab0dc8a90777f1',
-  DEFAULT_LOCAL_IPFS_GATEWAY: 'https://ipfs.infura.io/ipfs/',
+  DEFAULT_LOCAL_ENS_ADDRESS: '0x5d0d0212199b2c041483226b20f1e4ed6ca4a4de',
+  DEFAULT_LOCAL_IPFS_GATEWAY: 'https://ipfs.infura.io/ipfs',
 }
