d0622e734410d6c388aec4d1371fdaee34a4bc81
Valentin Racine <32465601+WissenIstNacht@users.noreply.github.com>
2021-09-20 20:19:35 +0200

    Feature/improve notfound modal dao 268 (#1637)
    * added .vscode to gitignore
    
    * moved utils file into separate folder
    
    * changed import statements
    
    * moved context into context folder
    
    * adapted import paths
    
    * refactored Onboarding topbar
    
    * added chain indicator to onboardin topbar
    
    * added chain indicator to orgview topbar
    
    * extracted network indicator module
    
    * grouped modal into folder
    
    * adapted imports
    
    * scaffold for network switch modal
    
    * added layout and content to modal
    
    * improved Modals module
    
    * changed networkswitch modal props
    
    * added networkswitch modal to orgview
    
    * made network indicator clickable
    
    * made clickable networkindicator open modal
    
    * small orgview refactoring
    
    * centered network indicator on orgview
    
    * network switch button hooked up to logic
    
    * use existing method for network name
    
    * refactored network config/utils
    
    * fixed network switching issues
    
    * added short vs full names
    
    * removed unused prop
    
    * addressed PR comments
    
    * changed variable name to testnet
    
    * fixed copy/paste error
    
    * changed not found copy on notfound modal
    
    * extracted message component
    
    * added found on other network message
    
    * handling addresses
    
    * add a space before the network name
    
    Co-authored-by: yuetloo <contact@yuetloo.com>

--- src/components/Error/DAONotFoundError.js
+++ src/components/Error/DAONotFoundError.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { useTheme, textStyle, Link, GU, Info } from '@aragon/ui'
+import styled from 'styled-components'
+
 import { isAddress } from '../../util/web3'
 import { useWallet } from '../../contexts/wallet'
 import { getNetworkFullName } from '../../util/network'
 
+DAONotFoundError.propTypes = {
+  dao: PropTypes.string,
+}
+
 function DAONotFoundError({ dao }) {
   const theme = useTheme()
-  const { networkType } = useWallet()
+  // mock object, to be determined dynamically as specified in DAO-269
+  const alternativeNetworks = ['polygon', 'ropsten']
 
   return (
     <React.Fragment>
@@ -21,24 +28,16 @@ function DAONotFoundError({ dao }) {
       >
         Organization not found
       </h1>
-      <div
-        css={`
-          margin-bottom: ${6 * GU}px;
-          text-align: center;
-          color: ${theme.surfaceContentSecondary};
-          ${textStyle('body2')};
-        `}
-      >
-        It looks like there’s no organization associated with{' '}
-        {isAddress(dao) ? (
-          <span css="font-weight: bold;">“{dao}”</span>
+      <MessageContainer>
+        {alternativeNetworks.length ? (
+          <NotFoundOnNetworkMessage
+            dao={dao}
+            alternatives={alternativeNetworks}
+          />
         ) : (
-          <React.Fragment>
-            the <strong>“{dao}”</strong> ENS domain
-          </React.Fragment>
-        )}{' '}
-        on the Ethereum {getNetworkFullName(networkType)}.
-      </div>
+          <NotFoundAtAllMessage dao={dao} />
+        )}
+      </MessageContainer>
       <Info>
         If you arrived here through a link, please double check that you were
         given the correct link. Alternatively, you may{' '}
@@ -54,8 +53,66 @@ function DAONotFoundError({ dao }) {
   )
 }
 
-DAONotFoundError.propTypes = {
+NotFoundAtAllMessage.propTypes = {
   dao: PropTypes.string,
 }
+
+function NotFoundAtAllMessage({ dao }) {
+  const theme = useTheme()
+  const { networkType } = useWallet()
+
+  return (
+    <Message color={theme.surfaceContentSecondary}>
+      There’s no organization associated with
+      <span css="font-weight: bold;">'{dao}'</span>
+      on the {getNetworkFullName(networkType)}.
+    </Message>
+  )
+}
+
+NotFoundOnNetworkMessage.propTypes = {
+  dao: PropTypes.string,
+  alternatives: PropTypes.string,
+}
+
+function NotFoundOnNetworkMessage({ dao, alternatives }) {
+  const theme = useTheme()
+  const { networkType } = useWallet()
+
+  return (
+    <React.Fragment>
+      <Message color={theme.surfaceContentSecondary}>
+        There’s no organization associated with
+        <span css="font-weight: bold;">'{dao}'</span> on the{' '}
+        {getNetworkFullName(networkType)}, but it does exist on another chain.
+        You may switch the application to another chain to see it.
+      </Message>
+      <LinksList>
+        {alternatives.map(a => (
+          <li>
+            <Link>
+              Open {!isAddress(dao) ? dao : 'it'} on {a}
+            </Link>
+          </li>
+        ))}
+      </LinksList>
+    </React.Fragment>
+  )
+}
+
+const MessageContainer = styled.div`
+  margin-bottom: ${6 * GU}px;
+  text-align: center;
+`
+
+const Message = styled.p`
+  ${textStyle('body2')};
+  color: ${props => props.color};
+`
+
+const LinksList = styled.ul`
+  list-style: none;
+  padding-top: ${2 * GU}px;
+`
 
 export default DAONotFoundError
