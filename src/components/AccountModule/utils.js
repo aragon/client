0547f745839542460ced779d864de466b12e654b
Ramon Canales <emaildoramon@gmail.com>
2021-12-22 14:23:13 +0000

    Adding Metis stardust testnet and andromeda (#1696)
    * Adding Metis stardust testnet and andromeda
    
    * cleaning code
    
    * removing gas price estimation
    
    * changing delay values to be more flexible because of L2s rollups

--- src/components/AccountModule/utils.js
+++ src/components/AccountModule/utils.js
@@ -5,8 +5,8 @@ import {
 
 export const DROPPED_PROVIDER_SYNC_DELAY = 45
 export const MAX_PROVIDER_SYNC_DELAY = 30
-export const MILD_PROVIDER_SYNC_DELAY = 5
-export const OK_PROVIDER_SYNC_DELAY = 3
+export const MILD_PROVIDER_SYNC_DELAY = 10
+export const OK_PROVIDER_SYNC_DELAY = 5
 
 export function getConnectionMessage(
   connectionStatus,
